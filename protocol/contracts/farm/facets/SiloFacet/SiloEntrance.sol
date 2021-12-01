167f4a192a09ef55c8a53ad4ceed046e2913613b
Daniel Trinh <danielhtrinh1@gmail.com>
2021-12-01 13:35:06 -0800

    Adding decrementBipRoots logic

--- protocol/contracts/farm/facets/SiloFacet/SiloEntrance.sol
+++ protocol/contracts/farm/facets/SiloFacet/SiloEntrance.sol
@@ -152,6 +152,8 @@ contract SiloEntrance is SiloExit {
 
         s.s.roots = s.s.roots.sub(roots);
         s.a[account].roots = s.a[account].roots.sub(roots);
+
+        decrementBipRoots(account, roots);
     }
 
     function addBeanDeposit(address account, uint32 _s, uint256 amount) internal {
@@ -181,6 +183,15 @@ contract SiloEntrance is SiloExit {
             for (uint256 i = 0; i < s.g.activeBips.length; i++) {
                 uint32 bip = s.g.activeBips[i];
                 if (s.g.voted[bip][account]) s.g.bips[bip].roots = s.g.bips[bip].roots.add(roots);
+            }
+        }
+    }
+
+    function decrementBipRoots(address account, uint256 roots) internal {
+        if (s.a[account].lockedUntil >= season()) {
+            for (uint256 i = 0; i < s.g.activeBips.length; i++) {
+                uint32 bip = s.g.activeBips[i];
+                if (s.g.voted[bip][account]) s.g.bips[bip].roots = s.g.bips[bip].roots.sub(roots);
             }
         }
     }
