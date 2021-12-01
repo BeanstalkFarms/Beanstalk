79a64296364887792137604a249b6b064401e0b6
Daniel Trinh <danielhtrinh1@gmail.com>
2021-12-01 13:35:06 -0800

    Adding decrementBipRoots logic

--- protocol/contracts/farm/facets/SiloFacet/UpdateSilo.sol
+++ protocol/contracts/farm/facets/SiloFacet/UpdateSilo.sol
@@ -106,4 +106,96 @@ contract UpdateSilo is SiloExit {
             s.a[account].lastRain = 0;
         }
     }
+
+    /**
+     * Silo
+    **/
+
+    function depositSiloAssets(address account, uint256 seeds, uint256 stalk) internal {
+        incrementBalanceOfStalk(account, stalk);
+        incrementBalanceOfSeeds(account, seeds);
+    }
+
+    function incrementBalanceOfSeeds(address account, uint256 seeds) internal {
+        s.s.seeds = s.s.seeds.add(seeds);
+        s.a[account].s.seeds = s.a[account].s.seeds.add(seeds);
+    }
+
+    function incrementBalanceOfStalk(address account, uint256 stalk) internal {
+        uint256 roots;
+        if (s.s.roots == 0) roots = stalk.mul(C.getRootsBase());
+        else roots = s.s.roots.mul(stalk).div(totalStalk());
+
+        s.s.stalk = s.s.stalk.add(stalk);
+        s.a[account].s.stalk = s.a[account].s.stalk.add(stalk);
+
+        s.s.roots = s.s.roots.add(roots);
+        s.a[account].roots = s.a[account].roots.add(roots);
+
+        incrementBipRoots(account, roots);
+    }
+
+    function withdrawSiloAssets(address account, uint256 seeds, uint256 stalk) internal {
+        decrementBalanceOfStalk(account, stalk);
+        decrementBalanceOfSeeds(account, seeds);
+    }
+
+    function decrementBalanceOfSeeds(address account, uint256 seeds) internal {
+        s.s.seeds = s.s.seeds.sub(seeds);
+        s.a[account].s.seeds = s.a[account].s.seeds.sub(seeds);
+    }
+
+    function decrementBalanceOfStalk(address account, uint256 stalk) internal {
+        if (stalk == 0) return;
+        uint256 roots = s.a[account].roots.mul(stalk).sub(1).div(s.a[account].s.stalk).add(1);
+
+        s.s.stalk = s.s.stalk.sub(stalk);
+        s.a[account].s.stalk = s.a[account].s.stalk.sub(stalk);
+
+        s.s.roots = s.s.roots.sub(roots);
+        s.a[account].roots = s.a[account].roots.sub(roots);
+
+        decrementBipRoots(account, roots);
+    }
+
+    function addBeanDeposit(address account, uint32 _s, uint256 amount) internal {
+        s.a[account].bean.deposits[_s] += amount;
+        emit BeanDeposit(account, _s, amount);
+    }
+
+    function incrementDepositedBeans(uint256 amount) internal {
+        s.bean.deposited = s.bean.deposited.add(amount);
+    }
+
+    modifier notLocked(address account) {
+        require(!(locked(account)),"locked");
+        _;
+    }
+
+    function updateBalanceOfRainStalk(address account) internal {
+        if (!s.r.raining) return;
+        if (s.a[account].roots < s.a[account].sop.roots) {
+            s.r.roots = s.r.roots.sub(s.a[account].sop.roots.sub(s.a[account].roots));
+            s.a[account].sop.roots = s.a[account].roots;
+        }
+    }
+
+    function incrementBipRoots(address account, uint256 roots) internal {
+        if (s.a[account].lockedUntil >= season()) {
+            for (uint256 i = 0; i < s.g.activeBips.length; i++) {
+                uint32 bip = s.g.activeBips[i];
+                if (s.g.voted[bip][account]) s.g.bips[bip].roots = s.g.bips[bip].roots.add(roots);
+            }
+        }
+    }
+
+    function decrementBipRoots(address account, uint256 roots) internal {
+        if (s.a[account].lockedUntil >= season()) {
+            for (uint256 i = 0; i < s.g.activeBips.length; i++) {
+                uint32 bip = s.g.activeBips[i];
+                if (s.g.voted[bip][account]) s.g.bips[bip].roots = s.g.bips[bip].roots.sub(roots);
+            }
+        }
+    }
+
 }
