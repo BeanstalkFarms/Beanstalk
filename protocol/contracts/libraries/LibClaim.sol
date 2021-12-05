83536d974325ecf12c3ebfd3c0ac4d06f3c46fe3
Beasley9 <57361391+Beasley9@users.noreply.github.com>
2021-12-05 11:59:36 -0600

    Update LibClaim.sol

--- protocol/contracts/libraries/LibClaim.sol
+++ protocol/contracts/libraries/LibClaim.sol
@@ -35,7 +35,7 @@ library LibClaim {
         uint256 minEthAmount;
     }
 
-    function claim(Claim calldata c, bool allocate)
+    function claim(Claim calldata c, uint256 beansToWallet)
         public
         returns (uint256 beansClaimed)
     {
@@ -44,16 +44,39 @@ library LibClaim {
         if (c.plots.length > 0) beansClaimed = beansClaimed.add(harvest(c.plots));
         if (c.lpWithdrawals.length > 0) {
             if (c.convertLP) {
-                if (allocate) beansClaimed = beansClaimed.add(removeAllocationAndClaimLP(c.lpWithdrawals, c.minBeanAmount, c.minEthAmount));
+                // Check the below if statement
+                if (beansToWallet == 0) beansClaimed = beansClaimed.add(removeAllocationAndClaimLP(c.lpWithdrawals, c.minBeanAmount, c.minEthAmount));
                 else removeAndClaimLP(c.lpWithdrawals, c.minBeanAmount, c.minEthAmount);
             }
             else claimLP(c.lpWithdrawals);
         }
         if (c.claimEth) claimEth();
-        if (allocate) s.a[msg.sender].claimableBeans += beansClaimed;
+
+        /*
+        Now beansClaimed contains the number of beans the user obtained from harvesting pods.
+        If beansToClaim is 0, then entire claimableBeans will be sent to state variable.
+        If beansToClaim is nonzero, then beansToClaim beans will be sent to wallet, and rest will be sent to state variable.
+        Note that beansToClaim can exceed the beansClaimed because users can also claim beans from s.a[msg.sender].claimableBeans, aka the state variable
+        */
+
+        if (beansToWallet > 0) {
+                if (beansToWallet >= s.a[msg.sender].claimableBeans.add(beansClaimed)) {
+                        IBean(s.c.bean).transfer(msg.sender, s.a[msg.sender].claimableBeans.add(beansClaimed));
+                        s.a[msg.sender].claimableBeans = 0;
+                }
+                else {
+                        IBean(s.c.bean).transfer(msg.sender, beansToWallet);
+                        if (beansToWallet < beansClaimed) {
+                                s.a[msg.sender].claimableBeans = s.a[msg.sender].claimableBeans.add(beansClaimed.sub(beansToWallet));
+                        }
+                        else {
+                                s.a[msg.sender].claimableBeans = s.a[msg.sender].claimableBeans.sub(beansToWallet.sub(beansClaimed));
+                        }
+                }
+        }
         else {
-            IBean(s.c.bean).transfer(msg.sender, beansClaimed);
-            }
+                s.a[msg.sender].claimableBeans = s.a[msg.sender].claimableBeans.add(beansClaimed);
+        }
     }
 
     // Claim Beans
