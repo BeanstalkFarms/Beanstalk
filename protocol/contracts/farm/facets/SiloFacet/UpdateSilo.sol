ecc8d8926ed642a38f4b95b628b0a71db44127e5
publius <beanstalk.publius@protonmail.com>
2021-12-11 20:24:45 -0500

    minor stylistic fixes

--- protocol/contracts/farm/facets/SiloFacet/UpdateSilo.sol
+++ protocol/contracts/farm/facets/SiloFacet/UpdateSilo.sol
@@ -219,5 +219,4 @@ contract UpdateSilo is SiloExit {
         Decimal.D256 memory stake = Decimal.ratio(balanceOfRoots(account), totalRoots());
         return stake.greaterThan(C.getGovernanceProposalThreshold());
     }
-
 }
