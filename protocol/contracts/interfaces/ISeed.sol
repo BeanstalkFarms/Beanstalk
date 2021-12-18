e636a24ad3466a78f741398e5bb963a3b249d61e
Beasley9 <57361391+Beasley9@users.noreply.github.com>
2021-12-18 09:29:14 -0600

    Create ISeed.sol

---
+++ protocol/contracts/interfaces/ISeed.sol
@@ -0,0 +1,19 @@
+/**
+ * SPDX-License-Identifier: MIT
+**/
+
+pragma solidity ^0.7.6;
+pragma experimental ABIEncoderV2;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+/**
+ * @title Seed Interface
+**/
+
+abstract contract ISeed is IERC20 {
+
+	function updateSilo(address user) public virtual payable;
+	function burn(uint256 amount) public virtual;
+	function mint(address account, uint256 amount) public virtual returns (bool);
+}
