029e8af37d7a840cf07fc7b08d26c894d007bff8
Beasley9 <57361391+Beasley9@users.noreply.github.com>
2021-12-20 20:57:01 -0600

    Update Seed.test.js

--- protocol/test/Seed.test.js
+++ protocol/test/Seed.test.js
@@ -31,6 +31,9 @@ describe('Seed', function () {
     await this.bean.connect(user2).approve(this.silo.address, '100000000000');
     await this.seed.connect(user).approve(this.silo.address, '10000000000');
     await this.seed.connect(user2).approve(this.silo.address, '1000000000');
+    await this.seed.connect(user).approve(user2Address, '10000000000');
+    await this.seed.connect(user2).approve(userAddress, '1000000000');
+
   });
 
   beforeEach (async function () {
@@ -41,62 +44,72 @@ describe('Seed', function () {
   });
 
     describe('ERC20 Traits', async function () {
-      it('properly mints the user seeds and beans', async function () {
+      it('properly mints the user seeds', async function () {
 	await this.seed.mint(userAddress, '1000');
 	await this.seed.mint(user2Address, '3000');
 	await this.bean.mint(userAddress, '5000');
 	await this.bean.mint(user2Address, '8000');
 	expect(await this.seed.balanceOf(userAddress)).to.eq('1000');
 	expect(await this.seed.balanceOf(user2Address)).to.eq('3000');
-	expect(await this.bean.balanceOf(userAddress)).to.eq('1000005000');
-	expect(await this.bean.balanceOf(user2Address)).to.eq('1000008000');
-      });
-      it('properly burns seeds and beans', async function () {
-	await this.seed.connect(user).burn('500');
-	await this.seed.connect(user2).burn('1000');
-	await this.bean.connect(user).burn('5000');
-	await this.bean.connect(user2).burn('8000');
-	expect(await this.seed.balanceOf(userAddress)).to.eq('500');
-        expect(await this.seed.balanceOf(user2Address)).to.eq('2000');
-	expect(await this.bean.balanceOf(userAddress)).to.eq('1000000000');
-	expect(await this.bean.balanceOf(user2Address)).to.eq('1000000000');
       });
       it('properly transfers seeds to other users', async function () {
 	await this.seed.connect(user).transfer(user2Address, '500');
-	await this.bean.connect(user).transfer(user2Address, '5000');
-	expect(await this.seed.balanceOf(userAddress)).to.eq('0');
+	await this.seed.connect(user2).transfer(userAddress, '1000');
+	expect(await this.seed.balanceOf(userAddress)).to.eq('1500');
 	expect(await this.seed.balanceOf(user2Address)).to.eq('2500');
-	expect(await this.bean.balanceOf(userAddress)).to.eq('999995000');
-	expect(await this.bean.balanceOf(user2Address)).to.eq('1000005000');
       });
-      it('properly transfers from another account', async function () {
-	await this.seed.transferFrom(user2Address, this.silo.address, '1000');
-	expect(await this.seed.balanceOf(this.silo.address)).to.eq('1000');
+      it('properly transfers seeds from another account', async function () {
+	await this.seed.connect(user).transferFrom(user2Address, userAddress, '1000');
+	expect(await this.seed.balanceOf(userAddress)).to.eq('2500');
 	expect(await this.seed.balanceOf(user2Address)).to.eq('1500');
      });
+      it('properly burns seeds', async function () {
+        await this.seed.connect(user).burn('2500');
+        await this.seed.connect(user2).burn('1500');
+        expect(await this.seed.balanceOf(userAddress)).to.eq('0');
+        expect(await this.seed.balanceOf(user2Address)).to.eq('0');
+	expect(await this.seed.totalSupply()).to.eq('0');
+      });
    });
    describe('Enumeration', async function() {
-      it('properly returns the correct balance of all fungible seeds', async function () {
-	expect(await this.seed.totalSupply()).to.eq('2500');
+      it('properly gives fungible seeds as rewards for newly deposited beans', async function () {
+	this.silo.connect(user).depositBeans('100000');
+	this.silo.connect(user2).depositBeans('100000');
+	expect(await this.seed.totalSupply()).to.eq('400000');
       });
-      it('properly returns the balance of all non-fungible seeds', async function () {
-	await this.silo.connect(user).depositBeans('1000');
-	await this.silo.connect(user2).depositBeans('1000');
-	expect(await this.silo.totalSeeds()).to.eq('4000');
+      it('properly returns the correct balance of non-fungible seeds', async function () {
+	expect(await this.silo.totalSeeds()).to.eq('0');
       });
    });
    describe('Transition from Silo to ERC20', async function () {
     describe('Seed Conversion', async function () {
-      it('properly converts non-fungible seeds to the ERC-20 token', async function () {
+      it('properly converts existing non-fungible seeds to the ERC-20 token', async function () {
+	await this.silo.connect(user).setNonFungibleSeeds('200000');
+	await this.silo.connect(user2).setNonFungibleSeeds('200000');
 	await this.silo.convertSeeds(userAddress);
 	await this.silo.convertSeeds(user2Address);
-	expect(await this.seed.balanceOf(userAddress)).to.eq('2000');
-	expect(await this.seed.balanceOf(user2Address)).to.eq('4500');
-	expect(await this.silo.totalSeeds()).to.eq('0');
+	expect(await this.seed.balanceOf(userAddress)).to.eq('400000');
+	expect(await this.seed.balanceOf(user2Address)).to.eq('400000');
+	await this.seed.connect(user).burn('200000');
+	await this.seed.connect(user2).burn('200000');
       });
-      it('does not create or destroy existing balance of both fungible and non-fungible seeds', async function () {
-	expect(await this.seed.totalSupply()).to.eq('6500');
-	expect(await this.silo.totalSeeds()).to.eq('0');
+      it('decrements fungible seeds when withdrawing from the silo', async function () {
+	await this.silo.connect(user).depositBeans('100000');
+	expect(await this.seed.balanceOf(userAddress)).to.eq('400000');
+	await this.silo.connect(user).withdrawBeans([1], ['100000']);
+	expect(await this.seed.balanceOf(userAddress)).to.eq('200000');
+      });
+      it('reverts when users do not have enough seeds to burn upon silo withdrawl', async function () {
+	await this.seed.connect(user2).transfer(userAddress, '200000');
+	await expect(this.silo.connect(user2).withdrawBeans([1], ['100000'])).to.be.revertedWith('ERC20: burn amount exceeds balance');
+	expect(await this.seed.balanceOf(user2Address)).to.eq('0');
+      });
+    });
+  });
+   describe('Seed Liquidity Pools', async function () {
+    describe('Adding Seeds to LP', async function () {
+      it('deposits seeds into the $SEED $ETH LP', async function () {
+      
       });
     });
   });
