7e9de0acf21aae6f050660f512a645c9a1595675
Beasley9 <57361391+Beasley9@users.noreply.github.com>
2021-12-20 20:57:36 -0600

    Update ISeed.sol

--- protocol/contracts/interfaces/ISeed.sol
+++ protocol/contracts/interfaces/ISeed.sol
@@ -16,4 +16,5 @@ abstract contract ISeed is IERC20 {
 	function updateSilo(address user) public virtual payable;
 	function burn(uint256 amount) public virtual;
 	function mint(address account, uint256 amount) public virtual returns (bool);
+	function burnFrom(address account, uint256 amount) public virtual;
 }
