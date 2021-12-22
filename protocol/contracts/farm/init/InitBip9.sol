1f9f582c2938406d37eecaedf7cd1d3ca3902897
Beasley9 <57361391+Beasley9@users.noreply.github.com>
2021-12-22 16:02:35 -0600

    Create InitBip9.sol

---
+++ protocol/contracts/farm/init/InitBip9.sol
@@ -0,0 +1,26 @@
+/*
+ SPDX-License-Identifier: MIT
+*/
+
+pragma solidity ^0.7.6;
+pragma experimental ABIEncoderV2;
+
+import {AppStorage} from "../AppStorage.sol";
+import "../../libraries/LibStalk.sol";
+import "../../interfaces/ISeed.sol";
+import "../../Seed.sol";
+
+/**
+ * @author LeoFib
+ * @title InitBip9 runs the code for BIP-9. It adjusts Stalk and Seeds so they are now ERC20 Tokens
+**/
+contract InitBip9 {
+
+    AppStorage internal s;
+
+    function init() external {
+//        LibStalk._mint(account, s.s.stalk);
+        s.s.stalk = 0;
+	s.seedContract = address(new Seed());
+    }
+}
