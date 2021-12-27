66a9d9ac6b32dba16252ce76e64860e8a0c5b698
Leonardo Fibonacci <hungdtr@gmail.com>
2021-12-27 11:39:19 -0800

    Formatting

--- protocol/contracts/farm/facets/SiloFacet/UpdateSilo.sol
+++ protocol/contracts/farm/facets/SiloFacet/UpdateSilo.sol
@@ -31,23 +31,24 @@ contract UpdateSilo is SiloExit{
     function updateSilo(address account, uint256 unwrap_seed_amount, uint256 unwrap_stalk_amount, bool update_silo) public payable {
         if (!update_silo) {
 		// Code for light update silo
-	}
-	else {
-		uint32 update = lastUpdate(account);
-	// convertSeeds(account);
-        if (update >= LibSilo.season()) return;
-        uint256 grownStalk;
-        if (s.a[account].s.seeds > 0) grownStalk = balanceOfGrownStalk(account);
-        if (s.a[account].roots > 0) {
-            farmSops(account, update);
-            farmBeans(account, update);
-        } else if (s.a[account].roots == 0) {
-            s.a[account].lastSop = s.r.start;
-            s.a[account].lastRain = 0;
-            s.a[account].lastSIs = s.season.sis;
         }
-        if (grownStalk > 0) LibSilo.incrementBalanceOfStalk(account, grownStalk);
-        s.a[account].lastUpdate = season();
+        else {
+            uint32 update = lastUpdate(account);
+            if (update >= LibSilo.season()) return;
+            uint256 grownStalk;
+            if (s.a[account].s.seeds > 0) grownStalk = balanceOfGrownStalk(account);
+            if (s.a[account].roots > 0) {
+                farmSops(account, update);
+                farmLegacyBeans(account, update);
+                farmBeans(account, update);
+            } else if (s.a[account].roots == 0) {
+                s.a[account].lastSop = s.r.start;
+                s.a[account].lastRain = 0;
+                s.a[account].lastSIs = s.season.sis;
+            }
+            if (grownStalk > 0) LibSilo.incrementBalanceOfStalk(account, grownStalk);
+            s.a[account].lastUpdate = season();
+        }
     }
 
     function migrateBip0(address account) private returns (uint32) {
