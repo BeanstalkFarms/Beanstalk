b5246c85fd0b6d6515597fb35d2674b0da166855
publius <beanstalk.publius@protonmail.com>
2022-07-13 16:08:17 -0400

    clean up

--- protocol/.solcover.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    skipFiles: [
-      'mocks/', 
-      'farm/init/',
-      'libraries/LibSafeMath32.sol',
-      'libraries/Decimal.sol',
-      'libraries/LibDiamond.sol'
-    ]
-  };
