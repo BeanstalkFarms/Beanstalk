61e9a203a7fc037644f912c51d8393dd9741a4c4
0xkokonut <0xkokonut@gmail.com>
2022-09-22 14:17:03 -0700

    update fdbdv

--- protocol/contracts/interfaces/IDelegation.sol
+++ protocol/contracts/interfaces/IDelegation.sol
@@ -3,5 +3,6 @@ pragma solidity ^0.8.0;
 pragma experimental ABIEncoderV2;
 
 interface IDelegation{
+    function clearDelegate(bytes32 _id) external;
     function setDelegate(bytes32 _id, address _delegate) external;
 }
--- protocol/contracts/tokens/FDBDV.sol
+++ protocol/contracts/tokens/FDBDV.sol
@@ -15,11 +15,7 @@ struct DepositTransfer {
     uint256[] amounts;
 }
 
-contract FDBDV is
-    UUPSUpgradeable,
-    ERC20PermitUpgradeable,
-    OwnableUpgradeable
-{
+contract FDBDV is UUPSUpgradeable, ERC20PermitUpgradeable, OwnableUpgradeable {
     using MathUpgradeable for uint256;
 
     event Deposits(
@@ -31,7 +27,7 @@ contract FDBDV is
         uint256 shares
     );
 
-     event Withdraws(
+    event Withdraws(
         address indexed account,
         DepositTransfer[] deposits,
         uint256 bdv,
@@ -40,14 +36,19 @@ contract FDBDV is
         uint256 shares
     );
 
-    address public constant BEANSTALK_ADDRESS = 0xC1E088fC1323b20BCBee9bd1B9fC9546db5624C5;
+    event AddWhitelistToken(address indexed token);
+    event RemoveWhitelistToken(address indexed token);
+
+    address public constant BEANSTALK_ADDRESS =
+        0xC1E088fC1323b20BCBee9bd1B9fC9546db5624C5;
+    uint256 private constant PRECISION = 1e18;
     mapping(address => bool) public whitelisted;
     uint256 public underlyingBdv;
 
-    function initialize(
-        string memory name,
-        string memory symbol
-    ) public initializer {
+    function initialize(string memory name, string memory symbol)
+        public
+        initializer
+    {
         __ERC20_init(name, symbol);
         __ERC20Permit_init(name);
         __Ownable_init();
@@ -57,102 +58,122 @@ contract FDBDV is
 
     function addWhitelistToken(address token) public onlyOwner {
         whitelisted[token] = true;
+        emit AddWhitelistToken(token);
     }
 
     function removeWhitelistToken(address token) public onlyOwner {
         delete whitelisted[token];
+        emit RemoveWhitelistToken(token);
     }
 
-    function setDelegate(address _delegateContract, address _delegate) external onlyOwner {
-        IDelegation(_delegateContract).setDelegate("beanstalkdao.eth", _delegate);
+    function setDelegate(
+        address _delegateContract,
+        address _delegate,
+        bytes32 snapshotId
+    ) external onlyOwner {
+        if (_delegate == address(0)) {
+            IDelegation(_delegateContract).clearDelegate(snapshotId);
+        } else {
+            IDelegation(_delegateContract).setDelegate(snapshotId, _delegate);
+        }
     }
-
-    // function createTempArrayAndConvert(uint32 season, uint256 amount) internal {
-    //     uint32[] memory tempSeasons = new uint32[](2);
-    //     uint256[] memory tempAmounts = new uint256[](2);
-    //     tempSeasons[0] = season;
-    //     tempAmounts[0] = amount;
-    //     tempSeasons[1] = depositedSiloSeason;
-    //     tempAmounts[1] = depositedSiloAmount;
-    //     convertAndUpdate(tempSeasons, tempAmounts, amount + depositedSiloAmount);
-    // }
-
-    // function convertAndUpdate(
-    //     uint32[] memory seasons,
-    //     uint256[] memory amounts,
-    //     uint256 totalAmount
-    // ) internal {
-    //     (uint32 toSeason, uint256 toAmount) = IBeanstalk(BEANSTALK_ADDRESS)
-    //         .convert(
-    //             abi.encode(
-    //                 ConvertKind.LAMBDA_LAMBDA,
-    //                 totalAmount,
-    //                 siloTokenAddress
-    //             ),
-    //             seasons,
-    //             amounts
-    //         );
-
-    //     //  Update stored season/amount
-    //     depositedSiloAmount = uint216(toAmount);
-    //     depositedSiloSeason = toSeason;
-    // }
 
     function earn() public {
-        IBeanstalk(BEANSTALK_ADDRESS).plant();
-        uint32 currentSeason = IBeanstalk(BEANSTALK_ADDRESS).season();
-        (,uint256 bdv) = IBeanstalk(BEANSTALK_ADDRESS).getDeposit(address(this), 0xBEA0000029AD1c77D3d5D23Ba2D8893dB9d1Efab, currentSeason);
-        underlyingBdv += bdv;
+        uint256 beans = IBeanstalk(BEANSTALK_ADDRESS).plant();
+        underlyingBdv += beans;
     }
 
-    function min(uint256 num1, uint256 num2, uint256 num3) internal pure returns (uint256) {
+    function min(
+        uint256 num1,
+        uint256 num2,
+        uint256 num3
+    ) internal pure returns (uint256) {
         num1 = MathUpgradeable.min(num1, num2);
         return MathUpgradeable.min(num1, num3);
     }
 
-    function max(uint256 num1, uint256 num2, uint256 num3) internal pure returns (uint256) {
+    function max(
+        uint256 num1,
+        uint256 num2,
+        uint256 num3
+    ) internal pure returns (uint256) {
         num1 = MathUpgradeable.max(num1, num2);
         return MathUpgradeable.max(num1, num3);
     }
 
-    function deposits(
-        DepositTransfer[] calldata _deposits
-    ) public virtual returns (uint256) {
-        (uint256 shares, uint256 bdv, uint256 stalk, uint256 seed) = _transferDeposits(_deposits, true);
+    function deposits(DepositTransfer[] calldata _deposits)
+        public
+        virtual
+        returns (uint256)
+    {
+        (
+            uint256 shares,
+            uint256 bdv,
+            uint256 stalk,
+            uint256 seed
+        ) = _transferDeposits(_deposits, true);
         _mint(msg.sender, shares);
         emit Deposits(msg.sender, _deposits, bdv, stalk, seed, shares);
         return shares;
     }
 
-    function withdraws(
-        DepositTransfer[] calldata _deposits
-    ) public virtual returns (uint256) {
-        (uint256 shares, uint256 bdv, uint256 stalk, uint256 seed) = _transferDeposits(_deposits, false);
+    function withdraws(DepositTransfer[] calldata _deposits)
+        public
+        virtual
+        returns (uint256)
+    {
+        (
+            uint256 shares,
+            uint256 bdv,
+            uint256 stalk,
+            uint256 seed
+        ) = _transferDeposits(_deposits, false);
         _burn(msg.sender, shares);
         emit Withdraws(msg.sender, _deposits, bdv, stalk, seed, shares);
         return shares;
     }
 
-    function _transferDeposits(DepositTransfer[] memory _deposits, bool isDeposit) internal returns (uint256 shares, uint256 bdv, uint256 stalk, uint256 seed) {
-        uint256 balanceOfSeedBefore = IBeanstalk(BEANSTALK_ADDRESS).balanceOfSeeds(address(this));
-        uint256 balanceOfStalkBefore = IBeanstalk(BEANSTALK_ADDRESS).balanceOfStalk(address(this));
+    function _transferDeposits(
+        DepositTransfer[] memory _deposits,
+        bool isDeposit
+    )
+        internal
+        returns (
+            uint256 shares,
+            uint256 bdv,
+            uint256 stalk,
+            uint256 seed
+        )
+    {
+        uint256 balanceOfSeedBefore = IBeanstalk(BEANSTALK_ADDRESS)
+            .balanceOfSeeds(address(this));
+        uint256 balanceOfStalkBefore = IBeanstalk(BEANSTALK_ADDRESS)
+            .balanceOfStalk(address(this));
 
-        for (uint256 i = 0; i < _deposits.length; i++) {
-            require(whitelisted[_deposits[i].token], "Token is not whitelisted");
-            uint256[] memory bdvs = IBeanstalk(BEANSTALK_ADDRESS).transferDeposits(
-                address(this),
-                msg.sender,
-                _deposits[i].token,
-                _deposits[i].seasons,
-                _deposits[i].amounts
+        for (uint256 i; i < _deposits.length; ++i) {
+            require(
+                whitelisted[_deposits[i].token],
+                "Token is not whitelisted"
             );
-            for (uint256 j = 0; j < bdvs.length; j++) {
+
+            uint256[] memory bdvs = IBeanstalk(BEANSTALK_ADDRESS)
+                .transferDeposits(
+                    isDeposit ? msg.sender : address(this),
+                    isDeposit ? address(this) : msg.sender,
+                    _deposits[i].token,
+                    _deposits[i].seasons,
+                    _deposits[i].amounts
+                );
+            for (uint256 j; j < bdvs.length; ++j) {
                 bdv += bdvs[j];
             }
         }
-       
-        uint256 balanceOfSeedAfter = IBeanstalk(BEANSTALK_ADDRESS).balanceOfSeeds(address(this));
-        uint256 balanceOfStalkAfter = IBeanstalk(BEANSTALK_ADDRESS).balanceOfStalk(address(this));
+
+        uint256 balanceOfSeedAfter = IBeanstalk(BEANSTALK_ADDRESS)
+            .balanceOfSeeds(address(this));
+        uint256 balanceOfStalkAfter = IBeanstalk(BEANSTALK_ADDRESS)
+            .balanceOfStalk(address(this));
+
         uint256 underlyingBdvAfter;
         if (isDeposit) {
             underlyingBdvAfter = underlyingBdv + bdv;
@@ -165,14 +186,52 @@ contract FDBDV is
         }
         uint256 supply = totalSupply();
         if (supply == 0) {
-            shares = bdv;
+            shares = stalk;
         } else if (isDeposit) {
-            shares = supply.mulDiv(min(underlyingBdvAfter.mulDiv(10**18, underlyingBdv, MathUpgradeable.Rounding.Down), balanceOfStalkAfter.mulDiv(10**18, balanceOfStalkBefore, MathUpgradeable.Rounding.Down), balanceOfSeedAfter.mulDiv(10**18, balanceOfSeedBefore, MathUpgradeable.Rounding.Down)), 10**18, MathUpgradeable.Rounding.Down);
+            shares = supply.mulDiv(
+                min(
+                    underlyingBdvAfter.mulDiv(
+                        PRECISION,
+                        underlyingBdv,
+                        MathUpgradeable.Rounding.Down
+                    ),
+                    balanceOfStalkAfter.mulDiv(
+                        PRECISION,
+                        balanceOfStalkBefore,
+                        MathUpgradeable.Rounding.Down
+                    ),
+                    balanceOfSeedAfter.mulDiv(
+                        PRECISION,
+                        balanceOfSeedBefore,
+                        MathUpgradeable.Rounding.Down
+                    )
+                ),
+                PRECISION,
+                MathUpgradeable.Rounding.Down
+            );
         } else {
-            shares = supply.mulDiv(max(underlyingBdvAfter.mulDiv(10**18, underlyingBdv, MathUpgradeable.Rounding.Up), balanceOfStalkAfter.mulDiv(10**18, balanceOfStalkBefore, MathUpgradeable.Rounding.Up), balanceOfSeedAfter.mulDiv(10**18, balanceOfSeedBefore, MathUpgradeable.Rounding.Up)), 10**18, MathUpgradeable.Rounding.Up);
+            shares = supply.mulDiv(
+                max(
+                    underlyingBdvAfter.mulDiv(
+                        PRECISION,
+                        underlyingBdv,
+                        MathUpgradeable.Rounding.Up
+                    ),
+                    balanceOfStalkAfter.mulDiv(
+                        PRECISION,
+                        balanceOfStalkBefore,
+                        MathUpgradeable.Rounding.Up
+                    ),
+                    balanceOfSeedAfter.mulDiv(
+                        PRECISION,
+                        balanceOfSeedBefore,
+                        MathUpgradeable.Rounding.Up
+                    )
+                ),
+                PRECISION,
+                MathUpgradeable.Rounding.Up
+            );
         }
-
-        require(shares > 0, "Share to mint is less than 0");
 
         underlyingBdv = underlyingBdvAfter;
     }
