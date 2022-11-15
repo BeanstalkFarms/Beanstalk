b8faecca30971060f9342bcba9be424da65cda24
0xkokonut <0xkokonut@gmail.com>
2022-11-15 09:08:59 -0800

    Revert "update root credit"
    This reverts commit 17ba20e718bb0098153c21e1b3989f38866274e4.

--- protocol/contracts/tokens/Root.sol
+++ protocol/contracts/tokens/Root.sol
@@ -85,11 +85,6 @@ contract Root is UUPSUpgradeable, ERC20PermitUpgradeable, OwnableUpgradeable {
     /// @return underlyingBdv total bdv of the silo deposit(s) in the contract
     uint256 public underlyingBdv;
 
-    /// @notice The total of outstanding loan balance
-    /// @dev only get updated on borrowLoan/repayLoan
-    /// @return outstandingLoanBalance total ROOT loan
-    uint256 public outstandingLoanBalance;
-
     /// @notice Nominated candidate to be the owner of the contract
     /// @dev The nominated candidate need to call the claimOwnership function
     /// @return ownerCandidate The nomindated candidate to become the new owner of the contract
@@ -219,16 +214,9 @@ contract Root is UUPSUpgradeable, ERC20PermitUpgradeable, OwnableUpgradeable {
         underlyingBdv += toBdv - fromBdv;
     }
 
-    /// @notice Return the total supply without outstanding loan balance
-    function totalSupplyMinusOutstandingLoanBalance() public view returns (uint256) {
-        return totalSupply() - outstandingLoanBalance;
-    }
-
     /// @notice Return the ratio of underlyingBdv per ROOT token
     function bdvPerRoot() external view returns (uint256) {
-        return
-            (underlyingBdv * PRECISION) /
-            totalSupplyMinusOutstandingLoanBalance();
+        return (underlyingBdv * PRECISION) / totalSupply();
     }
 
     /// @notice Call plant function on Beanstalk
@@ -510,7 +498,7 @@ contract Root is UUPSUpgradeable, ERC20PermitUpgradeable, OwnableUpgradeable {
             stalk = balanceOfStalkBefore - balanceOfStalkAfter;
             seeds = balanceOfSeedsBefore - balanceOfSeedsAfter;
         }
-        uint256 supply = totalSupplyMinusOutstandingLoanBalance();
+        uint256 supply = totalSupply();
         if (supply == 0) {
             shares = stalk * 1e8; // Stalk is 1e10 so we want to initialize the initial supply to 1e18
         } else if (isDeposit) {
@@ -578,21 +566,5 @@ contract Root is UUPSUpgradeable, ERC20PermitUpgradeable, OwnableUpgradeable {
             depositTransfer.seasons,
             depositTransfer.amounts
         );
-    }
-
-    /// @notice Borrow ROOT as a loan
-    /// @param amount Amount of Root to loan out
-    function borrowLoan(uint256 amount) external onlyOwner {
-        require(amount > 0, "Borrow amount must be greater than 0");
-        outstandingLoanBalance += amount;
-        _mint(msg.sender, amount);
-    }
-
-    /// @notice Repay loan
-    /// @param amount Amount of Root to loan out
-    function repayLoan(uint256 amount) external onlyOwner {
-        require(amount > 0, "Repay amount must be greater than 0");
-        _burn(msg.sender, amount);
-        outstandingLoanBalance -= amount;
     }
 }
