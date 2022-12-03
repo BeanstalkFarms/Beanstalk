63caa992d2a349f8f35147f0be3d2b7d6c934a4b
Silo Chad <silochad@protonmail.com>
2022-12-02 19:17:32 -0600

    update imports

--- protocol/contracts/mocks/mockFacets/MockFieldFacet.sol
+++ protocol/contracts/mocks/mockFacets/MockFieldFacet.sol
@@ -6,7 +6,7 @@ pragma solidity =0.7.6;
 pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
-import "../../beanstalk/facets/FieldFacet.sol";
+import "~/beanstalk/field/FieldFacet.sol";
 
 /**
  * @author Publius
