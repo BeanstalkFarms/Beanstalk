271b697cb83124120ed83a186dfb7a4bbf18b2ac
Brian <Brian.olympus33@gmail.com>
2022-12-13 00:40:25 -0600

    🐞 re-added mock uniswap contracts in foundry tests

--- protocol/test/foundry/utils/InitDiamondDeployer.sol
+++ protocol/test/foundry/utils/InitDiamondDeployer.sol
@@ -49,6 +49,8 @@ import {MockFertilizerFacet} from "~/mocks/mockFacets/MockFertilizerFacet.sol";
 import {MockToken} from "~/mocks/MockToken.sol";
 import {MockUnripeFacet} from "~/mocks/mockFacets/MockUnripeFacet.sol";
 import {Mock3Curve} from "~/mocks/curve/Mock3Curve.sol";
+import {MockUniswapV3Pool} from "~/mocks/uniswap/MockUniswapV3Pool.sol";
+import {MockUniswapV3Factory} from "~/mocks/uniswap/MockUniswapV3Factory.sol";
 import {MockCurveFactory} from "~/mocks/curve/MockCurveFactory.sol";
 import {MockCurveZap} from "~/mocks/curve/MockCurveZap.sol";
 import {MockMeta3Curve} from "~/mocks/curve/MockMeta3Curve.sol";
@@ -153,6 +155,7 @@ abstract contract InitDiamondDeployer is Test {
     //_mockCurveMetapool();
     _mockUnripe();
     //_mockFertilizer();
+    _mockUniswap();
 
     // create diamond    
     d = new Diamond(deployer);
@@ -234,6 +237,21 @@ abstract contract InitDiamondDeployer is Test {
     //
     MockCurveZap curveZap = MockCurveZap(_etch("MockCurveZap.sol", C.curveZapAddress()));
     curveZap.approve();
+  }
+  
+  function _mockUniswap() internal {
+    //address UNIV3_FACTORY = 0x1F98431c8aD98523631AE4a59f267346ea31F984; 
+    MockUniswapV3Factory uniFactory = MockUniswapV3Factory(new MockUniswapV3Factory());
+    address ethUsdc = 
+      uniFactory.createPool(
+        0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2,//weth
+        address(C.usdc()),//usdc
+        3000
+      );
+    bytes memory code = at(ethUsdc);
+    address targetAddr = C.UniV3EthUsdc();
+    vm.etch(targetAddr, code);
+    MockUniswapV3Pool(C.UniV3EthUsdc()).setOraclePrice(1000e6,18);
   }
 
   function _mockCurveMetapool() internal {
