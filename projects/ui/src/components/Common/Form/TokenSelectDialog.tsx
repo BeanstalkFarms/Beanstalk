0d78e770a8c48de42f85a57409fe21e612dd9850
Bean-Sama <beansama@bean.farm>
2023-01-14 17:37:24 -0800

    wip: token select dialog changes

--- projects/ui/src/components/Common/Form/TokenSelectDialog.tsx
+++ projects/ui/src/components/Common/Form/TokenSelectDialog.tsx
@@ -1,14 +1,18 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 
-import { Button, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography, Link } from '@mui/material';
+import { Button, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography, Link, Box, Stack, Tooltip } from '@mui/material';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 import { StyledDialog, StyledDialogActions, StyledDialogContent, StyledDialogTitle } from '~/components/Common/Dialog';
 import Token from '~/classes/Token';
 import { displayBN } from '~/util';
 import { ZERO_BN } from '~/constants';
-import { FarmerBalances } from '~/state/farmer/balances';
+import { ApplicableBalance, FarmerBalances } from '~/state/farmer/balances';
 import { FarmerSilo } from '~/state/farmer/silo';
 import { BeanstalkPalette, FontSize, IconSize } from '../../App/muiTheme';
 import Row from '~/components/Common/Row';
+import BalanceFromRow, { BalanceFrom } from './BalanceFromRow';
+import { ETH } from '~/constants/tokens';
+import useGetChainToken from '~/hooks/chain/useGetChainToken';
 
 export enum TokenSelectMode { MULTI, SINGLE }
 
@@ -31,7 +35,10 @@ export type TokenSelectDialogProps<K extends keyof TokenBalanceMode> = {
   title?: string | JSX.Element;
   /** */
   description?: string | JSX.Element;
-
+  /** the balance (circulating |farm | combined) being used */
+  balanceFrom?: BalanceFrom;
+  /** set the balance (circulating |farm | combined) to use */
+  setBalanceFrom?: (v: BalanceFrom) => void;
   /**
    * 
    */
@@ -45,9 +52,17 @@ export type TokenSelectDialogProps<K extends keyof TokenBalanceMode> = {
   tokenList: Token[];
   /** Single or multi-select */
   mode?: TokenSelectMode;
+  /** maping of additional balances to show along with balances */
+  applicableBalances?: Record<string, ApplicableBalance>;
 }
 
 type TokenSelectDialogC = React.FC<TokenSelectDialogProps<keyof TokenBalanceMode>>;
+
+const balanceFromText = {
+  [BalanceFrom.INTERNAL]: 'Displaying Farm Balances.',
+  [BalanceFrom.EXTERNAL]: 'Displaying Circulating Balances.',
+  [BalanceFrom.TOTAL]: 'Displaying Total Farm and Circulating Balances.',
+};
 
 const TokenSelectDialog : TokenSelectDialogC = React.memo(({
   // Dialog
@@ -57,21 +72,35 @@ const TokenSelectDialog : TokenSelectDialogC = React.memo(({
   handleSubmit,
   title,
   description,
+  balanceFrom = BalanceFrom.TOTAL,
+  setBalanceFrom,
   // Balances
   balancesType = 'farm',
   balances: _balances,
   // Tokens
   tokenList,
   mode = TokenSelectMode.MULTI,
+  applicableBalances,
 }) => {
+  /// Chain Constants
+  const getChainToken = useGetChainToken();
+  const Eth           = getChainToken(ETH);
+
   /** keep an internal copy of selected tokens */
   const [selectedInternal, setSelectedInternal] = useState<Set<Token>>(new Set<Token>());
 
   const getBalance = useCallback((addr: string) => {
     if (!_balances) return ZERO_BN;
-    if (balancesType === 'farm') return (_balances as TokenBalanceMode['farm'])?.[addr]?.total || ZERO_BN;
+    if (balancesType === 'farm') return (_balances as TokenBalanceMode['farm'])?.[addr]?.[balanceFrom] || ZERO_BN;
     return (_balances as TokenBalanceMode['silo-deposits'])?.[addr]?.deposited?.amount || ZERO_BN;
-  }, [_balances, balancesType]);
+  }, [_balances, balancesType, balanceFrom]);
+
+  const getApplicableBalances = useCallback((addr: string) => {
+    if (!applicableBalances || !(addr in applicableBalances)) return undefined;
+    const applied = applicableBalances[addr].applied;
+    const remaining = applicableBalances[addr].remaining;
+    return { applied, remaining };
+  }, [applicableBalances]);
 
   // Toggle the selection state of a token.
   const toggle = useCallback((token: Token) => {
@@ -84,6 +113,13 @@ const TokenSelectDialog : TokenSelectDialogC = React.memo(({
       setSelectedInternal(copy);
     }
   }, [selectedInternal]);
+
+  const filteredTokenList = useMemo(() => {
+    if (balanceFrom === BalanceFrom.INTERNAL) {
+      return tokenList.filter((tk) => tk !== Eth);
+    }
+    return tokenList;
+  }, [Eth, balanceFrom, tokenList]);
 
   // Whenever the Dialog opens, store a temporary copy of the currently
   // selected tokens so we can manipulate them quickly here without
@@ -131,67 +167,113 @@ const TokenSelectDialog : TokenSelectDialogC = React.memo(({
       </StyledDialogTitle>
       <StyledDialogContent sx={{ pb: mode === TokenSelectMode.MULTI ? 0 : 1, pt: 0 }}>
         {/**
+         * Balance From
+         */}
+        {setBalanceFrom ? (
+          <Stack pt={1.5} pb={2}>
+            <BalanceFromRow 
+              balanceFrom={balanceFrom} 
+              setBalanceFrom={setBalanceFrom} 
+            />
+            <Box 
+              pt={2}
+              sx={{ borderBottom: '0.5px solid', borderColor: 'text.light', width: '100%' }} 
+            />
+          </Stack>) : null}
+        {/**
           * Tokens
           */}
         <List sx={{ p: 0 }}>
-          {tokenList ? tokenList.map((_token) => (
-            <ListItem
-              key={_token.address}
-              color="primary"
-              // selected={selectedInternal.has(_token)}
-              disablePadding
-              onClick={onClickItem(_token)}
-              sx={{
-                // ListItem is used elsewhere so we define here
-                // instead of in muiTheme.ts
-                '& .MuiListItemText-primary': {
-                  fontSize: FontSize['1xl'],
-                  lineHeight: '1.875rem'
-                },
-                '& .MuiListItemText-secondary': {
-                  fontSize: FontSize.base,
-                  lineHeight: '1.25rem',
-                  color: BeanstalkPalette.lightGrey
-                }
-              }}
-            >
-              <ListItemButton disableRipple selected={selectedInternal.has(_token)}>
-                {/* Top-level button stack */}
-                <Row justifyContent="space-between" sx={{ width: '100%' }}>
-                  {/* Icon & text left side */}
-                  <Row justifyContent="center" gap={0}>
-                    <ListItemIcon>
-                      <img
-                        src={_token.logo}
-                        alt=""
-                        css={{ 
+          {filteredTokenList ? filteredTokenList.map((_token) => {
+            const tokenBalance = getBalance(_token.address);
+            const applicableBalance = getApplicableBalances(_token.address);
+            return (
+              <ListItem
+                key={_token.address}
+                color="primary"
+                // selected={selectedInternal.has(_token)}
+                disablePadding
+                onClick={onClickItem(_token)}
+                sx={{
+                  // ListItem is used elsewhere so we define here
+                  // instead of in muiTheme.ts
+                  '& .MuiListItemText-primary': {
+                    fontSize: FontSize['1xl'],
+                    lineHeight: '1.875rem'
+                  },
+                  '& .MuiListItemText-secondary': {
+                    fontSize: FontSize.base,
+                    lineHeight: '1.25rem',
+                    color: BeanstalkPalette.lightGrey
+                  }
+                }}
+              >
+                <ListItemButton disableRipple selected={selectedInternal.has(_token)}>
+                  {/* Top-level button stack */}
+                  <Row justifyContent="space-between" sx={{ width: '100%' }}>
+                    {/* Icon & text left side */}
+                    <Row justifyContent="center" gap={0}>
+                      <ListItemIcon>
+                        <img
+                          src={_token.logo}
+                          alt=""
+                          css={{ 
                           width: IconSize.tokenSelect,
                           height: IconSize.tokenSelect
                         }}
                       />
-                    </ListItemIcon>
-                    <ListItemText
-                      primary={_token.symbol}
-                      secondary={_token.name}
-                      sx={{ my: 0 }}
+                      </ListItemIcon>
+                      <ListItemText
+                        primary={_token.symbol}
+                        secondary={_token.name}
+                        sx={{ my: 0 }}
                     />
-                  </Row>
-                  {/* Balances right side */}
-                  {_balances ? (
-                    <Typography variant="bodyLarge">
-                      {displayBN(getBalance(_token.address))}
-                    </Typography>
+                    </Row>
+                    {/* Balances right side */}
+                    {_balances ? (
+                      <Typography variant="bodyLarge">
+                        {/* Token balance */}
+                        {displayBN(tokenBalance)}
+                        {/* additionally applied balance */}
+                        {applicableBalance?.applied.gt(0) ? (
+                          <Typography variant="inherit" color="primary" component="span">
+                            &nbsp; + {displayBN(applicableBalance.applied)}
+                          </Typography>
+                        ) : null}
+                        {/* remaining applicable balance */}
+                        {applicableBalance?.remaining.gt(0) ? (
+                          <Typography variant="inherit" color="text.tertiary" component="span">
+                            &nbsp; + {displayBN(applicableBalance.remaining)}
+                          </Typography>
+                        ) : null}
+                        {applicableBalance?.applied.gt(0) || applicableBalance?.remaining.gt(0) ? (
+                          <Tooltip
+                            title="tooltip data goes here" // TODO FIX ME
+                            placement="right"
+                          >
+                            <HelpOutlineIcon
+                              sx={{
+                                color: 'text.secondary',
+                                display: 'inline',
+                                mb: 0.5,
+                                fontSize: '12px',
+                              }}
+                            />
+                          </Tooltip>
+                        ) : null}
+                      </Typography>
                   ) : null}
-                </Row>
-              </ListItemButton>
-            </ListItem>
-          )) : null}
+                  </Row>
+                </ListItemButton>
+              </ListItem>
+            ); 
+          }) : null}
           {/**
             * Farm + Circulating Balances notification
             */}
           {_balances ? (
             <Typography ml={1} pt={0.5} textAlign="center" fontSize={FontSize.sm} color="gray">
-              Displaying total Farm and Circulating balances.&nbsp;
+              {balanceFromText[balanceFrom]}&nbsp;
               <Link href="https://docs.bean.money/almanac/protocol/asset-states" target="_blank" rel="noreferrer" underline="none">
                 Learn more &rarr;
               </Link>
