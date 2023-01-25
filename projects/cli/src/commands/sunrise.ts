e68cbc68d86804fabe2a60c884cbc600f5aea4d4
ALECKS <0xalecks@gmail.com>
2023-01-25 08:34:43 -0500

    cli: add sunrise

---
+++ projects/cli/src/commands/sunrise.ts
@@ -0,0 +1,27 @@
+import { BeanstalkSDK, Token } from "@beanstalk/sdk";
+import chalk from "chalk";
+
+export const sunrise = async (sdk, chain, {}) => {
+  const localSeason = await sdk.contracts.beanstalk.season();
+  const seasonTime = await sdk.contracts.beanstalk.seasonTime();
+
+  if (localSeason === seasonTime) {
+    console.log(`No need, ${chalk.bold.yellowBright(localSeason)} is the current season.`);
+    process.exit(0);
+  }
+
+  const diff = seasonTime - localSeason;
+
+  callSunrise(sdk);
+};
+
+async function callSunrise(sdk: BeanstalkSDK) {
+  try {
+    const res = await sdk.contracts.beanstalk.sunrise();
+    await res.wait();
+    const season = await sdk.contracts.beanstalk.season();
+    console.log(`${chalk.bold.greenBright("sunrise()")} called. New season is ${chalk.bold.yellowBright(season)}`);
+  } catch (err: any) {
+    console.log(err);
+  }
+}
