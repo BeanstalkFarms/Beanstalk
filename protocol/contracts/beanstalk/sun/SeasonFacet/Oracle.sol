2114fa16eac50ca8cd7e21dac00c763697815bd5
Silo Chad <silochad@protonmail.com>
2023-02-01 22:31:05 -0600

    Oracle(!): remove unused MetapoolOracle event

--- protocol/contracts/beanstalk/sun/SeasonFacet/Oracle.sol
+++ protocol/contracts/beanstalk/sun/SeasonFacet/Oracle.sol
@@ -1,6 +1,4 @@
-/**
- * SPDX-License-Identifier: MIT
- */
+// SPDX-License-Identifier: MIT
 
 pragma solidity =0.7.6;
 pragma experimental ABIEncoderV2;
@@ -10,20 +8,29 @@ import "~/beanstalk/ReentrancyGuard.sol";
 
 /**
  * @title Oracle
- * @notice Tracks the Delta B across the Uniswap and Curve Liquidity pools
+ * @notice Tracks the Delta B in available pools.
  * @author Publius, Chaikitty
  */
 contract Oracle is ReentrancyGuard {
-    event MetapoolOracle(uint32 indexed season, int256 deltaB, uint256[2] balances);
+    
+    //////////////////// ORACLE GETTERS ////////////////////
 
+    /**
+     * @notice Returns the current Delta B in the Curve liquidity pool.
+     */
     function totalDeltaB() external view returns (int256 deltaB) {
         deltaB = LibCurveOracle.check();
     }
 
+    /**
+     * @notice Returns the current Delta B for the requested pool.
+     */
     function poolDeltaB(address pool) external view returns (int256 deltaB) {
         if (pool == C.curveMetapoolAddress()) return LibCurveOracle.check();
         require(false, "Oracle: Pool not supported");
     }
+
+    //////////////////// ORACLE INTERNAL ////////////////////
 
     function stepOracle() internal returns (int256 deltaB, uint256[2] memory balances) {
         (deltaB, balances) = LibCurveOracle.capture();
