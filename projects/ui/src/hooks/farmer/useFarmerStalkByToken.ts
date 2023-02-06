8883c9f8de41e9eabf7e61eb36d040682bcf8ca9
uncoolzero <107518216+uncoolzero@users.noreply.github.com>
2023-02-06 12:09:53 -0300

    Balances page: fixed per-token stalk calculation

--- projects/ui/src/hooks/farmer/useFarmerStalkByToken.ts
+++ projects/ui/src/hooks/farmer/useFarmerStalkByToken.ts
@@ -8,10 +8,13 @@ import { STALK_PER_SEED_PER_SEASON } from '~/util';
 type BaseToGrownStalk = {
   base: BigNumber;
   grown: BigNumber;
+  seeds: BigNumber;
+  unclaimed: BigNumber;
 };
 
 export default function useFarmerStalkByToken() {
   const balances = useFarmerSiloBalances();
+  console.log("BALANCES: ", balances)
   const season = useSeason();
 
   return useMemo(
@@ -23,15 +26,18 @@ export default function useFarmerStalkByToken() {
             tokenBalances.deposited.crates.reduce<BaseToGrownStalk>(
               (acc, crate) => {
                 const elapsedSeasons = season.minus(crate.season);
+                const seasonsSinceUpdate = season.minus(tokenBalances.lastUpdate)
                 // add base stalk added from deposits
                 acc.base = acc.base.plus(crate.stalk);
                 // add grown stalk from deposits
-                acc.grown = acc.grown.plus(
-                  crate.seeds.times(elapsedSeasons).times(STALK_PER_SEED_PER_SEASON)
-                );
+                acc.grown = acc.grown.plus(crate.seeds.times(elapsedSeasons).times(STALK_PER_SEED_PER_SEASON));
+                // total seeds
+                acc.seeds = acc.seeds.plus(crate.seeds);
+                // grown stalk since last silo update (unclaimed stalks)
+                acc.unclaimed =  acc.seeds.times(seasonsSinceUpdate).times(STALK_PER_SEED_PER_SEASON)
                 return acc;
               },
-              { base: ZERO_BN, grown: ZERO_BN }
+              { base: ZERO_BN, grown: ZERO_BN, unclaimed: ZERO_BN, seeds: ZERO_BN }
             );
           return prev;
         },
