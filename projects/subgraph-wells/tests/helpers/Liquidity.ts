cfb366dfe5625c28bcfa99336a1899253832a370
Cujowolf <cujowolf12@gmail.com>
2023-02-06 15:10:18 -0600

    RemoveLiquidity event handler and tests

--- projects/subgraph-wells/tests/helpers/Liquidity.ts
+++ projects/subgraph-wells/tests/helpers/Liquidity.ts
@@ -1,9 +1,24 @@
-import { handleAddLiquidity } from "../../src/WellHandler"
+import { Deposit, Withdraw } from "../../generated/schema"
+import { handleAddLiquidity, handleRemoveLiquidity } from "../../src/WellHandler"
 import { BEAN_SWAP_AMOUNT, SWAP_ACCOUNT, WELL, WELL_LP_AMOUNT, WETH_SWAP_AMOUNT } from "./Constants"
-import { createAddLiquidityEvent } from "./Well"
+import { createAddLiquidityEvent, createRemoveLiquidityEvent } from "./Well"
 
 export function createDefaultAddLiquidity(): string {
     let newEvent = createAddLiquidityEvent(WELL, SWAP_ACCOUNT, WELL_LP_AMOUNT, [BEAN_SWAP_AMOUNT, WETH_SWAP_AMOUNT])
     handleAddLiquidity(newEvent)
     return newEvent.transaction.hash.toHexString() + '-' + newEvent.logIndex.toString()
+}
+
+export function createDefaultRemoveLiquidity(): string {
+    let newEvent = createRemoveLiquidityEvent(WELL, SWAP_ACCOUNT, WELL_LP_AMOUNT, [BEAN_SWAP_AMOUNT, WETH_SWAP_AMOUNT])
+    handleRemoveLiquidity(newEvent)
+    return newEvent.transaction.hash.toHexString() + '-' + newEvent.logIndex.toString()
+}
+
+export function loadDeposit(id: string): Deposit {
+    return Deposit.load(id) as Deposit
+}
+
+export function loadWithdraw(id: string): Withdraw {
+    return Withdraw.load(id) as Withdraw
 }
