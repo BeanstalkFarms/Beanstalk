0c47a0e6fe2eeea46616c49fa0919d6d1527a5cb
Cujowolf <cujowolf12@gmail.com>
2023-02-06 15:52:44 -0600

    RemoveLiquidityOne handlers and tests

--- projects/subgraph-wells/tests/helpers/Liquidity.ts
+++ projects/subgraph-wells/tests/helpers/Liquidity.ts
@@ -1,7 +1,8 @@
 import { Deposit, Withdraw } from "../../generated/schema"
-import { handleAddLiquidity, handleRemoveLiquidity } from "../../src/WellHandler"
+import { BEAN_ERC20, WETH } from "../../src/utils/Constants"
+import { handleAddLiquidity, handleRemoveLiquidity, handleRemoveLiquidityOneToken } from "../../src/WellHandler"
 import { BEAN_SWAP_AMOUNT, SWAP_ACCOUNT, WELL, WELL_LP_AMOUNT, WETH_SWAP_AMOUNT } from "./Constants"
-import { createAddLiquidityEvent, createRemoveLiquidityEvent } from "./Well"
+import { createAddLiquidityEvent, createRemoveLiquidityEvent, createRemoveLiquidityOneTokenEvent } from "./Well"
 
 export function createDefaultAddLiquidity(): string {
     let newEvent = createAddLiquidityEvent(WELL, SWAP_ACCOUNT, WELL_LP_AMOUNT, [BEAN_SWAP_AMOUNT, WETH_SWAP_AMOUNT])
@@ -12,6 +13,18 @@ export function createDefaultAddLiquidity(): string {
 export function createDefaultRemoveLiquidity(): string {
     let newEvent = createRemoveLiquidityEvent(WELL, SWAP_ACCOUNT, WELL_LP_AMOUNT, [BEAN_SWAP_AMOUNT, WETH_SWAP_AMOUNT])
     handleRemoveLiquidity(newEvent)
+    return newEvent.transaction.hash.toHexString() + '-' + newEvent.logIndex.toString()
+}
+
+export function createRemoveLiquidityOneBean(): string {
+    let newEvent = createRemoveLiquidityOneTokenEvent(WELL, SWAP_ACCOUNT, WELL_LP_AMOUNT, BEAN_ERC20, BEAN_SWAP_AMOUNT)
+    handleRemoveLiquidityOneToken(newEvent)
+    return newEvent.transaction.hash.toHexString() + '-' + newEvent.logIndex.toString()
+}
+
+export function createRemoveLiquidityOneWeth(): string {
+    let newEvent = createRemoveLiquidityOneTokenEvent(WELL, SWAP_ACCOUNT, WELL_LP_AMOUNT, WETH, WETH_SWAP_AMOUNT)
+    handleRemoveLiquidityOneToken(newEvent)
     return newEvent.transaction.hash.toHexString() + '-' + newEvent.logIndex.toString()
 }
 
