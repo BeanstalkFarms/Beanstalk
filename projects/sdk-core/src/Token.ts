8bf90d23c10498b102a533d69056df455c1ac78c
ALECKS <0xalecks@gmail.com>
2023-02-07 12:36:03 -0500

    sdk-core: create sdk-core project

---
+++ projects/sdk-core/src/Token.ts
@@ -0,0 +1,1 @@
+export { Token } from "src/classes/Token";
---
+++ projects/sdk-core/src/index.ts
@@ -0,0 +1,4 @@
+export { Token, NativeToken, ERC20Token } from "src/classes/Token";
+export { TokenValue } from "src/classes/TokenValue";
+export { DecimalBigNumber } from "src/classes/DecimalBigNumber";
+export { Address } from "src/classes/Address";
