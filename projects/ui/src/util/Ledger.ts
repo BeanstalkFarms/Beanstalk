c8197497236bc392a00bcfa0bf22051016020900
uncoolzero <107518216+uncoolzero@users.noreply.github.com>
2023-02-08 18:09:22 -0300

    First pass at more robust error handling
    Added all ethersjs error codes

--- projects/ui/src/util/Ledger.ts
+++ projects/ui/src/util/Ledger.ts
@@ -39,6 +39,7 @@ export const tokenResult = (_token: Token | ChainConstant<Token>) => {
  * @FIXME improve parsing
  */
 export const parseError = (error: any) => {
+
   switch (error.code) {
     /// ethers
     case 'UNSUPPORTED_OPERATION':
@@ -60,8 +61,184 @@ export const parseError = (error: any) => {
 
     /// Unknown
     default:
-      if (error?.message) return `${error?.message || error?.toString()}.${error?.code ? ` (code=${error?.code})` : ''}`;    
-      return `An unknown error occurred.${error?.code ? ` (code=${error?.code})` : ''}`;
+
+      const errorString = error.toString()
+
+      ///////////////////
+      // Blockchain Errors
+
+      // Call exception
+      //  - transaction: the transaction
+      //  - address?: the contract address
+      //  - args?: The arguments passed into the function
+      //  - method?: The Solidity method signature
+      //  - errorSignature?: The EIP848 error signature
+      //  - errorArgs?: The EIP848 error parameters
+      //  - reason: The reason (only for EIP848 "Error(string)")
+      if (errorString.includes("CALL_EXCEPTION")) {
+        if (error.reason)
+        {
+          return `Call Exception: ${error.reason}`;
+        }
+        return "Call exception."
+      }
+
+      // Insufficient funds (< value + gasLimit * gasPrice)
+      //   - transaction: the transaction attempted
+      if (errorString.includes("INSUFFICIENT_FUNDS")) {
+        return "Insufficient funds to complete the transaction."
+      }
+
+      // Nonce has already been used
+      //   - transaction: the transaction attempted
+      if (errorString.includes("NONCE_EXPIRED")) {
+        return "Error: Nonce has already been used.";
+      }
+
+      // The replacement fee for the transaction is too low
+      //   - transaction: the transaction attempted
+      if (errorString.includes("REPLACEMENT_UNDERPRICED")) {
+        return "Replacement fee for transaction is too low."
+      }
+
+      // The gas limit could not be estimated
+      //   - transaction: the transaction passed to estimateGas
+      if (errorString.includes("UNPREDICTABLE_GAS_LIMIT")) {
+        if (error.reason)
+        {
+          return `Unpredictable Gas Limit: ${error.reason}`;
+        }
+        return "Unpredictable gas limit."
+      }
+
+      // The transaction was replaced by one with a higher gas price
+      //   - reason: "cancelled", "replaced" or "repriced"
+      //   - cancelled: true if reason == "cancelled" or reason == "replaced")
+      //   - hash: original transaction hash
+      //   - replacement: the full TransactionsResponse for the replacement
+      //   - receipt: the receipt of the replacement
+      if (errorString.includes("TRANSACTION_REPLACED")) {
+        if (error.reason == "cancelled") {
+          return "Transaction cancelled."
+        }
+        if (error.reason == "replaced") {
+          return "Transaction replaced by one with a higher gas price."
+        }
+        if (error.reason == "repriced") {
+          return "Transaction repriced."
+        }
+        return "Transaction replaced by one with a higher gas price."
+      }
+
+      ///////////////////
+      // Interaction Errors
+
+      // The user rejected the action, such as signing a message or sending
+      // a transaction
+      if (errorString.includes("ACTION_REJECTED")) {
+        return "You rejected the signature request.";
+      }
+
+      if (errorString.includes("TRANSACTION_RAN_OUT_OF_GAS")) {
+        return "Transaction ran out of gas.";
+      }
+
+      if (errorString.includes("TRANSACTION_UNDERPRICED")) {
+        return "Transaction underpriced.";
+      }
+
+      if (errorString.includes("REJECTED_TRANSACTION")) {
+        return "Transaction rejected.";
+      }
+
+      if (errorString.includes("CALL_REVERTED")) {
+        return "Call reverted.";
+      }
+
+      if (errorString.includes("execution reverted")) {
+        return "Execution reverted.";
+      }
+
+      if (errorString.includes("NONCE_TOO_LOW")) {
+        return "Nonce too low.";
+      }
+
+      if (errorString.includes("INSUFFICIENT_FUNDS_FOR_GAS")) {
+        return "Insufficient funds for gas.";
+      }
+
+
+      ///////////////////
+      // "[ethjs-query] while formatting outputs from RPC" errors
+
+      if (errorString.includes("gas required exceeds allowance")) {
+        return "Gas required exceeds allowance.";
+      }
+
+      if (errorString.includes("max priority fee per gas higher than max fee per gas")) {
+        return "Max priority fee per gas higher than max fee per gas.";
+      }
+
+      if (errorString.includes("max fee per gas less than block base fee")) {
+        return "Max fee per gas lower than block base fee.";
+      }
+
+      ///////////////////
+      // Operational Errors
+
+      // Buffer Overrun
+      if (errorString.includes("BUFFER_OVERRUN")) {
+        return "Buffer overrun."
+      }
+
+      // Numeric Fault
+      //   - operation: the operation being executed
+      //   - fault: the reason this faulted
+      if (errorString.includes("NUMERIC_FAULT")) {
+        return "Numeric fault."
+      }
+
+      
+      ///////////////////
+      // Generic Errors
+
+      // Unknown Error
+      if (errorString.includes("UNKNOWN_ERROR")) {
+        return "Unknown error.";
+      }
+
+      // Not Implemented
+      if (errorString.includes("NOT_IMPLEMENTED")) {
+        return "Not implemented."
+      }
+
+      // Unsupported Operation
+      //   - operation
+      if (errorString.includes("UNSUPPORTED_OPERATION")) {
+        if (error.reason)
+        {
+          return `Unsupported Operation: ${error.reason}`
+        }
+        return "Unsupported operation."
+      }
+
+      // Network Error (i.e. Ethereum Network, such as an invalid chain ID)
+      //   - event ("noNetwork" is not re-thrown in provider.ready; otherwise thrown)
+      if (errorString.includes("NETWORK_ERROR")) {
+        return "Network error."
+      }
+
+      // Some sort of bad response from the server
+      if (errorString.includes("SERVER_ERROR")) {
+        return "Server error."
+      }
+
+      // Timeout
+      if (errorString.includes("TIMEOUT")) {
+        return "Operation timed out."
+      }
+
+      return "Unhandled error."
   }
 };
 
