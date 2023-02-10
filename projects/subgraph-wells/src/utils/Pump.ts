3f2f6c79f379de24b43bbd8b98a7a11d880151eb
Cujowolf <cujowolf12@gmail.com>
2023-02-10 12:17:42 -0600

    Handler and test updates from schema review

--- projects/subgraph-wells/src/utils/Pump.ts
+++ projects/subgraph-wells/src/utils/Pump.ts
@@ -1,11 +1,14 @@
 import { Address } from "@graphprotocol/graph-ts";
+import { BoreWellPumpsStruct } from "../../generated/Aquifer/Aquifer";
 import { Pump } from "../../generated/schema";
 
-export function loadOrCreatePump(pumpAddress: Address, wellAddress: Address): Pump {
-    let id = pumpAddress.toHexString() + '-' + wellAddress.toHexString()
+export function loadOrCreatePump(pumpData: BoreWellPumpsStruct, wellAddress: Address): Pump {
+    let id = pumpData[0].toAddress().toHexString() + '-' + wellAddress.toHexString()
     let pump = Pump.load(id)
     if (pump == null) {
         pump = new Pump(id)
+        pump.target = pumpData.target
+        pump.data = pumpData.data
         pump.well = wellAddress
         pump.save()
     }
