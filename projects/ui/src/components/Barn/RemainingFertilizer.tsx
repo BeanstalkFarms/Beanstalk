f9ccf8332214b6e2bb014beaaec5bf26d3811996
Bean-Sama <beansama@bean.farm>
2023-02-09 20:51:23 -0800

    wip: FertilizerAPYChip component + add to Barn page

--- projects/ui/src/components/Barn/RemainingFertilizer.tsx
+++ projects/ui/src/components/Barn/RemainingFertilizer.tsx
@@ -12,17 +12,23 @@ import { FontSize } from '../App/muiTheme';
 import Row from '~/components/Common/Row';
 
 import { FC } from '~/types';
+import FertilizerAPYChip from './FertilizerAPYChip';
 
 const RemainingFertilizer: FC<{}> = () => {
   // eslint-disable-next-line unused-imports/no-unused-vars
   const [humidity, nextDecreaseAmount] = useHumidity();
-  const { recapFundedPct, remaining } = useSelector<AppState, AppState['_beanstalk']['barn']>((state) => state._beanstalk.barn);
+  const { recapFundedPct, remaining } = useSelector<
+    AppState,
+    AppState['_beanstalk']['barn']
+  >((state) => state._beanstalk.barn);
   const season = useSeason();
-  
+
   // eslint-disable-next-line unused-imports/no-unused-vars
-  const nextDecreaseTimeString = season.eq(6074)
-    ? 'per Season upon Unpause'
-    :  <SunriseCountdown />;
+  const nextDecreaseTimeString = season.eq(6074) ? (
+    'per Season upon Unpause'
+  ) : (
+    <SunriseCountdown />
+  );
 
   return (
     <Card sx={{ p: 2 }}>
@@ -35,8 +41,16 @@ const RemainingFertilizer: FC<{}> = () => {
           gap={3}
         >
           {/* left column */}
-          <Box sx={{ width: 130, display: { xs: 'none', md: 'block' }, aspectRatio: '1/1' }}>
-            <FertilizerImage progress={Math.max(recapFundedPct.toNumber(), 0.05)} />
+          <Box
+            sx={{
+              width: 130,
+              display: { xs: 'none', md: 'block' },
+              aspectRatio: '1/1',
+            }}
+          >
+            <FertilizerImage
+              progress={Math.max(recapFundedPct.toNumber(), 0.05)}
+            />
           </Box>
           {/* right column */}
           <Stack justifyContent="space-between" gap={2}>
@@ -88,6 +102,7 @@ const RemainingFertilizer: FC<{}> = () => {
                 <Typography variant="bodyLarge">
                   {displayFullBN(humidity.multipliedBy(100))}%
                 </Typography>
+                <FertilizerAPYChip />
               </Row>
             </Stack>
           </Stack>
