7d08b521edb32be88e8f255c9022cb81f5de2225
Cujowolf <cujowolf12@gmail.com>
2023-02-12 13:39:21 -0600

    Well snapshot handlers

--- projects/subgraph-wells/src/utils/Dates.ts
+++ projects/subgraph-wells/src/utils/Dates.ts
@@ -1,11 +1,11 @@
 import { BigInt } from "@graphprotocol/graph-ts";
 
-export function dayFromTimestamp(timestamp: BigInt): string {
+export function dayFromTimestamp(timestamp: BigInt): i32 {
     let day_ts = timestamp.toI32() - (timestamp.toI32() % 86400)
-    return day_ts.toString()
+    return day_ts / 86400
 }
 
-export function hourFromTimestamp(timestamp: BigInt): string {
+export function hourFromTimestamp(timestamp: BigInt): i32 {
     let day_ts = timestamp.toI32() - (timestamp.toI32() % 3600)
-    return day_ts.toString()
+    return day_ts / 3600
 }
