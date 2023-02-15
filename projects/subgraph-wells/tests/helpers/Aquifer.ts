805d265fffebee5589e373072a89e08eb1f45b4e
Cujowolf <cujowolf12@gmail.com>
2023-02-15 16:18:49 -0600

    update USD handlers and tests

--- projects/subgraph-wells/tests/helpers/Aquifer.ts
+++ projects/subgraph-wells/tests/helpers/Aquifer.ts
@@ -3,46 +3,51 @@ import { newMockEvent } from "matchstick-as/assembly/index";
 import { BoreWell } from "../../generated/Aquifer/Aquifer";
 import { handleBoreWell } from "../../src/templates/AquiferHandler";
 import { BEAN_ERC20, WETH } from "../../src/utils/Constants";
-import { AQUIFER, AUGER, PUMP, WELL, WELL_FUNCTION } from "./Constants";
+import { AQUIFER, IMPLEMENTATION, PUMP, WELL, WELL_DATA, WELL_FUNCTION } from "./Constants";
+import { createContractCallMocks } from "./Functions";
 
-export function createBoreWellEvent(aquifer: Address, well: Address, tokens: Address[], wellFunction: ethereum.Tuple, pumps: ethereum.Tuple[], auger: Address): BoreWell {
-    let event = changetype<BoreWell>(newMockEvent())
+export function createBoreWellEvent(
+  aquifer: Address,
+  well: Address,
+  tokens: Address[],
+  wellFunction: ethereum.Tuple,
+  pumps: ethereum.Tuple[],
+  implementation: Address,
+  wellData: Bytes
+): BoreWell {
+  let event = changetype<BoreWell>(newMockEvent());
 
-    event.address = aquifer
-    event.parameters = new Array()
+  event.address = aquifer;
+  event.parameters = new Array();
 
-    let param1 = new ethereum.EventParam("well", ethereum.Value.fromAddress(well))
-    let param2 = new ethereum.EventParam("tokens", ethereum.Value.fromAddressArray(tokens))
-    let param3 = new ethereum.EventParam("wellFunction", ethereum.Value.fromTuple(wellFunction))
-    let param4 = new ethereum.EventParam("pumps", ethereum.Value.fromTupleArray(pumps))
-    let param5 = new ethereum.EventParam("auger", ethereum.Value.fromAddress(auger))
+  let param1 = new ethereum.EventParam("well", ethereum.Value.fromAddress(well));
+  let param2 = new ethereum.EventParam("implementation", ethereum.Value.fromAddress(implementation));
+  let param3 = new ethereum.EventParam("tokens", ethereum.Value.fromAddressArray(tokens));
+  let param4 = new ethereum.EventParam("wellFunction", ethereum.Value.fromTuple(wellFunction));
+  let param5 = new ethereum.EventParam("pumps", ethereum.Value.fromTupleArray(pumps));
+  let param6 = new ethereum.EventParam("auger", ethereum.Value.fromBytes(wellData));
 
-    event.parameters.push(param1)
-    event.parameters.push(param2)
-    event.parameters.push(param3)
-    event.parameters.push(param4)
-    event.parameters.push(param5)
+  event.parameters.push(param1);
+  event.parameters.push(param2);
+  event.parameters.push(param3);
+  event.parameters.push(param4);
+  event.parameters.push(param5);
+  event.parameters.push(param6);
 
-    return event as BoreWell
+  return event as BoreWell;
 }
 
 export function boreDefaultWell(): void {
-    let wellFunctionTuple = new ethereum.Tuple()
-    wellFunctionTuple.push(ethereum.Value.fromAddress(WELL_FUNCTION))
-    wellFunctionTuple.push(ethereum.Value.fromBytes(Bytes.empty()))
+  createContractCallMocks();
+  let wellFunctionTuple = new ethereum.Tuple();
+  wellFunctionTuple.push(ethereum.Value.fromAddress(WELL_FUNCTION));
+  wellFunctionTuple.push(ethereum.Value.fromBytes(Bytes.empty()));
 
-    let pump1Tuple = new ethereum.Tuple()
-    pump1Tuple.push(ethereum.Value.fromAddress(PUMP))
-    pump1Tuple.push(ethereum.Value.fromBytes(Bytes.empty()))
+  let pump1Tuple = new ethereum.Tuple();
+  pump1Tuple.push(ethereum.Value.fromAddress(PUMP));
+  pump1Tuple.push(ethereum.Value.fromBytes(Bytes.empty()));
 
-    let boreWellEvent = createBoreWellEvent(
-        AQUIFER,
-        WELL,
-        [BEAN_ERC20, WETH],
-        wellFunctionTuple,
-        [pump1Tuple],
-        AUGER
-    )
+  let boreWellEvent = createBoreWellEvent(AQUIFER, WELL, [BEAN_ERC20, WETH], wellFunctionTuple, [pump1Tuple], IMPLEMENTATION, WELL_DATA);
 
-    handleBoreWell(boreWellEvent)
+  handleBoreWell(boreWellEvent);
 }
