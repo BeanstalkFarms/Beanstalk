36392d278e3c72fa87d367b25dfb4cdd7204caf8
ALECKS <0xalecks@gmail.com>
2023-02-22 09:30:24 -0500

    sdk: initial support for swap through wells

---
+++ projects/sdk/src/lib/farm/actions/WellSwap.ts
@@ -0,0 +1,104 @@
+import { TokenValue } from "@beanstalk/sdk-core";
+import { ethers } from "ethers";
+import { Token } from "src/classes/Token";
+import { RunContext, RunMode, Step, StepClass, Workflow } from "src/classes/Workflow";
+import { AdvancedPipePreparedResult } from "src/lib/depot/pipe";
+
+export class WellSwap extends StepClass<AdvancedPipePreparedResult> {
+  public name: string = "wellSwap";
+
+  constructor(public wellAddress: string, public fromToken: Token, public toToken: Token, public recipient: string) {
+    super();
+  }
+
+  async run(_amountInStep: ethers.BigNumber, context: RunContext): Promise<Step<AdvancedPipePreparedResult>> {
+    const well = await WellSwap.sdk.wells.getWell(this.wellAddress, { name: true });
+    const reversed = context.runMode === RunMode.EstimateReversed;
+    WellSwap.sdk.debug(`>[${this.name}.run()]`, {
+      wellName: well.name,
+      wellAddress: this.wellAddress,
+      fromToken: this.fromToken.symbol,
+      toToken: this.toToken.symbol,
+      amountInStep: _amountInStep,
+      recipient: this.recipient,
+      reversed,
+      context
+    });
+
+    // Run estimate to calculate amountOut
+    let estimate: TokenValue;
+
+    if (reversed) {
+      // estimate reverse
+      estimate = await well.swapToQuote(this.fromToken, this.toToken, this.toToken.fromBlockchain(_amountInStep));
+    } else {
+      // estimate forward
+      estimate = await well.swapFromQuote(this.fromToken, this.toToken, this.fromToken.fromBlockchain(_amountInStep));
+    }
+
+    const wellFunctionName = reversed ? "swapTo" : "swapFrom";
+
+    return {
+      name: this.name,
+      amountOut: estimate.toBigNumber(),
+      value: ethers.BigNumber.from(0),
+      prepare: () => {
+        if (context.data.slippage === undefined) throw new Error("WellSwap: slippage required");
+
+        let callData: string;
+        if (!reversed) {
+          const minAmountOut = estimate.subSlippage(context.data.slippage);
+
+          WellSwap.sdk.debug(`>[${this.name}.prepare()]`, {
+            well: well.name,
+            tokenIn: this.fromToken.symbol,
+            tokenOut: this.toToken.symbol,
+            amountIn: _amountInStep.toString(),
+            amountInStep: _amountInStep.toString(),
+            minAmountOut: minAmountOut.toBigNumber().toString(),
+            reversed,
+            method: "swapFrom",
+            context
+          });
+
+          callData = well.contract.interface.encodeFunctionData("swapFrom", [
+            this.fromToken.address,
+            this.toToken.address,
+            _amountInStep,
+            minAmountOut.toBigNumber(),
+            this.recipient
+          ]);
+        } else {
+          const maxAmountIn = estimate.addSlippage(context.data.slippage);
+          WellSwap.sdk.debug(`>[${this.name}.prepare()]`, {
+            well: well.name,
+            tokenIn: this.fromToken.symbol,
+            tokenOut: this.toToken.symbol,
+            maxAmountIn: maxAmountIn.toBlockchain().toString(),
+            amountOut: _amountInStep.toString(),
+            amountInStep: _amountInStep.toString(),
+            reversed,
+            method: "swapTo",
+            context
+          });
+
+          callData = well.contract.interface.encodeFunctionData("swapTo", [
+            this.fromToken.address,
+            this.toToken.address,
+            maxAmountIn.toBigNumber(),
+            _amountInStep,
+            this.recipient
+          ]);
+        }
+
+        return {
+          target: this.wellAddress,
+          callData: callData
+        };
+      },
+      decode: (data: string) => well.contract.interface.decodeFunctionData(wellFunctionName, data),
+      // @ts-ignore
+      decodeResult: (data: string) => well.contract.interface.decodeFunctionResult(wellFunctionName, data)
+    };
+  }
+}
