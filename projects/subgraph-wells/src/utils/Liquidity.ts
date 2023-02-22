1c4bc680e3596f5e03feefa6a5c7c60b357ddb84
Cujowolf <cujowolf12@gmail.com>
2023-02-22 15:05:32 -0600

    Liquidity event USD value updates

--- projects/subgraph-wells/src/utils/Liquidity.ts
+++ projects/subgraph-wells/src/utils/Liquidity.ts
@@ -1,74 +1,80 @@
 import { BigInt } from "@graphprotocol/graph-ts";
 import { Deposit, Withdraw } from "../../generated/schema";
 import { AddLiquidity, RemoveLiquidity, RemoveLiquidityOneToken } from "../../generated/templates/Well/Well";
-import { ZERO_BD, ZERO_BI } from "./Decimals";
-import { loadWell } from "./Well";
+import { getBigDecimalArrayTotal, ZERO_BD, ZERO_BI } from "./Decimals";
+import { getCalculatedReserveUSDValues, loadWell } from "./Well";
 
 export function recordAddLiquidityEvent(event: AddLiquidity): void {
-    let id = event.transaction.hash.toHexString() + '-' + event.logIndex.toString()
-    let deposit = new Deposit(id)
-    let receipt = event.receipt
-    let well = loadWell(event.address)
+  let id = event.transaction.hash.toHexString() + "-" + event.logIndex.toString();
+  let deposit = new Deposit(id);
+  let receipt = event.receipt;
+  let well = loadWell(event.address);
 
-    deposit.hash = event.transaction.hash
-    deposit.nonce = event.transaction.nonce
-    deposit.logIndex = event.logIndex.toI32()
-    deposit.gasLimit = event.transaction.gasLimit
-    if (receipt !== null) { deposit.gasUsed = receipt.gasUsed }
-    deposit.gasPrice = event.transaction.gasPrice
-    deposit.account = event.transaction.from
-    deposit.well = event.address
-    deposit.blockNumber = event.block.number
-    deposit.timestamp = event.block.timestamp
-    deposit.liquidity = event.params.lpAmountOut
-    deposit.tokens = well.tokens
-    deposit.reserves = event.params.tokenAmountsIn
-    deposit.amountUSD = ZERO_BD
-    deposit.save()
+  deposit.hash = event.transaction.hash;
+  deposit.nonce = event.transaction.nonce;
+  deposit.logIndex = event.logIndex.toI32();
+  deposit.gasLimit = event.transaction.gasLimit;
+  if (receipt !== null) {
+    deposit.gasUsed = receipt.gasUsed;
+  }
+  deposit.gasPrice = event.transaction.gasPrice;
+  deposit.account = event.transaction.from;
+  deposit.well = event.address;
+  deposit.blockNumber = event.block.number;
+  deposit.timestamp = event.block.timestamp;
+  deposit.liquidity = event.params.lpAmountOut;
+  deposit.tokens = well.tokens;
+  deposit.reserves = event.params.tokenAmountsIn;
+  deposit.amountUSD = getBigDecimalArrayTotal(getCalculatedReserveUSDValues(well.tokens, event.params.tokenAmountsIn));
+  deposit.save();
 }
 
 export function recordRemoveLiquidityEvent(event: RemoveLiquidity): void {
-    let id = event.transaction.hash.toHexString() + '-' + event.logIndex.toString()
-    let withdraw = new Withdraw(id)
-    let receipt = event.receipt
-    let well = loadWell(event.address)
+  let id = event.transaction.hash.toHexString() + "-" + event.logIndex.toString();
+  let withdraw = new Withdraw(id);
+  let receipt = event.receipt;
+  let well = loadWell(event.address);
 
-    withdraw.hash = event.transaction.hash
-    withdraw.nonce = event.transaction.nonce
-    withdraw.logIndex = event.logIndex.toI32()
-    withdraw.gasLimit = event.transaction.gasLimit
-    if (receipt !== null) { withdraw.gasUsed = receipt.gasUsed }
-    withdraw.gasPrice = event.transaction.gasPrice
-    withdraw.account = event.transaction.from
-    withdraw.well = event.address
-    withdraw.blockNumber = event.block.number
-    withdraw.timestamp = event.block.timestamp
-    withdraw.liquidity = event.params.lpAmountIn
-    withdraw.tokens = well.tokens
-    withdraw.reserves = event.params.tokenAmountsOut
-    withdraw.amountUSD = ZERO_BD
-    withdraw.save()
+  withdraw.hash = event.transaction.hash;
+  withdraw.nonce = event.transaction.nonce;
+  withdraw.logIndex = event.logIndex.toI32();
+  withdraw.gasLimit = event.transaction.gasLimit;
+  if (receipt !== null) {
+    withdraw.gasUsed = receipt.gasUsed;
+  }
+  withdraw.gasPrice = event.transaction.gasPrice;
+  withdraw.account = event.transaction.from;
+  withdraw.well = event.address;
+  withdraw.blockNumber = event.block.number;
+  withdraw.timestamp = event.block.timestamp;
+  withdraw.liquidity = event.params.lpAmountIn;
+  withdraw.tokens = well.tokens;
+  withdraw.reserves = event.params.tokenAmountsOut;
+  withdraw.amountUSD = getBigDecimalArrayTotal(getCalculatedReserveUSDValues(well.tokens, event.params.tokenAmountsOut));
+  withdraw.save();
 }
 
 export function recordRemoveLiquidityOneEvent(event: RemoveLiquidityOneToken, tokenAmounts: BigInt[]): void {
-    let id = event.transaction.hash.toHexString() + '-' + event.logIndex.toString()
-    let withdraw = new Withdraw(id)
-    let receipt = event.receipt
-    let well = loadWell(event.address)
+  let id = event.transaction.hash.toHexString() + "-" + event.logIndex.toString();
+  let withdraw = new Withdraw(id);
+  let receipt = event.receipt;
+  let well = loadWell(event.address);
 
-    withdraw.hash = event.transaction.hash
-    withdraw.nonce = event.transaction.nonce
-    withdraw.logIndex = event.logIndex.toI32()
-    withdraw.gasLimit = event.transaction.gasLimit
-    if (receipt !== null) { withdraw.gasUsed = receipt.gasUsed }
-    withdraw.gasPrice = event.transaction.gasPrice
-    withdraw.account = event.transaction.from
-    withdraw.well = event.address
-    withdraw.blockNumber = event.block.number
-    withdraw.timestamp = event.block.timestamp
-    withdraw.liquidity = event.params.lpAmountIn
-    withdraw.tokens = well.tokens
-    withdraw.reserves = tokenAmounts
-    withdraw.amountUSD = ZERO_BD
-    withdraw.save()
+  withdraw.hash = event.transaction.hash;
+  withdraw.nonce = event.transaction.nonce;
+  withdraw.logIndex = event.logIndex.toI32();
+  withdraw.gasLimit = event.transaction.gasLimit;
+  if (receipt !== null) {
+    withdraw.gasUsed = receipt.gasUsed;
+  }
+  withdraw.gasPrice = event.transaction.gasPrice;
+  withdraw.account = event.transaction.from;
+  withdraw.well = event.address;
+  withdraw.blockNumber = event.block.number;
+  withdraw.timestamp = event.block.timestamp;
+  withdraw.liquidity = event.params.lpAmountIn;
+  withdraw.tokens = well.tokens;
+  withdraw.reserves = tokenAmounts;
+  withdraw.amountUSD = getBigDecimalArrayTotal(getCalculatedReserveUSDValues(well.tokens, tokenAmounts));
+  withdraw.save();
 }
