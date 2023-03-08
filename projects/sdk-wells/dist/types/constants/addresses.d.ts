081f1c23543952d156b685586405b1c5f2543749
ALECKS <0xalecks@gmail.com>
2023-03-08 14:25:43 -0500

    sdk-wells: remove dist files

--- projects/sdk-wells/dist/types/constants/addresses.d.ts
+++
@@ -1,6 +0,0 @@
-import { Address } from "@beanstalk/sdk-core";
-export declare const addresses: {
-    BEAN: Address;
-    WETH: Address;
-    USDC: Address;
-};
--- projects/sdk-wells/dist/types/lib/Pump.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class Pump {
-    readonly address: string;
-    readonly data: string;
-    constructor(address: string, data: string);
-}
