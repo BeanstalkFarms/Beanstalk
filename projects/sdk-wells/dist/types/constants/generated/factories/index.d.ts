59ac31d3d69a235dd534a0bd21253aea1d688695
ALECKS <0xalecks@gmail.com>
2023-03-08 14:25:43 -0500

    sdk-wells: remove dist files

--- projects/sdk-wells/dist/types/constants/generated/factories/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { Aquifer__factory } from "./Aquifer__factory";
-export { Auger__factory } from "./Auger__factory";
-export { ERC20__factory } from "./ERC20__factory";
-export { ERC20Permit__factory } from "./ERC20Permit__factory";
-export { Well__factory } from "./Well__factory";
--- projects/sdk-wells/dist/types/lib/Auger.d.ts
+++
@@ -1,8 +0,0 @@
-import { Auger as AugerContract } from "../constants/generated";
-import { WellsSDK } from "./WellsSDK";
-export declare class Auger {
-    sdk: WellsSDK;
-    readonly address: string;
-    readonly contract: AugerContract;
-    constructor(sdk: WellsSDK, address: string);
-}
