6b4cee744158a48dadcfb3ea24d6ee126ea63a99
k-xo <alkassimk@gmail.com>
2023-03-15 20:44:57 +0100

    add profit column

--- protocol/scripts/sunriseSimulate.js
+++ protocol/scripts/sunriseSimulate.js
@@ -3,6 +3,7 @@ const { upgradeWithNewFacets } = require('../scripts/diamond.js');
 const hre = require('hardhat');
 const { impersonateBeanstalkOwner, mintEth } = require('../utils');
 const fs = require('node:fs');
+const { BigNumber } = require('ethers');
 
 async function main() {
   const seasonFacet = await hre.ethers.getContractAt(
@@ -24,14 +25,14 @@ async function main() {
   );
 
   let csvContent =
-    'TX_HASH,GAS_USED,GAS_PRICE,GAS_COST,GAS_COST_IN_DOLLARS,BEAN_AMOUNT\n';
+    'TX_HASH,GAS_USED,GAS_PRICE,GAS_COST,GAS_COST_IN_DOLLARS,BEAN_AMOUNT,PROFIT\n';
 
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 5; i++) {
     // fetch eth price from uniswap pool
 
-    const ethPrice = await getETHPrice();
-
     const event = events[i];
+    const ethPrice = await getETHPrice(event.blockNumber);
+
     const txHash = event.transactionHash;
     const receipt = await ethers.provider.getTransactionReceipt(txHash);
 
@@ -46,6 +47,9 @@ async function main() {
     });
 
     const beanAmount = parseInt(beanTransfer?.data, 16);
+    const usdGasCost =
+      ethPrice * gasUsed * ethers.utils.formatUnits(gasPrice, 'ether');
+    const beanInUsd = ethers.utils.formatUnits(beanAmount, 6);
 
     csvContent +=
       receipt.transactionHash +
@@ -56,16 +60,21 @@ async function main() {
       ',' +
       gasUsed * ethers.utils.formatUnits(gasPrice, 'ether') +
       ',' +
-      ethPrice * gasUsed * ethers.utils.formatUnits(gasPrice, 'ether') +
+      usdGasCost +
       ',' +
+      // feeData.maxFeePerGas +
+      // ',' +
       beanAmount +
+      ',' +
+      (beanInUsd - usdGasCost) +
       '\n';
   }
 
   csvContent += '\n"AFTER UPGRADE"\n\n';
 
-  for (let i = 0; i < 10; i++) {
-    const ethPrice = await getETHPrice();
+  for (let i = 0; i < 5; i++) {
+    const event = events[i];
+    const ethPrice = await getETHPrice(event.blockNumber);
 
     const lastTimestamp = (await ethers.provider.getBlock('latest')).timestamp;
     const hourTimestamp = parseInt(lastTimestamp / 3600 + 1) * 3600;
@@ -85,33 +94,40 @@ async function main() {
 
     const [signer] = await hre.ethers.getSigners();
     const sunrise = await beanstalk.connect(signer).sunrise();
-    sunrise.wait().then((receipt) => {
-      const gasUsed = receipt.gasUsed;
-      const gasPrice = receipt.effectiveGasPrice;
-      const [beanTransfer] = receipt.logs.filter((log) => {
-        return (
-          log.address === '0xBEA0000029AD1c77D3d5D23Ba2D8893dB9d1Efab' &&
-          hre.ethers.utils.hexZeroPad(receipt.from.toLowerCase(), 32) ===
-            log.topics[2]
-        );
-      });
+    const receipt = await sunrise.wait();
 
-      const beanAmount = parseInt(beanTransfer?.data, 16);
-
-      csvContent +=
-        receipt.transactionHash +
-        ',' +
-        gasUsed +
-        ',' +
-        gasPrice +
-        ',' +
-        gasUsed * ethers.utils.formatUnits(gasPrice, 'ether') +
-        ',' +
-        ethPrice * gasUsed * ethers.utils.formatUnits(gasPrice, 'ether') +
-        ',' +
-        beanAmount +
-        '\n';
+    const gasUsed = receipt.gasUsed;
+    const gasPrice = receipt.effectiveGasPrice;
+    const [beanTransfer] = receipt.logs.filter((log) => {
+      return (
+        log.address === '0xBEA0000029AD1c77D3d5D23Ba2D8893dB9d1Efab' &&
+        hre.ethers.utils.hexZeroPad(receipt.from.toLowerCase(), 32) ===
+          log.topics[2]
+      );
     });
+
+    const beanAmount = parseInt(beanTransfer?.data, 16);
+    const usdGasCost =
+      ethPrice * gasUsed * ethers.utils.formatUnits(gasPrice, 'ether');
+    const beanInUsd = ethers.utils.formatUnits(beanAmount, 6);
+
+    csvContent +=
+      receipt.transactionHash +
+      ',' +
+      gasUsed +
+      ',' +
+      gasPrice +
+      ',' +
+      gasUsed * ethers.utils.formatUnits(gasPrice, 'ether') +
+      ',' +
+      usdGasCost +
+      ',' +
+      // feeData.maxFeePerGas +
+      // ',' +
+      beanAmount +
+      ',' +
+      (beanInUsd - usdGasCost) +
+      '\n';
   }
 
   try {
@@ -123,7 +139,18 @@ async function main() {
   }
 }
 
-async function getETHPrice() {
+async function getETHPrice(blockNumber) {
+  await network.provider.request({
+    method: 'hardhat_reset',
+    params: [
+      {
+        forking: {
+          jsonRpcUrl: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
+          blockNumber: blockNumber,
+        },
+      },
+    ],
+  });
   const usdcWethPool = await ethers.getContractAt(
     'IUniswapV3Pool',
     '0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640'
