219c4820972b3064190d4a370b1c6d2203fc5878
ALECKS <0xalecks@gmail.com>
2023-03-17 17:15:01 -0400

    dex: wagmi & sdk

--- projects/dex-ui/src/utils/WagmiClient.tsx
+++
@@ -1,7 +0,0 @@
-import { getDefaultProvider } from "ethers";
-import { createClient } from "wagmi";
-
-export const client = createClient({
-  autoConnect: true,
-  provider: getDefaultProvider()
-});
