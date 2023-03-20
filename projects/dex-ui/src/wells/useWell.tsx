b22accad1de573a176af24dddd5a8f5f1620cce9
ALECKS <0xalecks@gmail.com>
2023-03-20 09:09:44 -0400

    dex: Make single well page functional

---
+++ projects/dex-ui/src/wells/useWell.tsx
@@ -0,0 +1,35 @@
+// import { Well } from "@beanstalk/sdk/Wells";
+import { Well } from "@beanstalk/sdk/dist/types/Wells";
+import { useEffect, useState } from "react";
+import useSdk from "src/utils/sdk/useSdk";
+
+export const useWell = (address: string) => {
+  const sdk = useSdk();
+  const [well, setWell] = useState<Well>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error>();
+
+  useEffect(() => {
+    let listen = true;
+    const fetchData = async () => {
+      setLoading(true);
+      setError(undefined);
+      const well = await sdk.wells.getWell(address);
+      console.log("here", well);
+      if (listen) setWell(well);
+    };
+
+    fetchData()
+      .catch((err) => {
+        console.log(err);
+        setError(err);
+      })
+      .finally(() => setLoading(false));
+
+    return () => {
+      listen = false;
+    };
+  }, [address]);
+
+  return { well, loading, error };
+};
