4392fda7c3e4802eaf957d6a5d646c7322ab475c
ALECKS <0xalecks@gmail.com>
2023-03-20 09:09:44 -0400

    dex: Make single well page functional

--- projects/dex-ui/src/components/App/App.tsx
+++ projects/dex-ui/src/components/App/App.tsx
@@ -4,6 +4,7 @@ import { NotFound } from "src/pages/404";
 import { Home } from "src/pages/Home";
 import { Silo } from "src/pages/Silo";
 import { Swap } from "src/pages/Swap";
+import { Well } from "src/pages/Well";
 import { Wells } from "src/pages/Wells";
 import { Frame } from "../Frame/Frame";
 
@@ -14,6 +15,7 @@ export const App = () => {
         <Route index element={<Home />} />
         <Route path="/swap" element={<Swap />} />
         <Route path="/wells" element={<Wells />} />
+        <Route path="/wells/:address" element={<Well />} />
         <Route path="/silo" element={<Silo />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
