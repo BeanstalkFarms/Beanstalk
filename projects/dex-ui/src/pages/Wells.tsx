ed0cc2130da6214043f2cf33f1f0589d46166e22
ALECKS <0xalecks@gmail.com>
2023-03-20 09:09:29 -0400

    dex: make wells page functional

--- projects/dex-ui/src/pages/Wells.tsx
+++ projects/dex-ui/src/pages/Wells.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useWells } from "src/wells/useWells";
 
 export const Wells = () => {
-  return <div>Wells Page</div>;
+  const { wells, loading, error } = useWells();
+  if (loading) return <div>loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  const rows = wells?.map((well) => <div key={well.address}><Link to={`/wells/${well.address}`}>{well.name}</Link></div>);
+
+  return (
+    <div>
+      <strong>Wells:</strong>
+      <div>{rows}</div>
+    </div>
+  );
 };
---
+++ projects/dex-ui/src/wells/useWells.tsx
@@ -0,0 +1,44 @@
+// TODO: Fix this crazy typescript/node/ config stuff so we can import things properly.
+//       - check tsconfig 'moduleResolution' or something
+// import { Well } from "@beanstalk/sdk/Wells";
+import { Well } from "@beanstalk/sdk/dist/types/Wells";
+import { useEffect, useState } from "react";
+import { WELL_ADDRESSES } from "src/constants/addresses";
+import useSdk from "src/utils/sdk/useSdk";
+
+export const useWells = () => {
+  const sdk = useSdk();
+  const [wells, setWells] = useState<Well[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error>();
+
+  useEffect(() => {
+    let listen = true;
+    const fetchData = async () => {
+      setLoading(true);
+      setError(undefined);
+      const wells = await Promise.all(
+        WELL_ADDRESSES.map((address) => {
+          console.log(address);
+          return sdk.wells.getWell(address, {
+            name: true
+          });
+        })
+      );
+      if (listen) setWells(wells);
+    };
+
+    fetchData()
+      .catch((err) => {
+        console.log(err);
+        setError(err);
+      })
+      .finally(() => setLoading(false));
+
+    return () => {
+      listen = false;
+    };
+  }, []);
+
+  return { wells, loading, error };
+};
