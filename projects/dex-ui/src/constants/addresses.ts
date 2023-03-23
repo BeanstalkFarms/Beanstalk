a34a6ebf9ea15755fbce48e63cdf84f5b8a0162c
ALECKS <0xalecks@gmail.com>
2023-03-23 10:54:15 -0400

    dex: use env for well addresses

--- projects/dex-ui/src/constants/addresses.ts
+++ projects/dex-ui/src/constants/addresses.ts
@@ -1,4 +1,18 @@
+/**
+ * We are hardcoding Well addresses to start with.
+ * Once we have a registry, we can switch to polling that for Wells.
+ *
+ * Note: Until we have actual wells deployed on Mainnet, each dev
+ * will have to deploy them locally, thus getting different addresses.
+ * To make this easier, we will also load Well addresses from the .env vars.
+ * This should go away once we deploy and have real wells in the fork.
+ *
+ *  */
+
+const localWells = (import.meta.env.VITE_WELLS ?? "").toLowerCase().split(",");
+
 export const WELL_ADDRESSES = [
   // BEAN <> WETH
-  "0x0a9CBb76cEE4B11c7d831FcD13493abd2E1f98b3"
+  // "address",
+  ...localWells
 ];
