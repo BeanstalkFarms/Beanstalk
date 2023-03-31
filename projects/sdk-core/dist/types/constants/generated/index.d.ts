070c8932d0a2b48d744f1478a54746a8e8afd090
ALECKS <0xalecks@gmail.com>
2023-03-31 14:40:52 -0400

    sdk-core: remove dist folder

--- projects/sdk-core/dist/types/constants/generated/index.d.ts
+++
@@ -1,5 +0,0 @@
-export type { ERC20 } from "./ERC20";
-export type { ERC20Permit } from "./ERC20Permit";
-export * as factories from "./factories";
-export { ERC20__factory } from "./factories/ERC20__factory";
-export { ERC20Permit__factory } from "./factories/ERC20Permit__factory";
--- projects/sdk-core/dist/types/lib/token/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { Token } from "./Token";
-export { ERC20Token } from "./ERC20Token";
-export { NativeToken } from "./NativeToken";
