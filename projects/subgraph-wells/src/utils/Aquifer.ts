705e39ddc9ab12b3711cd0ee8808110a22a1442f
Cujowolf <cujowolf12@gmail.com>
2023-04-16 10:57:20 -0500

    Use sunrise event to trigger snapshots

--- projects/subgraph-wells/src/utils/Aquifer.ts
+++ projects/subgraph-wells/src/utils/Aquifer.ts
@@ -2,10 +2,11 @@ import { Address } from "@graphprotocol/graph-ts";
 import { Aquifer } from "../../generated/schema";
 
 export function loadOrCreateAquifer(aquiferAddress: Address): Aquifer {
-    let aquifer = Aquifer.load(aquiferAddress)
-    if (aquifer == null) {
-        aquifer = new Aquifer(aquiferAddress)
-        aquifer.save()
-    }
-    return aquifer as Aquifer
+  let aquifer = Aquifer.load(aquiferAddress);
+  if (aquifer == null) {
+    aquifer = new Aquifer(aquiferAddress);
+    aquifer.wells = [];
+    aquifer.save();
+  }
+  return aquifer as Aquifer;
 }
