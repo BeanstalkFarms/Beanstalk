d277037a09e307d9c013725ff2669d75b1f4d25a
publius <beanstalk.publius@protonmail.com>
2023-04-19 13:00:06 -0700

    revert legacy init bips

--- protocol/contracts/beanstalk/init/InitBip8.sol
+++ protocol/contracts/beanstalk/init/InitBip8.sol
@@ -27,7 +27,7 @@ contract InitBip8 {
     function init() external {
         s.deprecated_isBudget[beanSprout] = true;
         s.deprecated_isBudget[beanstalkFarms] = true;
-        C.bean().mint(beanSprout, beanSproutBudget);
-        C.bean().mint(beanstalkFarms, beanstalkFarmsBudget);
+        IBean(s.c.bean).mint(beanSprout, beanSproutBudget);
+        IBean(s.c.bean).mint(beanstalkFarms, beanstalkFarmsBudget);
     }
 }
