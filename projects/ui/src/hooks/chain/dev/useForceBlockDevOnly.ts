45ea2dacbdd6739bb0ac5050ab84d65686cc0f1c
BeanSama <BeanSama@bean.farm>
2023-04-24 11:15:27 -0700

    comment out unused

--- projects/ui/src/hooks/chain/dev/useForceBlockDevOnly.ts
+++ projects/ui/src/hooks/chain/dev/useForceBlockDevOnly.ts
@@ -1,80 +1,113 @@
-import { TestUtils } from '@beanstalk/sdk';
+// import { TestUtils } from '@beanstalk/sdk';
 
-import { useMemo, useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { SupportedChainId } from '~/constants';
+// import { useMemo, useEffect, useState } from 'react';
+// import { useSelector } from 'react-redux';
+// import { DateTime } from 'luxon';
+// import { SupportedChainId } from '~/constants';
 
-import useSdk from '~/hooks/sdk';
-import useChainId from '~/hooks/chain/useChainId';
+// import useSdk from '~/hooks/sdk';
+// import useChainId from '~/hooks/chain/useChainId';
 
-import { IS_DEV } from '~/util';
-import { selectSunriseBlock } from '~/state/beanstalk/sun/reducer';
+// import { IS_DEV } from '~/util';
+// import { selectMorning, selectMorningBlockUpdate, selectSunriseBlock } from '~/state/beanstalk/sun/reducer';
 
-const FORCE_BLOCK_CONFIG = {
-  force: true,
-  debug: true,
-};
+// const FORCE_BLOCK_CONFIG = {
+//   force: true,
+//   debug: true,
+//   /// approximately takes 1-2 seconds to force a block
+//   lagOffset: 1
+// };
 
-const CAN_FORCE_BLOCK = FORCE_BLOCK_CONFIG.force && IS_DEV;
+// const CAN_FORCE_BLOCK = FORCE_BLOCK_CONFIG.force && IS_DEV;
 
-/// When developing in local host, blocks don't update automatically.
-/// IF AND ONLY IF env === DEV && chainID == Localhost we force update the block,
-/// every 12 seconds, relative to when sunrise was called
-export default function useForceBlockDevOnly() {
-  const sdk = useSdk();
-  const chainId = useChainId();
-  const chainUtil = useMemo(() => new TestUtils.BlockchainUtils(sdk), [sdk]);
-  const sunriseTime = useSelector(selectSunriseBlock).timestamp;
+// /// When developing in local host, blocks don't update automatically.
+// /// we force update the block every 12 seconds, relative to when sunrise
+// /// was called to replicate on-chain activity
+// export default function useForceBlockDevOnly() {
+//   const sdk = useSdk();
+//   const chainId = useChainId();
+//   const chainUtil = useMemo(() => new TestUtils.BlockchainUtils(sdk), [sdk]);
+//   const sunriseData = useSelector(selectSunriseBlock);
+//   const morningData = useSelector(selectMorning);
 
-  /// Timer for debugging. To set, set FORCE_BLOCK_CONFIG.debug to true
-  const [_, setLastTime] = useState(0);
+//   const morningUpdate = useSelector(selectMorningBlockUpdate);
 
-  useEffect(() => {
-    if (
-      !CAN_FORCE_BLOCK ||
-      chainId !== SupportedChainId.LOCALHOST ||
-      sunriseTime.lte(0)
-    ) {
-      return;
-    }
+//   const sunriseTime = sunriseData.timestamp;
+//   const morningBlock = morningData.blockNumber;
+//   const morningBlockTime = morningData.timestamp;
 
-    const _sunriseTime = sunriseTime.times(1000).toNumber();
+//   console.log("morningBlockTime: ", morningUpdate.remaining.seconds);
 
-    const intervalId = setInterval(() => {
-      const _currentTime = new Date().getTime();
-      const secondsDiff = Math.ceil(
-        Math.abs(_currentTime - _sunriseTime) / 1000
-      );
+//   /// Timer for debugging. To set, set FORCE_BLOCK_CONFIG.debug to true
+//   const [_, setLastTime] = useState(0);
 
-      console.debug('[useForceBlock][DEV-ONLY]: secondsDiff', secondsDiff);
+//   useEffect(() => {
+//     if (
+//       !morningData.isMorning ||
+//       !CAN_FORCE_BLOCK ||
+//       chainId !== SupportedChainId.LOCALHOST ||
+//       !sunriseTime
+//     ) {
+//       return;
+//     }
 
-      /// if it's been less than 1 block since the start of season, return
-      if (secondsDiff < 12) return;
+//     // if (morningUpdate.remaining.seconds === 0) {
+//     //   chainUtil.forceBlock();
+//     // }
 
-      if (secondsDiff % 12 <= 0) {
-        chainUtil.forceBlock();
-        if (!FORCE_BLOCK_CONFIG.debug) return;
-        setLastTime((prev) => {
-          if (prev === 0) {
-            console.debug(
-              '[useForceBlock][DEV-ONLY]: first time, Forcing block update...'
-            );
-            return _currentTime;
-          }
-          const diff = Math.abs(prev - _currentTime);
-          const elapsed = diff / 1000;
-          console.debug(
-            `[useForceBlock][DEV-ONLY]`,
-            elapsed,
-            'seconds have elapsed. Forcing block update...'
-          );
-          return _currentTime;
-        });
-      }
-    }, 1_000);
+//     const currblockTs = morningData.blockNumber;
 
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [sunriseTime, chainId, chainUtil]);
-}
+//     const intervalId = setInterval(async () => {
+//       const _sunriseTime = sunriseTime.toSeconds();
+//       const _currentTime = DateTime.now().toSeconds();
+
+//       const blockTime = morningData.timestamp
+
+//       const secsDiff = Math.abs(blockTime.toSeconds() - _sunriseTime);
+
+//       console.log("secsDiff: ", secsDiff);
+
+//       const now = DateTime.now();
+
+//       const remaining = Math.ceil(blockTime.diff(now, 'seconds').seconds % 12);
+//       console.log("remaining:", remaining);
+
+//       if (remaining === 1) {
+//         console.log('[useForceBlock] updating block...');
+//         chainUtil.forceBlock();
+//       }
+
+//       const secondsDiff = Math.ceil(Math.abs(sunriseTime.diffNow().as('seconds')));
+//       const offset = sunriseTime.toSeconds() % 12;
+
+//       /// if it's been less than 1 block since the start of season, return
+//       // if (secondsDiff < 12) return;
+
+//       // console.log('secondsDiff % 12', secondsDiff % 12);
+//       // if (secondsDiff % 12 === offset) {
+//       //   chainUtil.forceBlock();
+//       //   if (!FORCE_BLOCK_CONFIG.debug) return;
+//       //   setLastTime((prev) => {
+//       //     if (prev === 0) {
+//       //       console.debug(
+//       //         '[useForceBlock][DEV-ONLY]: first time, Forcing block update...'
+//       //       );
+//       //       return _currentTime;
+//       //     }
+//       //     const diff = Math.abs(prev - _currentTime);
+//       //     const elapsed = diff / 1000;
+//       //     console.debug(
+//       //       `[useForceBlock][DEV-ONLY]`,
+//       //       elapsed,
+//       //       'seconds have elapsed. Forcing block update...'
+//       //     );
+//       //     return _currentTime;
+//       //   });
+//       // }
+//     }, 1_000);
+
+//     return () => {
+//       clearInterval(intervalId);
+//     };
+//   }, [sunriseTime, chainId, chainUtil, morningData.isMorning, morningData.blockNumber, morningData.timestamp]);
+// }
