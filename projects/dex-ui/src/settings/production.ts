10c590971562ae7e232e1eb89e8a637cbf9902d0
ALECKS <0xalecks@gmail.com>
2023-04-26 16:37:12 -0400

    dex: use Settings object for configuration

---
+++ projects/dex-ui/src/settings/production.ts
@@ -0,0 +1,7 @@
+import { DexSettings } from ".";
+
+export const ProdSettings: DexSettings = {
+  AQUIFER_ADDRESS: "0xTBD",
+  SUBGRAPH_URL: "http://TBD/subgraphs/name/beanstalk-wells",
+  WELLS_ORIGIN_BLOCK: 16400000
+};
