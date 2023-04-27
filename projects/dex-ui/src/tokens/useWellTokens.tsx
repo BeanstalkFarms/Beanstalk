bb400968200d399f294ec0f0244f9b51d6e33539
ALECKS <0xalecks@gmail.com>
2023-04-27 15:51:30 -0400

    dex: improvements

--- projects/dex-ui/src/tokens/useWellTokens.tsx
+++ projects/dex-ui/src/tokens/useWellTokens.tsx
@@ -4,23 +4,28 @@ import { useQuery } from "@tanstack/react-query";
 import { useWells } from "src/wells/useWells";
 
 export const useWellTokens = () => {
-  const { data: wells } = useWells();
+  const { data: wells, error: wellsError } = useWells();
+  // @dev: if we fail to load wells, we still execute the useQuery below (see the `enabled` option), but
+  // make useQuery return the error, instead of throwing here and needing the parent to handle two type of errors
   return useQuery<Token[], Error>(
-    ["tokens"],
+    ["tokens", wellsError],
     () => {
+      if (wellsError) throw wellsError;
       const tokens: Token[] = [];
       for (const well of wells!) {
         if (well.tokens && Array.isArray(well.tokens)) {
           tokens.push(...well.tokens);
         }
       }
-
+      if (tokens.length === 0) {
+        throw new Error("No tokens found in wells");
+      }
       return tokens;
     },
     {
-      // initialData: [sdk.tokens.BEAN, sdk.tokens.WETH],
-      enabled: Array.isArray(wells),
-      refetchOnWindowFocus: false
+      enabled: Array.isArray(wells) || !!wellsError,
+      refetchOnWindowFocus: false,
+      retry: false
     }
   );
 };
