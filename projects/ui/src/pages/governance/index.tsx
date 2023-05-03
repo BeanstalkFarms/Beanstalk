23de85933d48e0043c1d8c8c14f430ce027f0d6e
BeanSama <BeanSama@bean.farm>
2023-05-03 13:33:19 -0700

    update theme + schema

--- projects/ui/src/pages/governance/index.tsx
+++ projects/ui/src/pages/governance/index.tsx
@@ -6,36 +6,24 @@ import GuideButton from '~/components/Common/Guide/GuideButton';
 
 import { FC } from '~/types';
 import GovernanceSpaces from '~/components/Governance/GovernanceSpaces';
-import FarmerDelegationsUpdater from '~/state/farmer/delegations/updater';
 
 const GovernancePage: FC<{}> = () => (
-  <>
-    <FarmerDelegationsUpdater />
-    <Container maxWidth="lg">
-      <Stack gap={2}>
-        <PageHeader
-          title="Governance"
-          description="Participate in Beanstalk governance as a Stalkholder"
-          href="https://docs.bean.money/almanac/governance/proposals"
-          control={
-            <GuideButton
-              title="The Farmers' Almanac: Governance Guides"
-              guides={[HOW_TO_VOTE]}
-            />
-          }
-        />
-        <GovernanceSpaces />
-        {/* <Grid container direction={{ xs: 'column', md: 'row' }} spacing={2}>
-        <Grid item xs={12} lg={3.5}>
-          <StalkholderCard />
-        </Grid>
-        <Grid item xs={12} lg={8.5}>
-          <Proposals />
-        </Grid>
-      </Grid> */}
-      </Stack>
-    </Container>
-  </>
+  <Container maxWidth="lg">
+    <Stack gap={2}>
+      <PageHeader
+        title="Governance"
+        description="Participate in Beanstalk governance"
+        href="https://docs.bean.money/almanac/governance/proposals"
+        control={
+          <GuideButton
+            title="The Farmers' Almanac: Governance Guides"
+            guides={[HOW_TO_VOTE]}
+          />
+        }
+      />
+      <GovernanceSpaces />
+    </Stack>
+  </Container>
 );
 
 export default GovernancePage;
