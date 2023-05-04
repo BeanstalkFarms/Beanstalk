2cdf79be26d4686f0b7b991299e859d4fa78e58f
marshall-bean <marshall@bean.farm>
2023-05-04 12:19:16 -0400

    Feedback applied

--- projects/dex-ui/src/components/Liquidity/allowance.ts
+++ projects/dex-ui/src/components/Liquidity/allowance.ts
@@ -1,16 +1,15 @@
-import { ERC20Token } from "@beanstalk/sdk-core";
-import { BigNumber } from "ethers";
+import { ERC20Token, TokenValue } from "@beanstalk/sdk-core";
 import { Log } from "src/utils/logger";
 
-const hasMinimumAllowance = async (walletAddress: string, spender: string, token: ERC20Token, mininumAllowance: BigNumber) => {
+const hasMinimumAllowance = async (walletAddress: string, spender: string, token: ERC20Token, mininumAllowance: TokenValue) => {
   const existingAllowance = await token.getAllowance(walletAddress, spender);
-  return existingAllowance.toBigNumber().gte(mininumAllowance);
+  return existingAllowance.gte(mininumAllowance);
 };
 
-const ensureAllowance = async (walletAddress: string, spender: string, token: ERC20Token, mininumAllowance: BigNumber) => {
+const ensureAllowance = async (walletAddress: string, spender: string, token: ERC20Token, mininumAllowance: TokenValue) => {
   if (!(await hasMinimumAllowance(walletAddress, spender, token, mininumAllowance))) {
     try {
-      const approveTXN = await token.getContract().approve(spender, mininumAllowance, { gasLimit: 50000 });
+      const approveTXN = await token.getContract().approve(spender, mininumAllowance.toBigNumber(), { gasLimit: 50000 });
       await approveTXN.wait();
     } catch (error: any) {
       Log.module("allowance").error("Error druing ensureAllowance: ", (error as Error).message);
