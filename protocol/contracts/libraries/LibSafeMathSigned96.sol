4b24331297be61dfbbca0b67cd2d81b97dd069d8
pizzaman1337 <pizzaman1337@gmail.com>
2023-05-03 22:33:43 -0600

    Update safe math signed int96

--- protocol/contracts/libraries/LibSafeMathSigned96.sol
+++ protocol/contracts/libraries/LibSafeMathSigned96.sol
@@ -3,99 +3,14 @@
 pragma solidity >=0.6.0 <0.8.0;
 
 /**
- * @author Brean
- * @title LibSafeMathSigned96 is a int96 variation of Open Zeppelin's Safe Math library.
-**/
-library LibSafeMathSigned96 {
-    /**
-     * @dev Returns the addition of two unsigned integers, with an overflow flag.
-     *
-     * _Available since v3.4._
-     */
-    function tryAdd(int96 a, int96 b) internal pure returns (bool, int96) {
-        int96 c = a + b;
-        if (c < a) return (false, 0);
-        return (true, c);
-    }
+ * @title SignedSafeMath
+ * @dev Signed math operations with safety checks that revert on error.
+ */
+library SignedSafeMath {
+    int96 constant private _INT96_MIN = -2**95;
 
     /**
-     * @dev Returns the substraction of two unsigned integers, with an overflow flag.
-     *
-     * _Available since v3.4._
-     */
-    function trySub(int96 a, int96 b) internal pure returns (bool, int96) {
-        if (b > a) return (false, 0);
-        return (true, a - b);
-    }
-
-    /**
-     * @dev Returns the multiplication of two unsigned integers, with an overflow flag.
-     *
-     * _Available since v3.4._
-     */
-    function tryMul(int96 a, int96 b) internal pure returns (bool, int96) {
-        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
-        // benefit is lost if 'b' is also tested.
-        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
-        if (a == 0) return (true, 0);
-        int96 c = a * b;
-        if (c / a != b) return (false, 0);
-        return (true, c);
-    }
-
-    /**
-     * @dev Returns the division of two unsigned integers, with a division by zero flag.
-     *
-     * _Available since v3.4._
-     */
-    function tryDiv(int96 a, int96 b) internal pure returns (bool, int96) {
-        if (b == 0) return (false, 0);
-        return (true, a / b);
-    }
-
-    /**
-     * @dev Returns the remainder of dividing two unsigned integers, with a division by zero flag.
-     *
-     * _Available since v3.4._
-     */
-    function tryMod(int96 a, int96 b) internal pure returns (bool, int96) {
-        if (b == 0) return (false, 0);
-        return (true, a % b);
-    }
-
-    /**
-     * @dev Returns the addition of two unsigned integers, reverting on
-     * overflow.
-     *
-     * Counterpart to Solidity's `+` operator.
-     *
-     * Requirements:
-     *
-     * - Addition cannot overflow.
-     */
-    function add(int96 a, int96 b) internal pure returns (int96) {
-        int96 c = a + b;
-        require(c >= a, "SafeMath: addition overflow");
-        return c;
-    }
-
-    /**
-     * @dev Returns the subtraction of two unsigned integers, reverting on
-     * overflow (when the result is negative).
-     *
-     * Counterpart to Solidity's `-` operator.
-     *
-     * Requirements:
-     *
-     * - Subtraction cannot overflow.
-     */
-    function sub(int96 a, int96 b) internal pure returns (int96) {
-        require(b <= a, "SafeMath: subtraction overflow");
-        return a - b;
-    }
-
-    /**
-     * @dev Returns the multiplication of two unsigned integers, reverting on
+     * @dev Returns the multiplication of two signed integers, reverting on
      * overflow.
      *
      * Counterpart to Solidity's `*` operator.
@@ -105,14 +20,23 @@ library LibSafeMathSigned96 {
      * - Multiplication cannot overflow.
      */
     function mul(int96 a, int96 b) internal pure returns (int96) {
-        if (a == 0) return 0;
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
+        require(!(a == -1 && b == _INT96_MIN), "SignedSafeMath: multiplication overflow");
+
         int96 c = a * b;
-        require(c / a == b, "SafeMath: multiplication overflow");
+        require(c / a == b, "SignedSafeMath: multiplication overflow");
+
         return c;
     }
 
     /**
-     * @dev Returns the integer division of two unsigned integers, reverting on
+     * @dev Returns the integer division of two signed integers. Reverts on
      * division by zero. The result is rounded towards zero.
      *
      * Counterpart to Solidity's `/` operator. Note: this function uses a
@@ -124,33 +48,17 @@ library LibSafeMathSigned96 {
      * - The divisor cannot be zero.
      */
     function div(int96 a, int96 b) internal pure returns (int96) {
-        require(b > 0, "SafeMath: division by zero");
-        return a / b;
+        require(b != 0, "SignedSafeMath: division by zero");
+        require(!(b == -1 && a == _INT96_MIN), "SignedSafeMath: division overflow");
+
+        int96 c = a / b;
+
+        return c;
     }
 
     /**
-     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
-     * reverting when dividing by zero.
-     *
-     * Counterpart to Solidity's `%` operator. This function uses a `revert`
-     * opcode (which leaves remaining gas untouched) while Solidity uses an
-     * invalid opcode to revert (consuming all remaining gas).
-     *
-     * Requirements:
-     *
-     * - The divisor cannot be zero.
-     */
-    function mod(int96 a, int96 b) internal pure returns (int96) {
-        require(b > 0, "SafeMath: modulo by zero");
-        return a % b;
-    }
-
-    /**
-     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
-     * overflow (when the result is negative).
-     *
-     * CAUTION: This function is deprecated because it requires allocating memory for the error
-     * message unnecessarily. For custom revert reasons use {trySub}.
+     * @dev Returns the subtraction of two signed integers, reverting on
+     * overflow.
      *
      * Counterpart to Solidity's `-` operator.
      *
@@ -158,48 +66,27 @@ library LibSafeMathSigned96 {
      *
      * - Subtraction cannot overflow.
      */
-    function sub(int96 a, int96 b, string memory errorMessage) internal pure returns (int96) {
-        require(b <= a, errorMessage);
-        return a - b;
+    function sub(int96 a, int96 b) internal pure returns (int96) {
+        int96 c = a - b;
+        require((b >= 0 && c <= a) || (b < 0 && c > a), "SignedSafeMath: subtraction overflow");
+
+        return c;
     }
 
     /**
-     * @dev Returns the integer division of two unsigned integers, reverting with custom message on
-     * division by zero. The result is rounded towards zero.
+     * @dev Returns the addition of two signed integers, reverting on
+     * overflow.
      *
-     * CAUTION: This function is deprecated because it requires allocating memory for the error
-     * message unnecessarily. For custom revert reasons use {tryDiv}.
-     *
-     * Counterpart to Solidity's `/` operator. Note: this function uses a
-     * `revert` opcode (which leaves remaining gas untouched) while Solidity
-     * uses an invalid opcode to revert (consuming all remaining gas).
+     * Counterpart to Solidity's `+` operator.
      *
      * Requirements:
      *
-     * - The divisor cannot be zero.
+     * - Addition cannot overflow.
      */
-    function div(int96 a, int96 b, string memory errorMessage) internal pure returns (int96) {
-        require(b > 0, errorMessage);
-        return a / b;
-    }
+    function add(int96 a, int96 b) internal pure returns (int96) {
+        int96 c = a + b;
+        require((b >= 0 && c >= a) || (b < 0 && c < a), "SignedSafeMath: addition overflow");
 
-    /**
-     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
-     * reverting with custom message when dividing by zero.
-     *
-     * CAUTION: This function is deprecated because it requires allocating memory for the error
-     * message unnecessarily. For custom revert reasons use {tryMod}.
-     *
-     * Counterpart to Solidity's `%` operator. This function uses a `revert`
-     * opcode (which leaves remaining gas untouched) while Solidity uses an
-     * invalid opcode to revert (consuming all remaining gas).
-     *
-     * Requirements:
-     *
-     * - The divisor cannot be zero.
-     */
-    function mod(int96 a, int96 b, string memory errorMessage) internal pure returns (int96) {
-        require(b > 0, errorMessage);
-        return a % b;
+        return c;
     }
 }
