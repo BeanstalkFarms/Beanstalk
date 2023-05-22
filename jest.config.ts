ada72076fad279167b0514d8302b80a0a79ef0d3
marshall-bean <marshall@bean.farm>
2023-05-22 11:16:22 -0400

    Initial test setup and test cases for wells SDK

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,17 @@ const jestConfig: JestConfigWithTsJest = {
     {
       // @ts-ignore
       preset: "ts-jest",
+      displayName: "sdk-wells",
+      rootDir: "projects/sdk-wells",
+      testMatch: ["<rootDir>/src/**/?(*.)+(spec|test).[jt]s?(x)", "<rootDir>/test/**/?(*.)+(spec|test).[jt]s?(x)"],
+      moduleNameMapper: {
+        "@beanstalk/wells/(.*)$": "<rootDir>/src/$1",
+        "^src/(.*)$": "<rootDir>/src/$1"
+      }
+    },
+    {
+      // @ts-ignore
+      preset: "ts-jest",
       displayName: "sdk",
       rootDir: "projects/sdk",
       testMatch: ["<rootDir>/src/**/?(*.)+(spec|test).[jt]s?(x)"],
