16329120af2b67584d904a4aa88c08f44998c26a
ALECKS <0xalecks@gmail.com>
2023-05-24 16:02:54 -0400

    dex: update settings

--- projects/dex-ui/src/settings/index.ts
+++ projects/dex-ui/src/settings/index.ts
@@ -1,6 +1,11 @@
 import { Address } from "src/types";
 import { DevSettings } from "./development";
 import { ProdSettings } from "./production";
+import { Log } from "src/utils/logger";
+
+const netlifyContext = import.meta.env.VITE_NETLIFY_CONTEXT;
+const netlifyCommitHash = import.meta.env.VITE_COMMIT_HASH;
+const netlifyBuildId = import.meta.env.VITE_NETLIFY_BUILD_ID;
 
 export type DexSettings = {
   PRODUCTION: boolean;
@@ -8,6 +13,19 @@ export type DexSettings = {
   SUBGRAPH_URL: string;
   WELLS_ORIGIN_BLOCK: number;
   LOAD_HISTORY_FROM_GRAPH: boolean;
+  NETLIFY_CONTEXT: string;
+  NETLIFY_COMMIT_HASH: string;
+  NETLIFY_BUILD_ID: string;
 };
 
-export const Settings = import.meta.env.DEV ? DevSettings : ProdSettings;
+const temp = netlifyContext === "production" ? ProdSettings : DevSettings;
+
+export const Settings = {
+  ...temp,
+  NETLIFY_CONTEXT: netlifyContext,
+  NETLIFY_COMMIT_HASH: netlifyCommitHash,
+  NETLIFY_BUILD_ID: netlifyBuildId
+};
+
+// @ts-ignore
+globalThis.settings = () => Log.module("settings").log(Settings);
