f092d92a124498e1eed152c32f3631f867d87d7e
uncoolzero <107518216+uncoolzero@users.noreply.github.com>
2023-05-31 14:49:58 -0300

    Made deltaB chart only show post-replant data

--- projects/ui/src/components/Analytics/Bean/DeltaB.tsx
+++ projects/ui/src/components/Analytics/Bean/DeltaB.tsx
@@ -23,6 +23,12 @@ const statProps = {
   titleTooltip: 'The liquidity and time weighted average shortage of Beans in liquidity pools on the Oracle Whitelist at the end of every Season.',
   gap: 0.25,
 };
+
+const queryConfig = {
+  variables: { season_gte: 6074 },
+  context: { subgraph: 'beanstalk' },
+};
+
 const lineChartProps: Partial<LineChartProps> = {
   yTickFormat: tickFormatLocale,
   horizontalLineNumber: 0,
@@ -34,6 +40,7 @@ const DeltaB: FC<{ height?: SeasonPlotBaseProps['height'] }> = ({ height }) => (
     height={height}
     getValue={getValue}
     formatValue={formatValue}
+    queryConfig={queryConfig}
     StatProps={statProps}
     LineChartProps={lineChartProps}
   />
