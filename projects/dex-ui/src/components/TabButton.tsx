ab9a10db465e2981a4e1c30703e1b2c723f4dcef
uncoolzero <107518216+uncoolzero@users.noreply.github.com>
2023-06-06 19:17:48 -0300

    Tab Button styling, enabled Slippage (unstyled)

--- projects/dex-ui/src/components/TabButton.tsx
+++ projects/dex-ui/src/components/TabButton.tsx
@@ -1,15 +1,17 @@
 import styled from "styled-components";
 
-export const TabButton = styled.button<{ active?: boolean; stretch?: boolean }>`
+export const TabButton = styled.button<{ active?: boolean; stretch?: boolean; bold?: boolean, justify?: boolean }>`
   display: flex;
   flex-direction: row;
   height: 48px;
   border: none;
   box-sizing: border-box;
   align-items: center;
+  ${({ justify }) => justify && `justify-content: center;`}
   padding: 12px 16px;
   ${({ stretch }) => stretch && `width: 100%;`}
-
+  font-weight: ${({ bold }) => (bold ? "bold" : "normal")};
+  z-index: ${({ active }) => (active ? "2" : "1")};
   outline: 0.5px solid ${({ active }) => (active ? "#000" : "#9CA3AF")};
   outline-offset: -0.5px;
   background-color: ${({ active }) => (active ? "#fff" : "#F9F8F6")};
