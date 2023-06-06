55092ea48af236df8973bc5d3846edb93cbfbeae
Brean0 <midoryia33@proton.me>
2023-06-06 18:34:36 +0545

    remove unused AssetAdded Struct

--- protocol/contracts/beanstalk/silo/SiloFacet/TokenSilo.sol
+++ protocol/contracts/beanstalk/silo/SiloFacet/TokenSilo.sol
@@ -213,12 +213,6 @@ contract TokenSilo is Silo {
         );
     }
 
-    struct AssetsAdded {
-        uint256 stalkAdded;
-        uint256 seedsAdded;
-        uint256 bdvAdded;
-    }
-
     /**
      * @notice Handles withdraw accounting for multiple deposits.
      *
