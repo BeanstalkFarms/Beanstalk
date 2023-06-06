73b5d02c72a0c77ce3556811c5bc31a331673d48
uncoolzero <107518216+uncoolzero@users.noreply.github.com>
2023-06-06 19:34:53 -0300

    Tab Button styling tweak

--- projects/dex-ui/src/components/TabButton.tsx
+++ projects/dex-ui/src/components/TabButton.tsx
@@ -10,7 +10,7 @@ export const TabButton = styled.button<{ active?: boolean; stretch?: boolean; bo
   ${({ justify }) => justify && `justify-content: center;`}
   padding: 12px 16px;
   ${({ stretch }) => stretch && `width: 100%;`}
-  font-weight: ${({ bold }) => (bold ? "bold" : "normal")};
+  font-weight: ${({ bold }) => (bold ? "600" : "normal")};
   z-index: ${({ active }) => (active ? "2" : "1")};
   outline: 0.5px solid ${({ active }) => (active ? "#000" : "#9CA3AF")};
   outline-offset: -0.5px;
