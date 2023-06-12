79a658fd444e2ec0916ae0a284ef1ef6da66495e
uncoolzero <107518216+uncoolzero@users.noreply.github.com>
2023-06-12 00:49:58 -0300

    Learn Boxes filled

--- projects/dex-ui/src/components/Well/LearnPump.tsx
+++ projects/dex-ui/src/components/Well/LearnPump.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 import { ExpandBox } from "src/components/ExpandBox";
-
+import styled from "styled-components";
 import { FC } from "src/types";
+
+function PumpDetails() {
+  return (
+    <TextContainer>
+      <div>A Pump is an Oracle that integrates with a Well.</div>
+      <div>Depending on their use case, developers can define the conditions under which their Well should write new reserves pricing data to the Pump, which can be used as a token price feed.</div>
+      <div>Check out this <StyledLink href="https://app.bean.money/">multi-block MEV manipulation resistant Pump</StyledLink> developed by Beanstalk Farms!</div>
+    </TextContainer>
+  )
+}
 
 export const LearnPump: FC<{}> = ({}) => {
   return (
@@ -12,7 +22,20 @@ export const LearnPump: FC<{}> = ({}) => {
         </span>{" "}
         What’s a pump?
       </ExpandBox.Header>
-      <ExpandBox.Body>Well function details here</ExpandBox.Body>
+      <ExpandBox.Body><PumpDetails /></ExpandBox.Body>
     </ExpandBox>
   );
 };
+
+const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  color: #4B5563;
+`
+
+const StyledLink = styled.a`
+  font-weight: 600;
+  color: #4B5563;
+  text-decoration-thickness: 1px;
+`
