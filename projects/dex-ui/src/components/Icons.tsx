81a4b15d487c99c3e43391fa91288d3ef08469bf
uncoolzero <107518216+uncoolzero@users.noreply.github.com>
2023-06-12 22:11:21 -0300

    Camelcased icon properties

--- projects/dex-ui/src/components/Icons.tsx
+++ projects/dex-ui/src/components/Icons.tsx
@@ -8,7 +8,7 @@ export type SVGProps = {
 
 export const ChevronDown = ({ color = "#000", width, height }: SVGProps) => (
   <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 10 6" fill="none">
-    <path d="M1 1L5 5L9 1" stroke={color} stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M1 1L5 5L9 1" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
   </svg>
   
 );
