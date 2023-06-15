a341a135484d20f43f272eeb35d68c30627c21ea
uncoolzero <107518216+uncoolzero@users.noreply.github.com>
2023-06-15 16:18:24 -0300

    Quote Details component is now an accordion

--- projects/dex-ui/src/components/ImageButton.tsx
+++ projects/dex-ui/src/components/ImageButton.tsx
@@ -8,20 +8,22 @@ type Props = {
   padding?: string;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
   rotate?: string;
+  margin?: string;
 } & ({ src: string; component?: never } | { src?: never; component?: JSXElementConstructor<any> });
 
 type StyleProps = {
   padding?: string;
   rotate?: string;
+  margin?: string;
 };
 
 // This component supports accepting either an imagine in two ways:
 // -- as a string url via `src`
 // -- as an SVG component via `component`. See src/components/Icons.tsx
 // for acceptable components
-export const ImageButton: FC<Props> = ({ size = 32, src, component, alt = "Image", onClick, padding, rotate }) => {
+export const ImageButton: FC<Props> = ({ size = 32, src, component, alt = "Image", onClick, padding, rotate, margin }) => {
   return (
-    <Button onClick={onClick} padding={padding} rotate={rotate}>
+    <Button onClick={onClick} padding={padding} rotate={rotate} margin={margin}>
       {src && <img src={src} alt={alt} width={size} />}
       {component && React.createElement(component, { width: size, height: size, color: "#000" })}
     </Button>
@@ -38,5 +40,6 @@ const Button = styled.button<StyleProps>`
   padding: ${(props) => props.padding ?? "5px"};
   ${(props) => props.onClick && "cursor: pointer;"};
   rotate: ${(props) => props.rotate ?? "0"}deg;
+  margin: ${(props) => props.margin ?? "0px"};
   transition-duration: 0.2s;
 `;
