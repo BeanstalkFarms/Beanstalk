ee5591608cf9e94155d3791440eb9f143dec742d
publius <beanstalk.publius@protonmail.com>
2023-06-15 14:19:53 +0545

    refactor ownership/whitelist tests

--- protocol/test/Whitelist.test.js
+++ protocol/test/Whitelist.test.js
@@ -32,7 +32,8 @@ describe('Whitelist', function () {
     ownerAddress = contracts.account;
     this.diamond = contracts.beanstalkDiamond;
     this.silo = await ethers.getContractAt('MockSiloFacet', this.diamond.address);
-    this.whitelist = await ethers.getContractAt('WhitelistFacet', this.diamond.address);
+    this.whitelist = await ethers.getContractAt('WhitelistFacet', this.diamond.address)
+    this.season = await ethers.getContractAt('SeasonFacet', this.diamond.address);;
     this.bdv = await ethers.getContractAt('BDVFacet', this.diamond.address);
 
     const SiloToken = await ethers.getContractFactory("MockToken");
@@ -49,53 +50,118 @@ describe('Whitelist', function () {
     await revertToSnapshot(snapshotId);
   });
 
-  it('reverts if not owner', async function () {
-    await expect(this.whitelist.connect(user).whitelistTokenWithEncodeType(
-      this.siloToken.address,
-      this.bdv.interface.getSighash('wellBdv'),
-      1,
-      1,
-      0
-    )).to.revertedWith('LibDiamond: Must be contract or owner');
-  });
-
-  it('reverts if invalid selector', async function () {
-    await expect(this.whitelist.connect(owner).whitelistTokenWithEncodeType(
-      this.siloToken.address,
-      '0x12345678',
-      1,
-      1,
-      1
-    )).to.revertedWith('Whitelist: Invalid selector');
-  });
-
-  it('reverts if already whitelisted', async function () {
-    this.whitelist.connect(owner).whitelistTokenWithEncodeType(
-      this.siloToken.address,
-      this.bdv.interface.getSighash('beanToBDV'),
-      1,
-      1,
-      1
-    )
+  describe('whitelist', async function () {
+    it('reverts if not owner', async function () {
+      await expect(this.whitelist.connect(user2).whitelistToken(
+        this.siloToken.address, 
+        this.silo.interface.getSighash("mockBDV(uint256 amount)"), 
+        '10000',
+        '1')).to.be.revertedWith('LibDiamond: Must be contract or owner')
+    })
 
 
-    await expect(this.whitelist.connect(owner).whitelistTokenWithEncodeType(
-      this.siloToken.address,
-      this.bdv.interface.getSighash('beanToBDV'),
-      1,
-      1,
-      1
-    )).to.revertedWith('Whitelist: Token already whitelisted');
-  });
+    it('whitelists token', async function () {
+      this.result = this.whitelist.connect(owner).whitelistToken(
+        this.siloToken.address, 
+        this.silo.interface.getSighash("mockBDV(uint256 amount)"), 
+        '10000',
+        '1')
+      const settings = await this.silo.tokenSettings(this.siloToken.address)
 
-  it('reverts if wrong encode type', async function () {
-    await expect(this.whitelist.connect(owner).whitelistTokenWithEncodeType(
-      this.siloToken.address,
-      this.bdv.interface.getSighash('wellBdv'),
-      1,
-      1,
-      2
-    )).to.revertedWith('Silo: Invalid encodeType');
-  });
 
+      expect(settings[0]).to.equal(this.silo.interface.getSighash("mockBDV(uint256 amount)"))
+
+      expect(settings[1]).to.equal(1)
+
+      expect(settings[2]).to.equal(10000)
+      await expect(this.result).to.emit(this.whitelist, 'WhitelistToken').withArgs(this.siloToken.address, 
+        this.silo.interface.getSighash("mockBDV(uint256 amount)"), 
+        1,
+        10000)
+    })
+
+    it('reverts on whitelisting same token again', async function () {
+      this.resultFirst = await this.whitelist.connect(owner).whitelistToken(
+        this.siloToken.address, 
+        this.silo.interface.getSighash("mockBDV(uint256 amount)"), 
+        '10000',
+        '1')
+      
+      await expect(this.whitelist.connect(owner).whitelistToken(
+          this.siloToken.address, 
+          this.silo.interface.getSighash("mockBDV(uint256 amount)"), 
+          '10000',
+          '1')).to.be.revertedWith("Whitelist: Token already whitelisted");
+    })
+
+    it('reverts on updating stalk per bdv per season for token that is not whitelisted', async function () {
+      await expect(this.whitelist.connect(owner).updateStalkPerBdvPerSeasonForToken(this.siloToken.address, 1)).to.be.revertedWith("Token not whitelisted");
+    });
+
+    it('reverts on whitelisting token with bad selector', async function () {
+      await expect(this.whitelist.connect(owner).whitelistToken(
+        this.siloToken.address,
+        '0x00000000',
+        '10000',
+        '1')).to.be.revertedWith("Whitelist: Invalid selector");
+    });
+
+    it('reverts on updating stalk per bdv per season for token that is not whitelisted', async function () {
+      await expect(this.whitelist.connect(owner).updateStalkPerBdvPerSeasonForToken(this.siloToken.address, 1)).to.be.revertedWith("Token not whitelisted");
+    });
+  })
+
+  describe('update stalk per bdv per season for token', async function () {
+    it('reverts if not owner', async function () {
+      await expect(this.whitelist.connect(user2).updateStalkPerBdvPerSeasonForToken(this.siloToken.address, 1)).to.be.revertedWith('LibDiamond: Must be contract or owner')
+    })
+
+    it('updates stalk per bdv per season', async function () {
+      //do initial whitelist so there's something to update
+      this.whitelist.connect(owner).whitelistToken(
+        this.siloToken.address, 
+        this.silo.interface.getSighash("mockBDV(uint256 amount)"), 
+        '10000',
+        '1')
+      this.result = this.whitelist.connect(owner).updateStalkPerBdvPerSeasonForToken(
+        this.siloToken.address, 
+        '50000'
+      )
+      const settings = await this.silo.tokenSettings(this.siloToken.address)
+
+      expect(settings[1]).to.equal(50000)
+      const currentSeason = await this.season.season()
+      await expect(this.result).to.emit(this.whitelist, 'UpdatedStalkPerBdvPerSeason').withArgs(this.siloToken.address, 50000, currentSeason)
+    })
+
+    it('reverts if wrong encode type', async function () {
+      await expect(this.whitelist.connect(owner).whitelistTokenWithEncodeType(
+        this.siloToken.address,
+        this.bdv.interface.getSighash('wellBdv'),
+        1,
+        1,
+        2
+      )).to.revertedWith('Silo: Invalid encodeType');
+    });
+  })
+
+  describe('dewhitelist', async function () {
+    it('reverts if not owner', async function () {
+      await expect(this.whitelist.connect(user2).dewhitelistToken(this.siloToken.address)).to.be.revertedWith('LibDiamond: Must be contract or owner')
+    })
+
+    it('dewhitelists token', async function () {
+      await this.whitelist.connect(owner).whitelistToken(
+        this.siloToken.address, 
+        this.silo.interface.getSighash("mockBDV(uint256 amount)"), 
+        '10000',
+        '1')
+      this.result = await this.whitelist.connect(owner).dewhitelistToken(this.siloToken.address)
+      const settings = await this.silo.tokenSettings(this.siloToken.address)
+      expect(settings[0]).to.equal('0x00000000')
+      expect(settings[1]).to.equal(0)
+      expect(settings[2]).to.equal(0)
+      await expect(this.result).to.emit(this.whitelist, 'DewhitelistToken').withArgs(this.siloToken.address)
+    })
+  })
 });
