af453f979c8d020c2531c7379e963d4bc35090eb
alecks <0xalecks@gmail.com>
2023-06-21 11:18:39 -0400

    eslint

--- projects/dex-ui/src/pages/Home.tsx
+++ projects/dex-ui/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
 import React from "react";
 import { RightArrow, RightArrowCircle } from "src/components/Icons";
 import styled from "styled-components";
