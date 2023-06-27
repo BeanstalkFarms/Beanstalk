3c63786680406defb694e99b5f5ba7e088aac2b4
Silo Chad <silochad@protonmail.com>
2023-06-27 15:42:08 -0500

    ui: refactor calls to legacy update function

--- projects/ui/src/lib/Txn/FarmSteps/silo/MowFarmStep.ts
+++ projects/ui/src/lib/Txn/FarmSteps/silo/MowFarmStep.ts
@@ -11,15 +11,19 @@ export class MowFarmStep extends FarmStep implements EstimatesGas {
     // TODO(silo-v3): .update doesn't exist anymore.
     // Rewrite this mow step to use `mow()` or `mowMultiple()` depending on
     // the tokens requested to be mown. this will require ui changes or defaults
-    private _tokens: Token[] = [],
+    private _token: Token
   ) {
     super(_sdk);
     this._account = _account;
+    this._token = this._sdk.tokens.BEAN;
   }
 
   async estimateGas(): Promise<ethers.BigNumber> {
     const { beanstalk } = this._sdk.contracts;
-    const gasAmount = await beanstalk.estimateGas.update(this._account);
+    const gasAmount = await beanstalk.estimateGas.mow(
+      this._account,
+      this._token.address
+    );
     console.debug(`[MowFarmStep][estimateGas]: `, gasAmount.toString());
 
     return gasAmount;
@@ -32,18 +36,19 @@ export class MowFarmStep extends FarmStep implements EstimatesGas {
 
     this.pushInput({
       input: async (_amountInStep) => ({
-        name: 'update',
+        name: 'mow',
         amountOut: _amountInStep,
         prepare: () => ({
           target: beanstalk.address,
-          callData: beanstalk.interface.encodeFunctionData('update', [
+          callData: beanstalk.interface.encodeFunctionData('mow', [
             this._account,
+            this._token.address,
           ]),
         }),
         decode: (data: string) =>
-          beanstalk.interface.decodeFunctionData('update', data),
+          beanstalk.interface.decodeFunctionData('mow', data),
         decodeResult: (result: string) =>
-          beanstalk.interface.decodeFunctionResult('update', result),
+          beanstalk.interface.decodeFunctionResult('mow', result),
       }),
     });
 
