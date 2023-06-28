10de4cecd77836900c4f34b56bd350b96939d294
Silo Chad <silochad@protonmail.com>
2023-06-27 19:36:21 -0500

    fix: MowFarmStep constructor defaults

--- projects/ui/src/lib/Txn/FarmSteps/silo/MowFarmStep.ts
+++ projects/ui/src/lib/Txn/FarmSteps/silo/MowFarmStep.ts
@@ -4,14 +4,17 @@ import { ethers } from 'ethers';
 import { FarmStep, EstimatesGas } from '~/lib/Txn/Interface';
 
 export class MowFarmStep extends FarmStep implements EstimatesGas {
+  private _token: Token;
+
+  private _account: string;
+
   constructor(
     _sdk: BeanstalkSDK,
-    private _account: string,
-
+    _account: string,
     // TODO(silo-v3): .update doesn't exist anymore.
     // Rewrite this mow step to use `mow()` or `mowMultiple()` depending on
     // the tokens requested to be mown. this will require ui changes or defaults
-    private _token: Token
+    _token?: Token
   ) {
     super(_sdk);
     this._account = _account;
@@ -20,6 +23,7 @@ export class MowFarmStep extends FarmStep implements EstimatesGas {
 
   async estimateGas(): Promise<ethers.BigNumber> {
     const { beanstalk } = this._sdk.contracts;
+
     const gasAmount = await beanstalk.estimateGas.mow(
       this._account,
       this._token.address
