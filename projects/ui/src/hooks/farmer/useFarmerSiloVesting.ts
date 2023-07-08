5bedc0a6efec033acd3316db9aa0de67f362a5d9
BeanSama <BeanSama@bean.farm>
2023-07-08 15:25:59 -0600

    update useFarmerSiloVesting hook

--- projects/ui/src/hooks/farmer/useFarmerSiloVesting.ts
+++ projects/ui/src/hooks/farmer/useFarmerSiloVesting.ts
@@ -6,22 +6,45 @@ import { useAppSelector } from '~/state';
 export const MAX_SILO_VESTING_BLOCKS = new BigNumber(10);
 
 export default function useFarmerSiloVesting() {
+  /// App State
   const morning = useAppSelector((s) => s._beanstalk.sun.morning);
-
-  /// TODO: Fix me with the new earned beans amount
-  const earnedBeans = useAppSelector((s) => s._farmer.silo.beans.earned);
+  const farmerSilo = useAppSelector((s) => s._farmer.silo);
+  const silo = useAppSelector((s) => s._beanstalk.silo);
 
   return useMemo(() => {
-    const isVesting = morning.isMorning && morning.index.lte(10);
+    const isVesting =
+      morning.isMorning && morning.index.lte(MAX_SILO_VESTING_BLOCKS);
 
     const remainingBlocks = isVesting
       ? MAX_SILO_VESTING_BLOCKS.minus(morning.index).plus(1)
       : ZERO_BN;
 
+    const totalStalkPrevSeason = silo.stalk.totalPrevSeason;
+    const farmerStalkPrevSeason = farmerSilo.stalk.totalPrevSeason;
+
+    const totalEarnedBeans = silo.beans.earned;
+    const totalEarnedBeansPrevSeason = silo.beans.earnedPrevSeason;
+
+    const deltaEarnedBeans = totalEarnedBeans.minus(totalEarnedBeansPrevSeason);
+
+    const farmerPrevSeasonStalkPct =
+      farmerStalkPrevSeason.div(totalStalkPrevSeason);
+
+    const earnedBeansThisSeason = deltaEarnedBeans.times(
+      farmerPrevSeasonStalkPct
+    );
+
     return {
-      amount: earnedBeans,
+      amount: earnedBeansThisSeason,
       isVesting,
       remainingBlocks,
     };
-  }, [earnedBeans, morning.index, morning.isMorning]);
+  }, [
+    silo.stalk.totalPrevSeason,
+    farmerSilo.stalk.totalPrevSeason,
+    morning.index,
+    morning.isMorning,
+    silo.beans.earned,
+    silo.beans.earnedPrevSeason,
+  ]);
 }
