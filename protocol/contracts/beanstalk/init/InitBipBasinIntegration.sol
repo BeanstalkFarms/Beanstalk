937b9b0cf1091a5f2869f132ab9bf753c7f91f77
publius <beanstalk.publius@protonmail.com>
2023-08-01 12:22:58 +0200

    add init script

---
+++ protocol/contracts/beanstalk/init/InitBipBasinIntegration.sol
@@ -0,0 +1,49 @@
+/*
+ SPDX-License-Identifier: MIT
+*/
+
+pragma solidity =0.7.6;
+pragma experimental ABIEncoderV2;
+
+import {AppStorage} from "../AppStorage.sol";
+import {C} from "contracts/C.sol";
+import {LibWhitelist} from "contracts/libraries/Silo/LibWhitelist.sol";
+
+
+/**
+ * @author Publius
+ * @title InitBipWellsIntegration runs the code for the Basin Integration
+**/
+
+interface IBDVFacet {
+    function wellBdv(address token, uint256 amount)
+        external
+        view
+        returns (uint256);
+}
+
+contract InitBipBasinIntegration {
+
+    AppStorage internal s;
+
+    uint32 constant private NEW_BEAN_SEEDS_PER_BDV = 3e6;
+    uint32 constant private NEW_BEAN_3CRV_SEEDS_PER_BDV = 3.25e6;
+    uint32 constant private BEAN_ETH_SEEDS_PER_BDV = 4.5e6;
+
+    uint32 constant private STALK_ISSUED_PER_BDV = 10000;
+    
+    
+    function init() external {
+        LibWhitelist.updateStalkPerBdvPerSeasonForToken(C.BEAN, NEW_BEAN_SEEDS_PER_BDV);
+        LibWhitelist.updateStalkPerBdvPerSeasonForToken(C.CURVE_BEAN_METAPOOL, NEW_BEAN_3CRV_SEEDS_PER_BDV);
+        LibWhitelist.whitelistToken(
+            C.BEAN_ETH_WELL,
+            IBDVFacet.wellBdv.selector,
+            STALK_ISSUED_PER_BDV,
+            BEAN_ETH_SEEDS_PER_BDV,
+            0x01
+        );
+
+        s.beanEthPrice = 1;
+    }
+}
--- protocol/scripts/bips.js
+++ protocol/scripts/bips.js
@@ -74,6 +74,33 @@ async function bipNewSilo(mock = true, account = undefined) {
     })
 }
 
+//BIP to integration Basin into Beanstalk
+async function bipBasinIntegration(mock = true, account = undefined) {
+  if (account == undefined) {
+    account = await impersonateBeanstalkOwner();
+    await mintEth(account.address);
+  }
+
+    await upgradeWithNewFacets({
+        diamondAddress: BEANSTALK,
+        facetNames: [
+          'DepotFacet',
+          'BDVFacet',
+          'ConvertFacet',
+          'SiloFacet',
+          'EnrootFacet',
+          'WhitelistFacet',
+          'SeasonFacet',
+          'MetadataFacet'
+        ],
+        initFacetName: 'InitBipBasinIntegration',
+        bip: false,
+        object: !mock, //if this is true, something would get spit out in the diamond cuts folder with all the data (due to gnosis safe deployment flow)
+        verbose: true,
+        account: account
+    })
+}
+
 async function mockBeanstalkAdmin(mock = true, account = undefined) {
     if (account == undefined) {
         account = await impersonateBeanstalkOwner()
@@ -120,4 +147,5 @@ exports.bip29 = bip29
 exports.bip30 = bip30
 exports.bip34 = bip34
 exports.bipNewSilo = bipNewSilo
+exports.bipBasinIntegration = bipBasinIntegration
 exports.mockBeanstalkAdmin = mockBeanstalkAdmin
