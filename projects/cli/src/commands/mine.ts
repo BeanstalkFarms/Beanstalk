a623d69702a7216e266dfdf96b633ae2ddfac842
alecks <0xalecks@gmail.com>
2023-09-24 13:34:29 -0400

    cli: add mine() command

---
+++ projects/cli/src/commands/mine.ts
@@ -0,0 +1,18 @@
+import { BeanstalkSDK } from "@beanstalk/sdk";
+import chalk from "chalk";
+import { table } from "table";
+
+export const mineBlocks = async (sdk: BeanstalkSDK, { amount }) => {
+  const numBlocks = (amount = 5000 ? 1 : amount);
+
+  async function mineBlocks(blockNumber) {
+    while (blockNumber > 0) {
+      blockNumber--;
+      await sdk.provider.send("evm_mine", []);
+    }
+  }
+
+  await mineBlocks(numBlocks);
+
+  console.log(`${chalk.bold.whiteBright("Mined ")} ${chalk.greenBright(numBlocks)} block(s)`);
+};
