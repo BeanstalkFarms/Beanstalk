76066733bcddb944b9af8f29acf150c02a5b8437
brendan <brendangsanderson@gmail.com>
2023-09-28 08:27:26 -0400

    add UsdOracle contract

---
+++ protocol/contracts/ecosystem/oracles/UsdOracle.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity =0.7.6;
+pragma experimental ABIEncoderV2;
+
+import {LibUsdOracle, LibEthUsdOracle} from "contracts/libraries/Oracle/LibUsdOracle.sol";
+
+/**
+ * @title UsdOracle
+ * @author Publius
+ * @notice Holds functions to query USD prices of tokens.
+ */
+contract UsdOracle {
+    
+    function getUsdPrice(address token) external view returns (uint256) {
+        return LibUsdOracle.getUsdPrice(token);
+    }
+
+    function getEthUsdPrice() external view returns (uint256) {
+        return LibEthUsdOracle.getEthUsdPrice();
+    }
+
+}
