25798a26d248df8f3176d2809f04a61a041f7b41
Brean0 <midoryia33@proton.me>
2023-10-09 11:09:52 +0900

    make libIncentive external again to pass tests

--- protocol/contracts/libraries/LibIncentive.sol
+++ protocol/contracts/libraries/LibIncentive.sol
@@ -59,7 +59,7 @@ library LibIncentive {
      * "step" functions have been executed.
      */
     function determineReward(uint256 initialGasLeft, uint256 blocksLate, uint256 beanEthPrice)
-        internal
+        external
         view
         returns (uint256)
     {
