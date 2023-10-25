5f1068794b3bd8cb960f0fa9bf57476b26789198
Brean0 <midoryia33@proton.me>
2023-10-25 15:44:14 -0500

    if statement checkForInvalidTimestampOrAnswer

--- protocol/contracts/libraries/Oracle/LibChainlinkOracle.sol
+++ protocol/contracts/libraries/Oracle/LibChainlinkOracle.sol
@@ -50,7 +50,9 @@ library LibChainlinkOracle {
         ) {
             // Check for an invalid roundId that is 0
             if (roundId == 0) return 0;
-            checkForInvalidTimestampOrAnswer(timestamp, answer);
+            if (checkForInvalidTimestampOrAnswer(timestamp, answer)) {
+                return 0;
+            }
             // Adjust to 6 decimal precision.
             return uint256(answer).mul(PRECISION).div(10 ** decimals);
         } catch {
