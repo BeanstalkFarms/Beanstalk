3a30f049e86f8d81237eec8b9714a0bd721dcf95
spacebean <wndgud00@gmail.com>
2023-11-14 19:02:49 -0700

    update hooks

--- projects/dex-ui/src/tokens/useSiloBalance.tsx
+++ projects/dex-ui/src/tokens/useSiloBalance.tsx
@@ -18,7 +18,7 @@ export const useSiloBalance = (token: Token) => {
         balance = TokenValue.ZERO;
       } else {
         const sdkLPToken = sdk.tokens.findByAddress(token.address);
-        const result = await sdk.silo.getBalance(sdkLPToken!, address, {source: DataSource.LEDGER});
+        const result = await sdk.silo.getBalance(sdkLPToken!, address, { source: DataSource.LEDGER });
         balance = result.amount;
       }
       return balance;
@@ -33,6 +33,54 @@ export const useSiloBalance = (token: Token) => {
       refetchInterval: 1000 * 30,
       refetchIntervalInBackground: false,
       refetchOnWindowFocus: "always"
+    }
+  );
+
+  return { data, isLoading, error, refetch, isFetching };
+};
+
+export const useSiloBalanceMany = (tokens: Token[]) => {
+  const { address } = useAccount();
+  const sdk = useSdk();
+
+  const queryClient = useQueryClient();
+
+  const { data, isLoading, error, refetch, isFetching } = useQuery<Record<string, TokenValue>, Error>(
+    ["silo", "balance", sdk, ...tokens.map((token) => token.symbol)],
+    async () => {
+      const resultMap: Record<string, TokenValue> = {};
+      if (!address) return resultMap;
+
+      /**
+       * For some reason the symbol sdk.tokens.findByAddress returns a
+       * token with symbol of BEANETH & the token symbol stored in the well is BEANWETHCP2w
+       *
+       * We find the silo balance using the token with symbol BEANETH &
+       * then use BEANWETHCP2w as the key in the resultMap
+       */
+      const _tokens = tokens
+        .map((token) => {
+          return {
+            token,
+            sdkToken: sdk.tokens.findByAddress(token.address)
+          };
+        })
+        .filter((tk) => tk.sdkToken !== undefined);
+
+      const result = await Promise.all(
+        _tokens.map((item) =>
+          sdk.silo
+            .getBalance(item.sdkToken!, address, { source: DataSource.LEDGER })
+            .then((result) => ({ token: item.token, amount: result.amount }))
+        )
+      );
+
+      result.forEach((val) => {
+        resultMap[val.token.symbol] = val.amount;
+        queryClient.setQueryData(["silo", "balance", sdk, val.token.symbol], val.amount);
+      });
+
+      return resultMap;
     }
   );
 
