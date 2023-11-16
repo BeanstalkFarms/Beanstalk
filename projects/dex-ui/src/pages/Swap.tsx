12bc75a452469dfb976c10cff6ff50a66ce3933a
spacebean <wndgud00@gmail.com>
2023-11-16 12:47:10 -0700

    update loading state for swap

--- projects/dex-ui/src/pages/Swap.tsx
+++ projects/dex-ui/src/pages/Swap.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { Page } from "src/components/Page";
 import { Title } from "src/components/PageComponents/Title";
+import SwapLoading from "src/components/Swap/SwapLoading";
 import { SwapRoot } from "src/components/Swap/SwapRoot";
+import { useWellTokens } from "src/tokens/useWellTokens";
+
+/**
+ * Normally we would not check the loading state at this level, but
+ * if we don't it'll cause errors in the SwapRoot component & it's children.
+ * It's simpler to render a separate loading component here instead of handling it
+ * everywhere else.
+ */
 
 export const Swap = () => {
+  const { isLoading } = useWellTokens();
+
   return (
     <Page>
       <Title title="Swap" />
-      <SwapRoot />
+      {isLoading ? <SwapLoading /> : <SwapRoot />}
     </Page>
   );
 };
--- projects/dex-ui/src/tokens/TokenProvider.tsx
+++ projects/dex-ui/src/tokens/TokenProvider.tsx
@@ -3,7 +3,6 @@ import React, { createContext, useContext } from "react";
 
 import { useWellTokens } from "src/tokens/useWellTokens";
 import { images } from "src/assets/images/tokens";
-import { Loading } from "src/components/Loading";
 import { Error } from "src/components/Error";
 
 const tokenMap: Record<string, Token> = {};
@@ -13,16 +12,16 @@ export const TokenProvider = ({ children }: { children: React.ReactNode }) => {
   const { data: tokens, isLoading, error } = useWellTokens();
 
   if (isLoading) {
-    return <Loading />
+    <></>;
   }
 
   if (error) {
-    return <Error message={error?.message} />
+    return <Error message={error?.message} />;
   }
 
   const add = (token: Token) => (tokenMap[token.symbol] = token);
 
-  for (const token of tokens! || []) {
+  for (const token of tokens || []) {
     let logo = images[token.symbol] ?? images.DEFAULT;
     token.setMetadata({ logo });
     add(token);
