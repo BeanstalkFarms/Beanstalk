c35f95a3f353e52a9ed910244278dd16e55c1784
spacebean <wndgud00@gmail.com>
2023-11-20 14:49:33 -0700

    well page loading

--- projects/dex-ui/src/components/Well/Chart/ChartSection.tsx
+++ projects/dex-ui/src/components/Well/Chart/ChartSection.tsx
@@ -9,20 +9,21 @@ import { TabButton } from "src/components/TabButton";
 import useWellChartData from "src/wells/useWellChartData";
 import { ChartContainer } from "./ChartStyles";
 import { BottomDrawer } from "src/components/BottomDrawer";
-import { size } from "src/breakpoints";
+import { mediaQuery, size } from "src/breakpoints";
+import { LoadingTemplate } from "src/components/LoadingTemplate";
 
 function timeToLocal(originalTime: number) {
   const d = new Date(originalTime * 1000);
   return Date.UTC(d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), d.getMinutes(), d.getSeconds(), d.getMilliseconds()) / 1000;
 }
 
-export const ChartSection: FC<{ well: Well }> = ({ well }) => {
+const ChartSectionContent: FC<{ well: Well }> = ({ well }) => {
   const [tab, setTab] = useState(0);
   const [showDropdown, setShowDropdown] = useState(false);
   const [timePeriod, setTimePeriod] = useState("week");
   const [dropdownButtonText, setDropdownButtonText] = useState("1 WEEK");
 
-  const { data: chartData, refetch, error, isLoading } = useWellChartData(well, timePeriod);
+  const { data: chartData, refetch, error, isLoading: chartDataLoading } = useWellChartData(well, timePeriod);
 
   const [liquidityData, setLiquidityData] = useState<any[]>([]);
   const [volumeData, setVolumeData] = useState<any[]>([]);
@@ -74,60 +75,63 @@ export const ChartSection: FC<{ well: Well }> = ({ well }) => {
   return (
     <Container id="chart-section">
       <DesktopRow>
-        <TabButton onClick={(e) => showTab(e, 0)} active={tab === 0} hover>
-          LIQUIDITY
-        </TabButton>
-        <TabButton onClick={(e) => showTab(e, 1)} active={tab === 1} hover>
-          VOLUME
-        </TabButton>
-        <FilterButton
-          onClick={() => {
-            setShowDropdown(!showDropdown);
-          }}
-        >
-          {dropdownButtonText} <ChevronDown width={6} />
-          <Dropdown enabled={showDropdown}>
-            <DropdownItem
-              stretch
-              hover
-              onClick={() => {
-                setChartRange("day");
-              }}
-            >
-              1 DAY
-            </DropdownItem>
-            <DropdownItem
-              stretch
-              hover
-              onClick={() => {
-                setChartRange("week");
-              }}
-            >
-              1 WEEK
-            </DropdownItem>
-            <DropdownItem
-              stretch
-              hover
-              onClick={() => {
-                setChartRange("month");
-              }}
-            >
-              1 MONTH
-            </DropdownItem>
-            <DropdownItem
-              stretch
-              hover
-              onClick={() => {
-                setChartRange("all");
-              }}
-            >
-              ALL
-            </DropdownItem>
-          </Dropdown>
-        </FilterButton>
+        <>
+          <TabButton onClick={(e) => showTab(e, 0)} active={tab === 0} hover>
+            LIQUIDITY
+          </TabButton>
+          <TabButton onClick={(e) => showTab(e, 1)} active={tab === 1} hover>
+            VOLUME
+          </TabButton>
+          <FilterButton
+            onClick={() => {
+              setShowDropdown(!showDropdown);
+            }}
+          >
+            {dropdownButtonText} <ChevronDown width={6} />
+            <Dropdown enabled={showDropdown}>
+              <DropdownItem
+                stretch
+                hover
+                onClick={() => {
+                  setChartRange("day");
+                }}
+              >
+                1 DAY
+              </DropdownItem>
+              <DropdownItem
+                stretch
+                hover
+                onClick={() => {
+                  setChartRange("week");
+                }}
+              >
+                1 WEEK
+              </DropdownItem>
+              <DropdownItem
+                stretch
+                hover
+                onClick={() => {
+                  setChartRange("month");
+                }}
+              >
+                1 MONTH
+              </DropdownItem>
+              <DropdownItem
+                stretch
+                hover
+                onClick={() => {
+                  setChartRange("all");
+                }}
+              >
+                ALL
+              </DropdownItem>
+            </Dropdown>
+          </FilterButton>
+        </>
       </DesktopRow>
       <MobileRow>
         <TabButton onClick={() => setChartTypeDrawerOpen(true)}>{tab === 0 ? "LIQUIDITY" : "VOLUME"}</TabButton>
+
         <BottomDrawer showDrawer={isChartTypeDrawerOpen} headerText={"View Chart"} toggleDrawer={setChartTypeDrawerOpen}>
           <DrawerRow
             onClick={() => {
@@ -144,9 +148,11 @@ export const ChartSection: FC<{ well: Well }> = ({ well }) => {
             VOLUME
           </DrawerRow>
         </BottomDrawer>
+
         <FilterButton onClick={() => setChartRangeDrawerOpen(true)}>
           {dropdownButtonText} <ChevronDown width={6} />
         </FilterButton>
+
         <BottomDrawer showDrawer={isChartRangeDrawerOpen} headerText={"Time Period"} toggleDrawer={setChartRangeDrawerOpen}>
           <DrawerRow
             onClick={() => {
@@ -178,15 +184,56 @@ export const ChartSection: FC<{ well: Well }> = ({ well }) => {
           </DrawerRow>
         </BottomDrawer>
       </MobileRow>
-      {error !== null && <ChartLoader>{`Error Loading Chart Data :(`}</ChartLoader>}
-      {isLoading && <ChartLoader>Loading Chart Data...</ChartLoader>}
-      {tab === 0 && !error && !isLoading && <Chart data={liquidityData} legend={"TOTAL LIQUIDITY"} />}
-      {tab === 1 && !error && !isLoading && <Chart data={volumeData} legend={"HOURLY VOLUME"} />}
+      {error !== null && <ChartError>{`Error Loading Chart Data :(`}</ChartError>}
+      {chartDataLoading && (
+        <ChartLoader>
+          <LoadingTemplate gap={4}>
+            <LoadingTemplate.Item width={100} height={24} />
+            <LoadingTemplate.Item width={150} height={24} />
+          </LoadingTemplate>
+        </ChartLoader>
+      )}
+      {tab === 0 && !error && !chartDataLoading && <Chart data={liquidityData} legend={"TOTAL LIQUIDITY"} />}
+      {tab === 1 && !error && !chartDataLoading && <Chart data={volumeData} legend={"HOURLY VOLUME"} />}
     </Container>
   );
 };
 
+export const ChartSection: FC<{ well: Well | undefined; loading?: boolean }> = ({ well, loading }) => {
+  if (!well || loading) {
+    return (
+      <Container id="chart-section-loading">
+        <DesktopRow>
+          <LoadingTabButton width={110.59}>{""}</LoadingTabButton>
+          <LoadingTabButton width={99.17} active>
+            {""}
+          </LoadingTabButton>
+          <LoadingFilterButton width={103.41}>{""}</LoadingFilterButton>
+        </DesktopRow>
+        <MobileRow>
+          <LoadingTabButton width={84.03}>{""}</LoadingTabButton>
+          <LoadingFilterButton width={103.41}>{""}</LoadingFilterButton>
+        </MobileRow>
+        <ChartLoader>
+          <LoadingTemplate gap={4}>
+            <LoadingTemplate.Item width={100} height={24} />
+            <LoadingTemplate.Item width={150} height={24} />
+          </LoadingTemplate>
+        </ChartLoader>
+      </Container>
+    );
+  }
+
+  return <ChartSectionContent well={well} />;
+};
+
 const ChartLoader = styled(ChartContainer)`
+  padding: 24px;
+  justify-content: flex-start;
+  box-sizing: border-box;
+`;
+
+const ChartError = styled(ChartContainer)`
   justify-content: center;
   align-items: center;
 `;
@@ -258,5 +305,28 @@ const FilterButton = styled.div`
   }
   @media (max-width: ${size.mobile}) {
     height: 40px;
+  }
+`;
+
+const LoadingTabButton = styled(TabButton)<{ width: number }>`
+  min-height: 48px;
+  min-width: ${(props) => props.width}px;
+
+  :hover {
+    cursor: default;
+  }
+`;
+
+const LoadingFilterButton = styled(FilterButton)<{ width: number }>`
+  min-height: 48px;
+  min-width: ${(props) => props.width}px;
+
+  :hover {
+    cursor: default;
+    background: white;
+  }
+
+  ${mediaQuery.sm.only} {
+    min-height: 40px;
   }
 `;
