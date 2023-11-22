f5d65fb331b010f7e63c428e06202537a4849794
spacebean <wndgud00@gmail.com>
2023-11-21 17:27:13 -0700

    add silo apy query

--- projects/dex-ui/src/wells/subgraphFetch.ts
+++ projects/dex-ui/src/wells/subgraphFetch.ts
@@ -2,9 +2,19 @@ import request from "graphql-request";
 import { type TypedDocumentNode } from "@graphql-typed-document-node/core";
 import { Settings } from "src/settings";
 
+type AdditionalSubgraphFetchOptions = {
+  useBeanstalkSubgraph?: boolean;
+};
+
 export function fetchFromSubgraphRequest<TResult, TVariables>(
   document: TypedDocumentNode<TResult, TVariables>,
-  variables: TVariables extends Record<string, never> ? undefined : TVariables
+  variables: TVariables extends Record<string, never> ? undefined : TVariables,
+  options?: AdditionalSubgraphFetchOptions
 ): () => Promise<TResult> {
-  return async () => request(Settings.SUBGRAPH_URL, document, variables ? variables : undefined);
+  return async () =>
+    request(
+      options?.useBeanstalkSubgraph ? Settings.BEANSTALK_SUBGRAPH_URL : Settings.SUBGRAPH_URL,
+      document,
+      variables ? variables : undefined
+    );
 }
