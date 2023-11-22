57814358919aa6eb3397f2604eef9d347e3b6b50
spacebean <wndgud00@gmail.com>
2023-11-21 17:27:57 -0700

    add fetch hooks

---
+++ projects/dex-ui/src/wells/useBeanstalkSiloAPYs.tsx
@@ -0,0 +1,55 @@
+import { useQuery } from "@tanstack/react-query";
+import { loadSiloAPYData } from "./apyFetcher";
+import { Well } from "@beanstalk/sdk/Wells";
+import { useCallback } from "react";
+import { useBeanstalkSiloWhitelist } from "./useBeanstalkSiloWhitelist";
+
+const useBeanstalkSiloAPYs = () => {
+  const { getSeedsWithWell } = useBeanstalkSiloWhitelist();
+
+  const query = useQuery(
+    ["wells", "APYs"],
+    async () => {
+      const data = await loadSiloAPYData();
+      return data;
+    },
+    {
+      staleTime: 1000 * 60,
+      refetchOnWindowFocus: false
+    }
+  );
+
+  const getSiloAPYWithWell = useCallback(
+    (well: Well | undefined) => {
+      const seeds = getSeedsWithWell(well);
+      if (!query.data || !seeds) return undefined;
+
+      const d = query.data;
+
+      switch (seeds) {
+        case 0:
+          return d.zeroSeedBeanAPY;
+        case 2:
+          return d.twoSeedBeanAPY;
+        case 3:
+          return d.threeSeedBeanAPY;
+        case 3.5:
+          return d.threePointTwoFiveSeedBeanAPY;
+        case 4:
+          return d.fourSeedBeanAPY;
+        case 4.5:
+          return d.fourPointFiveSeedBeanAPY;
+        default:
+          return undefined;
+      }
+    },
+    [getSeedsWithWell, query.data]
+  );
+
+  return {
+    ...query,
+    getSiloAPYWithWell
+  };
+};
+
+export default useBeanstalkSiloAPYs;
