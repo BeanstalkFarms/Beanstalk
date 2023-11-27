7de44cf4acde4371df5f922ea4f602cf3831bf3e
spacebean <wndgud00@gmail.com>
2023-11-26 18:26:16 -0700

    create useMultiFlowPumpTWAReserves hook + update whitelist hook

--- projects/dex-ui/src/wells/useBeanstalkSiloWhitelist.ts
+++ projects/dex-ui/src/wells/useBeanstalkSiloWhitelist.ts
@@ -1,13 +1,38 @@
-import { useCallback, useMemo } from "react";
+import { useMemo } from "react";
 import { Well } from "@beanstalk/sdk/Wells";
+
+const BEANETH = "0xbea0e11282e2bb5893bece110cf199501e872bad";
 
 const WHITELIST_MAP = {
   /// BEANWETHCP2w (BEANETH LP)
-  "0xbea0e11282e2bb5893bece110cf199501e872bad": {
+  [`${BEANETH}`]: {
     address: "0xBEA0e11282e2bB5893bEcE110cF199501e872bAd",
-    lpTokenAddress: "0xbea0e11282e2bb5893bece110cf199501e872bad",
+    /// better way to do this?
+    isMultiFlowPump: true,
     /// Can we make this mapping dynamic?
     seeds: 4.5
+  }
+};
+
+const functions = {
+  isWhitelisted: (well: Well | undefined) => {
+    if (!well) return false;
+    const wellAddress = well.address.toLowerCase();
+
+    return wellAddress in WHITELIST_MAP;
+  },
+  seedsWithWell: (well: Well | undefined) => {
+    const wellAddress = well?.address.toLowerCase();
+    const key = wellAddress as keyof typeof WHITELIST_MAP;
+    return WHITELIST_MAP?.[key]?.seeds || undefined;
+  },
+  getIsMultiPumpWell: (well: Well | undefined) => {
+    if (well) {
+      const wellAddress = well.address.toLowerCase();
+      const key = wellAddress as keyof typeof WHITELIST_MAP;
+      return WHITELIST_MAP?.[key]?.isMultiFlowPump || false;
+    }
+    return false;
   }
 };
 
@@ -15,18 +40,5 @@ const WHITELIST_MAP = {
 export const useBeanstalkSiloWhitelist = () => {
   const whitelistedAddresses = useMemo(() => Object.keys(WHITELIST_MAP), []);
 
-  const getIsWhitelisted = useCallback((well: Well | undefined) => {
-    if (!well) return false;
-    const wellAddress = well.address.toLowerCase();
-
-    return wellAddress in WHITELIST_MAP;
-  }, []);
-
-  const getSeedsWithWell = useCallback((well: Well | undefined) => {
-    const wellAddress = well?.address.toLowerCase();
-    const key = wellAddress as keyof typeof WHITELIST_MAP;
-    return WHITELIST_MAP?.[key]?.seeds || undefined;
-  }, []);
-
-  return { whitelist: whitelistedAddresses, getIsWhitelisted, getSeedsWithWell } as const;
+  return { whitelist: whitelistedAddresses, ...functions } as const;
 };
