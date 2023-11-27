d783e846ba4a813816e65044089e04c804ce967f
uncoolzero <107518216+uncoolzero@users.noreply.github.com>
2023-11-27 15:47:34 -0300

    Use junction

--- projects/sdk-wells/src/constants/addresses.ts
+++ projects/sdk-wells/src/constants/addresses.ts
@@ -11,5 +11,6 @@ export const addresses = {
   // Contracts
   DEPOT: Address.make("0xDEb0f00071497a5cc9b4A6B96068277e57A82Ae2"),
   PIPELINE: Address.make("0xb1bE0000C6B3C62749b5F0c92480146452D15423"),
-  WETH9: Address.make("0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2")
+  WETH9: Address.make("0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"),
+  JUNCTION: Address.make("0x737cad465b75cdc4c11b3e312eb3fe5bef793d96"),
 };
