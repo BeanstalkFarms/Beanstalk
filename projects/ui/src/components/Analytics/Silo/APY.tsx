31074923beda78b4930e86ff9ab952efe5f0b8bb
Cujowolf <cujowolf12@gmail.com>
2023-12-13 16:10:04 -0600

    Initial vAPY changes related to subgraph schema update

--- projects/ui/src/components/Analytics/Silo/APY.tsx
+++ projects/ui/src/components/Analytics/Silo/APY.tsx
@@ -5,6 +5,7 @@ import { LineChartProps } from '~/components/Common/Charts/LineChart';
 import SeasonPlot, {
   SeasonPlotBaseProps,
 } from '~/components/Common/Charts/SeasonPlot';
+import { SILO_WHITELIST } from '~/constants/tokens';
 import { SeasonalApyDocument, SeasonalApyQuery } from '~/generated/graphql';
 import { SnapshotData } from '~/hooks/beanstalk/useSeasonsQuery';
 
@@ -14,24 +15,29 @@ const formatValue = (value: number) => `${value.toFixed(2)}%`;
 const queryConfig = {
   variables: {
     season_gt: 6074,
+    token: SILO_WHITELIST[0][1].address,
   },
 };
 const lineChartProps: Partial<LineChartProps> = {
   yTickFormat: tickFormatPercentage,
 };
 const metricToKey = {
-  ZeroSeeds: 'zeroSeedBeanAPY',
-  ThreeSeeds: 'threeSeedBeanAPY',
-  ThreePointTwoFiveSeeds: 'threePointTwoFiveSeedBeanAPY',
-  FourPointFiveSeeds: 'fourPointFiveSeedBeanAPY',
+  Bean: SILO_WHITELIST[0][1].address,
+  Bean3Curve: SILO_WHITELIST[1][1].address,
+  BeanETHWell: SILO_WHITELIST[2][1].address,
+  UnripeBean: SILO_WHITELIST[3][1].address,
+  UnripeBeanETH: SILO_WHITELIST[4][1].address,
 };
 
 const metricTitles = {
-  ZeroSeeds: '0 Seeds Bean vAPY',
-  ThreeSeeds: '3 Seeds Bean vAPY',
-  ThreePointTwoFiveSeeds: '3.25 Seeds Bean vAPY',
-  FourPointFiveSeeds: '4.5 Seeds Bean vAPY',
+  Bean: 'Bean vAPY',
+  Bean3Curve: 'BEAN3CRV vAPY',
+  BeanETHWell: 'BEANETH Well vAPY',
+  UnripeBean: 'urBEAN vAPY',
+  UnripeBeanETH: 'urBEANETH Well vAPY',
 };
+
+console.log(SILO_WHITELIST);
 
 const APY: FC<{
   height?: SeasonPlotBaseProps['height'];
@@ -42,10 +48,8 @@ const APY: FC<{
     document={SeasonalApyDocument}
     getValue={useCallback(
       (season: SnapshotData<SeasonalApyQuery>) =>
-        new BigNumber(season[metricToKey[metric] as keyof typeof season])
-          .times(100)
-          .toNumber(),
-      [metric]
+        new BigNumber(season.beanAPY).times(100).toNumber(),
+      []
     )}
     formatValue={formatValue}
     StatProps={useMemo(
