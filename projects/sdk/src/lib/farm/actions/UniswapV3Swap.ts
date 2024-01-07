fc3b8b11c0e41dc40f6813dd8dc3c7e3f44fdbc9
uncoolzero <107518216+uncoolzero@users.noreply.github.com>
2024-01-07 00:26:57 -0300

    add clipboard

--- projects/sdk/src/lib/farm/actions/UniswapV3Swap.ts
+++ projects/sdk/src/lib/farm/actions/UniswapV3Swap.ts
@@ -1,10 +1,11 @@
-import { BigNumberish, BigNumber, ethers } from "ethers";
+import { BigNumberish, ethers } from "ethers";
 import { RunContext, RunMode, StepClass, Workflow } from "src/classes/Workflow";
 import { Token } from "src/classes/Token";
 import { AdvancedPipePreparedResult } from "src/lib/depot/pipe";
-import { FarmFromMode, FarmToMode } from "../types";
 import { deadlineSecondsToBlockchain } from "src/utils";
 import { TokenValue } from "@beanstalk/sdk-core";
+import { Clipboard } from "src/lib/depot";
+import { ClipboardSettings } from "src/types";
 
 export class UniswapV3Swap extends StepClass<AdvancedPipePreparedResult> {
   public name: string = "uniswapV3Swap";
@@ -15,6 +16,7 @@ export class UniswapV3Swap extends StepClass<AdvancedPipePreparedResult> {
     public readonly tokenOut: Token,
     public readonly recipient: string,
     public readonly deadline?: number,
+    public clipboard?: ClipboardSettings
   ) {
     super();
     this.transactionDeadline = deadline ? deadlineSecondsToBlockchain(deadline) : TokenValue.MAX_UINT256.toBlockchain();
@@ -27,10 +29,23 @@ export class UniswapV3Swap extends StepClass<AdvancedPipePreparedResult> {
       context.runMode !== RunMode.EstimateReversed // _forward
     );
 
+    if (!this.clipboard) {
+      const pipelineBeanWethSwapIndex = context.steps.findIndex(step => step.name === "pipelineBeanWethSwap");
+      // If the action before (happens when reverse estimating) or after this one is a BEAN -> WETH swap through Pipeline...
+      if (pipelineBeanWethSwapIndex >= 0 && Math.abs(pipelineBeanWethSwapIndex - context.step.index) === 1) {
+        // We use clipboard...
+        this.clipboard = {
+          // Then find the correct tag in the tag map
+          tag: Object.keys(context.tagMap).find(tag => context.tagMap[tag] === pipelineBeanWethSwapIndex)!, 
+          copySlot: 9, 
+          pasteSlot: 5
+        };
+      };
+    };
+
     const quoter = UniswapV3Swap.sdk.contracts.uniswapV3QuoterV2;
-    
-    let estimate: any;
-    estimate = await quoter.callStatic.quoteExactInputSingle({
+
+    const estimate = await quoter.callStatic.quoteExactInputSingle({
         tokenIn: tokenIn.address,
         tokenOut: tokenOut.address,
         amountIn: _amountInStep,
@@ -44,6 +59,7 @@ export class UniswapV3Swap extends StepClass<AdvancedPipePreparedResult> {
       prepare: () => {
         if (context.data.slippage === undefined) throw new Error("Exchange: slippage required");
         const minAmountOut = Workflow.slip(estimate[0], context.data.slippage);
+        const sqrtPriceX96After = estimate[1];
         if (!minAmountOut) throw new Error("Exhange: missing minAmountOut");
         return {
           target: UniswapV3Swap.sdk.contracts.uniswapV3Router.address,
@@ -55,8 +71,9 @@ export class UniswapV3Swap extends StepClass<AdvancedPipePreparedResult> {
             deadline: this.transactionDeadline,
             amountIn: _amountInStep,
             amountOutMinimum: minAmountOut,
-            sqrtPriceLimitX96: 0
-          }])
+            sqrtPriceLimitX96: sqrtPriceX96After
+          }]),
+          clipboard: this.clipboard ? Clipboard.encodeSlot(context.step.findTag(this.clipboard.tag), this.clipboard.copySlot, this.clipboard.pasteSlot) : undefined
         };
       },
       decode: (data: string) => UniswapV3Swap.sdk.contracts.uniswapV3Router.interface.decodeFunctionData("exactInputSingle", data),
