4bf7cad91bf4cbaf35858606f2a253834f902bab
uncoolzero <107518216+uncoolzero@users.noreply.github.com>
2024-01-24 12:03:32 -0300

    use drawer on mobile

--- projects/ui/src/components/Governance/Actions/ListOfVotes.tsx
+++ projects/ui/src/components/Governance/Actions/ListOfVotes.tsx
@@ -1,5 +1,13 @@
 import React, { useState } from 'react';
-import { Box, Link, Tab, Tabs, Typography } from '@mui/material';
+import {
+  Box,
+  Link,
+  Tab,
+  Tabs,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
 import { ArrowBackIosNew, ArrowForwardIos } from '@mui/icons-material';
 import BigNumber from 'bignumber.js';
 import { FC } from '~/types';
@@ -9,8 +17,9 @@ import { displayBN, trimAddress } from '~/util';
 import AddressIcon from '~/components/Common/AddressIcon';
 import { Proposal } from '~/util/Governance';
 import useProposalBlockData from '~/hooks/beanstalk/useProposalBlockData';
+import FolderMenu from '~/components/Nav/FolderMenu';
 
-const ListOfVotes: FC<{
+const VotesTable: FC<{
   proposal: Proposal;
   quorum: ReturnType<typeof useProposalBlockData>;
   votes: any;
@@ -31,21 +40,21 @@ const ListOfVotes: FC<{
           choices[index] = 'Against';
         } else if (choice.startsWith('Add')) {
           choices[index] = 'For';
-        };
+        }
       } else if (proposalType === 'BFCP-B') {
         if (choice.startsWith('Keep') || choice.startsWith("Don't")) {
           choices[index] = 'Against';
         } else if (choice.startsWith('Remove') || choice.startsWith('Reveal')) {
           choices[index] = 'For';
-        };
+        }
       } else if (proposalType === 'BFCP-C') {
         const remove1 = choice.replace('Extend ', '');
         const remove2 = remove1.replace("'s term", '');
         const remove3 = remove2.replace("' term", '');
         choices[index] = remove3;
-      };
+      }
     });
-  };
+  }
 
   const votesPerChoice: Array<any[]> = [];
 
@@ -62,7 +71,7 @@ const ListOfVotes: FC<{
             votesPerChoice[option - 1] = [vote];
           } else {
             votesPerChoice[option - 1].push(vote);
-          };
+          }
         });
       } else {
         if (!votesPerChoice[vote.choice - 1]) {
@@ -72,7 +81,7 @@ const ListOfVotes: FC<{
         };
       };
     });
-  };
+  }
 
   // Pagination
   const votesPerPage = 10;
@@ -97,119 +106,151 @@ const ListOfVotes: FC<{
               ))}
             </Tabs>
           </Row>
-          {votesPerChoice[tab] && votesPerChoice[tab].length > 0 ? (
-            <>
-              <Row
-                justifyContent="space-between"
-                alignItems="center"
-                sx={{ p: 2 }}
-              >
-                <Box flexGrow={1}>
-                  <Typography
-                    variant="h4"
-                    color="text.secondary"
-                    sx={{ fontWeight: 400 }}
-                  >
-                    Address
-                  </Typography>
-                </Box>
-                <Box>
-                  <Typography
-                    variant="h4"
-                    color="text.secondary"
-                    sx={{ fontWeight: 400 }}
-                  >
-                    Votes
-                  </Typography>
-                </Box>
-              </Row>
-              {votesPerChoice[tab].map(
-                (vote: any, index) =>
-                  index >= firstVoteOnPage &&
-                  index <= lastVoteOnPage && (
-                    <Row
-                      justifyContent="space-between"
-                      alignItems="center"
-                      sx={{ px: 2, pb: 1 }}
-                    >
-                      <Box display="flex" flexDirection="row" flexGrow={1}>
-                        <Link
-                          href={`https://snapshot.org/#/profile/${vote.voter}`}
-                          underline="hover"
-                        >
-                          <Box display="flex" gap={0.5}>
-                            <AddressIcon size={18} address={vote.voter} />
-                            <Typography
-                              variant="h4"
-                              color="text.secondary"
-                              sx={{ fontWeight: 400 }}
-                            >
-                              {trimAddress(vote.voter)}
-                            </Typography>
-                          </Box>
-                        </Link>
-                      </Box>
-                      <Box>{displayBN(BigNumber(vote.vp))}</Box>
-                    </Row>
-                  )
-              )}
-              {totalPages > 1 && (
+          <Box sx={{ px: 2, pt: 2, pb: 1, minHeight: '390px' }}>
+            {votesPerChoice[tab] && votesPerChoice[tab].length > 0 ? (
+              <>
                 <Row
-                  justifyContent="flex-end"
+                  justifyContent="space-between"
                   alignItems="center"
-                  gap={0.5}
-                  sx={{ px: 2, pb: 1 }}
+                  sx={{ pb: 2 }}
                 >
-                  <Box
-                    sx={{ cursor: 'pointer' }}
-                    onClick={() =>
-                      setCurrentPage(currentPage > 1 ? currentPage - 1 : 1)
-                    }
-                  >
-                    <ArrowBackIosNew
-                      sx={{
-                        color:
-                          currentPage === 1 ? 'text.disabled' : 'text.primary',
-                        fontSize: 14,
-                      }}
-                    />
+                  <Box flexGrow={1}>
+                    <Typography
+                      variant="h4"
+                      color="text.secondary"
+                      sx={{ fontWeight: 400 }}
+                    >
+                      Address
+                    </Typography>
                   </Box>
-                  <Box
-                    display="flex"
-                    justifyContent="center"
-                    sx={{ width: '80px' }}
-                  >{`${currentPage} of ${totalPages}`}</Box>
-                  <Box
-                    sx={{ cursor: 'pointer' }}
-                    onClick={() =>
-                      setCurrentPage(
-                        currentPage < totalPages ? currentPage + 1 : totalPages
-                      )
-                    }
-                  >
-                    <ArrowForwardIos
-                      sx={{
-                        color:
-                          currentPage === totalPages
-                            ? 'text.disabled'
-                            : 'text.primary',
-                        fontSize: 14,
-                      }}
-                    />
+                  <Box>
+                    <Typography
+                      variant="h4"
+                      color="text.secondary"
+                      sx={{ fontWeight: 400 }}
+                    >
+                      Votes
+                    </Typography>
                   </Box>
                 </Row>
-              )}
-            </>
-          ) : (
-            <Box sx={{ p: 2 }}>
-              <Typography color="text.tertiary" align="center">
-                There are no votes for this choice.
-              </Typography>
-            </Box>
-          )}
+                {votesPerChoice[tab].map(
+                  (vote: any, index) =>
+                    index >= firstVoteOnPage &&
+                    index <= lastVoteOnPage && (
+                      <Row
+                        justifyContent="space-between"
+                        alignItems="center"
+                        sx={{ pb: 1 }}
+                      >
+                        <Box display="flex" flexDirection="row" flexGrow={1}>
+                          <Link
+                            href={`https://snapshot.org/#/profile/${vote.voter}`}
+                            underline="hover"
+                          >
+                            <Box display="flex" gap={0.5}>
+                              <AddressIcon size={18} address={vote.voter} />
+                              <Typography
+                                variant="h4"
+                                color="text.secondary"
+                                sx={{ fontWeight: 400 }}
+                              >
+                                {trimAddress(vote.voter)}
+                              </Typography>
+                            </Box>
+                          </Link>
+                        </Box>
+                        <Box>{displayBN(BigNumber(vote.vp))}</Box>
+                      </Row>
+                    )
+                )}
+                {totalPages > 1 && (
+                  <Row position="absolute" bottom="10px" right="20px" gap={0.5}>
+                    <Box
+                      sx={{ cursor: 'pointer' }}
+                      onClick={() =>
+                        setCurrentPage(currentPage > 1 ? currentPage - 1 : 1)
+                      }
+                    >
+                      <ArrowBackIosNew
+                        sx={{
+                          color:
+                            currentPage === 1
+                              ? 'text.disabled'
+                              : 'text.primary',
+                          fontSize: 14,
+                        }}
+                      />
+                    </Box>
+                    <Box
+                      display="flex"
+                      justifyContent="center"
+                      sx={{ width: '80px' }}
+                    >{`${currentPage} of ${totalPages}`}</Box>
+                    <Box
+                      sx={{ cursor: 'pointer' }}
+                      onClick={() =>
+                        setCurrentPage(
+                          currentPage < totalPages
+                            ? currentPage + 1
+                            : totalPages
+                        )
+                      }
+                    >
+                      <ArrowForwardIos
+                        sx={{
+                          color:
+                            currentPage === totalPages
+                              ? 'text.disabled'
+                              : 'text.primary',
+                          fontSize: 14,
+                        }}
+                      />
+                    </Box>
+                  </Row>
+                )}
+              </>
+            ) : (
+              <Box
+                height={310}
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Typography color="text.tertiary" align="center">
+                  There are no votes for this choice.
+                </Typography>
+              </Box>
+            )}
+          </Box>
         </Box>
       )}
     </>
+  );
+};
+
+const ListOfVotes: FC<{
+  proposal: Proposal;
+  quorum: ReturnType<typeof useProposalBlockData>;
+  votes: any;
+}> = (props) => {
+  const proposal = props.proposal;
+  const quorum = props.quorum;
+  const votes = props.votes;
+
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('lg'));
+
+  return isMobile ? (
+    <FolderMenu
+      buttonContent={<>See List of Votes</>}
+      drawerContent={
+        <VotesTable proposal={proposal} quorum={quorum} votes={votes} />
+      }
+      hotkey=""
+      sx={{ width: '100%' }}
+    />
+  ) : (
+    <VotesTable proposal={proposal} quorum={quorum} votes={votes} />
   );
 };
 
