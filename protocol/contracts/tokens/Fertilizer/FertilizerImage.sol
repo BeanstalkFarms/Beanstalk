70cf3c57f1fd3243a900107247bd7209ba0b4723
nickkatsios <t8200071@aueb.gr>
2024-01-26 17:20:27 +0200

    Refactored and optimized fertilizer svgs. Reduced contract size by 41%

--- protocol/contracts/tokens/Fertilizer/FertilizerImage.sol
+++ protocol/contracts/tokens/Fertilizer/FertilizerImage.sol
@@ -22,19 +22,21 @@ contract FertilizerImage {
 
     address internal constant BEANSTALK = 0xC1E088fC1323b20BCBee9bd1B9fC9546db5624C5;
 
-    /////////////////////   CONSTANTS TO ASSEMBLE SVG   ////////////////////////////
+    //////////////////////   CONSTANTS TO ASSEMBLE SVG   ////////////////////////////
 
     string internal constant BASE_JSON_URI = "data:application/json;base64,";
 
-    string private constant BASE_SVG_ACTIVE =
-        'M164.47 327.241L28.6247 405.768L27.7471 184.217L163.596 105.658L164.47 327.241Z" fill="#3DAA47"/><path d="M118.059 354.077L76.9574 377.823L76.083 156.272L117.184 132.494L118.059 354.077Z" fill="#3DAA47"/><ellipse cx="113.247" cy="220.688" rx="38.7172" ry="38.7739" fill="#7F5533"/><ellipse cx="113.247" cy="220.688" rx="38.7172" ry="38.7739" fill="#7F5533"/><ellipse cx="70.0135" cy="236.844" rx="38.7172" ry="38.7739" fill="#7F5533"/><path d="M26.8247 184.242L27.6958 405.809L121.062 460.148L120.191 238.584L26.8247 184.242Z" fill="#3DB542"/><path d="M163.257 105.98L164.128 327.548L257.495 381.886L256.624 160.322L163.257 105.98Z" fill="#3DB542"/><ellipse cx="156.805" cy="198.715" rx="38.7172" ry="38.7739" fill="#7F5533"/><ellipse cx="198.103" cy="189.668" rx="38.7172" ry="38.7739" fill="#7F5533"/><path d="M256.898 381.609L121.052 460.136L120.175 238.585L256.024 160.025L256.898 381.609Z" fill="#6DCB60"/><path d="M210.486 408.445L169.385 432.19L168.51 210.639L209.612 186.861L210.486 408.445Z" fill="#3DAA47"/><path d="M240.901 364.949L136.494 425.337L136.171 267.859L240.579 207.508L240.901 364.949Z" fill="white"/><path d="M195.789 268.025C218.926 261.311 232.664 278.656 228.095 303.258C224.075 324.91 206.743 346.103 188.326 353.079C169.155 360.339 152.609 350.811 152.029 329.113C151.364 304.191 171.442 275.092 195.789 268.025Z" fill="#46B955"/><path d="M206.417 275.615L178.337 349.192C178.337 349.192 153.768 313.795 206.417 275.615Z" fill="white"/><path d="M183.39 343.977L202.951 293.061C202.951 293.061 226.782 310.25 183.39 343.977Z" fill="white"/><rect width="78.3284" height="68.4768" transform="matrix(0.996731 0.0807976 -0.0805627 0.99675 154.216 336.166)" fill="url(#pattern0)"/><defs><pattern id="pattern0" patternContentUnits="objectBoundingBox" width="1" height="1"><use xlink:href="#image0_10349_104998" transform="scale(0.00325733 0.00373134)"/';
+    // Start for all fert svgs
+    string private constant BASE_SVG_START = '<path d="M164.47 327.241 28.625 405.768l-.878-221.551 135.849-78.559.874 221.583Z" fill="#3DAA47"/><path d="m118.059 354.077-41.102 23.746-.874-221.551 41.101-23.778.875 221.583Z" fill="#3DAA47"/><path d="m26.825 184.242.87 221.567 93.367 54.339-.871-221.564-93.366-54.342Zm136.432-78.262.871 221.568 93.367 54.338-.871-221.564-93.367-54.342Z" fill="#3DB542"/>';
 
-    string private constant BASE_SVG_AVAILIBLE =
-        'M26.8247 184.244L27.6958 405.811L121.062 460.15L120.191 238.586L26.8247 184.244Z" fill="#3DB542"/><path d="M256.898 381.611L121.052 460.138L120.175 238.587L256.024 160.027L256.898 381.611Z" fill="#6DCB60"/><path d="M210.486 408.445L169.385 432.19L168.51 210.639L209.612 186.861L210.486 408.445Z" fill="#3DAA47"/><path d="M76.6343 162.915L211.999 84.1328L256.033 160.042L120.191 238.586L76.6343 162.915Z" fill="#81D672"/><path d="M124.966 134.97L165.59 110.969L209.621 186.875L168.523 210.64L124.966 134.97Z" fill="#46B955"/><path d="M212.125 47.9183L212.009 84.146L76.6151 162.912L76.7312 126.742C76.7312 124.71 75.3406 122.329 73.6016 121.285C72.7304 120.762 71.9206 120.762 71.3398 121.052L206.734 42.2864C207.314 41.9374 208.066 41.9956 208.937 42.519C210.673 43.5077 212.125 45.944 212.125 47.9183Z" fill="#6DCB60"/><path d="M165.713 74.7523L165.597 110.98L124.947 134.968L125.063 98.7986C125.063 96.7662 123.673 94.3848 121.934 93.3411C121.062 92.8177 120.253 92.8177 119.672 93.1085L160.322 69.1203C160.902 68.7714 161.654 68.8295 162.525 69.353C164.264 70.3385 165.713 72.778 165.713 74.7523Z" fill="#42A84C"/><path d="M73.5789 121.298C75.3179 122.303 76.7408 124.72 76.7376 126.723L76.6343 162.916L120.191 238.583L26.8247 184.244L70.346 159.226L70.4493 123.085C70.4525 121.085 71.8399 120.29 73.5789 121.298Z" fill="#2C9A2C"/><path d="M107.879 226.766L36.6201 185.565L72.3625 165.17L83.7905 184.964L107.879 226.766Z" fill="#6DCB60"/><path d="M81.3481 180.731L36.6201 185.565L72.3625 165.17L81.3481 180.731Z" fill="#81D672"/><path d="M240.901 364.949L136.494 425.337L136.171 267.859L240.579 207.508L240.901 364.949Z" fill="white"/><path d="M95.493 209.237C86.046 212.203 77.6476 219.874 73.8727 230.789C73.3759 232.378 71.1948 232.378 70.6011 230.789C67.3295 220.559 59.1957 212.513 49.0808 209.237C47.2966 208.639 47.2966 206.455 49.0808 205.86C59.1957 202.548 67.2553 194.354 70.6011 184.308C71.1948 182.619 73.3791 182.619 73.8727 184.308C77.6412 194.997 85.4362 202.503 95.493 205.86C97.1804 206.455 97.1804 208.639 95.493 209.237Z" fill="white"/><path d="M195.789 264.257C218.926 257.543 232.664 274.888 228.096 299.49C224.075 321.142 206.743 342.335 188.327 349.311C169.155 356.572 152.61 347.043 152.029 325.346C151.365 300.424 171.443 271.324 195.789 264.257Z" fill="#46B955"/><path d="M206.417 271.848L178.337 345.424C178.337 345.424 153.768 310.027 206.417 271.848Z" fill="white"/><path d="M183.39 340.21L202.952 289.293C202.952 289.293 226.782 306.483 183.39 340.21Z" fill="white"/><rect width="78.3284" height="68.4768" transform="matrix(0.996731 0.0807976 -0.0805627 0.99675 154.216 336.166)" fill="url(#pattern0)"/><defs><pattern id="pattern0" patternContentUnits="objectBoundingBox" width="1" height="1"><use xlink:href="#image0_10349_104960" transform="scale(0.00325733 0.00373134)"/';
+    // End for all fert svgs
+    string private constant BASE_SVG_END = '<path d="m256.898 381.609-135.846 78.527-.877-221.551 135.849-78.56.874 221.584Z" fill="#6DCB60"/><path d="m210.486 408.445-41.101 23.745-.875-221.551 41.102-23.778.874 221.584Z" fill="#3DAA47"/><path d="m240.901 364.949-104.407 60.387-.323-157.477 104.408-60.351.322 157.441Z" fill="#fff"/><path d="M195.789 268.025c23.137-6.714 36.875 10.631 32.306 35.233-4.02 21.652-21.352 42.845-39.769 49.821-19.171 7.26-35.717-2.268-36.297-23.966-.665-24.922 19.413-54.021 43.76-61.088Z" fill="#46B955"/><path d="m206.417 275.615-28.08 73.577s-24.569-35.397 28.08-73.577Zm-23.027 68.362 19.561-50.916s23.831 17.189-19.561 50.916Z" fill="#fff"/>';
 
-    string private constant BASE_SVG_USED =
-        'M164.47 327.241L28.6247 405.768L27.7471 184.217L163.596 105.658L164.47 327.241Z" fill="#3DAA47"/><path d="M118.059 354.077L76.9574 377.823L76.083 156.272L117.184 132.494L118.059 354.077Z" fill="#3DAA47"/><path d="M26.8247 184.242L27.6958 405.809L121.062 460.148L120.191 238.584L26.8247 184.242Z" fill="#3DB542"/><path d="M163.257 105.98L164.128 327.548L257.495 381.886L256.624 160.322L163.257 105.98Z" fill="#3DB542"/><path d="M256.898 381.609L121.052 460.136L120.175 238.585L256.024 160.025L256.898 381.609Z" fill="#6DCB60"/><path d="M210.486 408.445L169.385 432.19L168.51 210.639L209.612 186.861L210.486 408.445Z" fill="#3DAA47"/><path d="M240.901 364.949L136.494 425.337L136.171 267.859L240.579 207.508L240.901 364.949Z" fill="white"/><path d="M195.789 268.025C218.926 261.311 232.664 278.656 228.095 303.258C224.075 324.91 206.743 346.103 188.326 353.079C169.155 360.339 152.609 350.811 152.029 329.113C151.364 304.191 171.442 275.092 195.789 268.025Z" fill="#46B955"/><path d="M206.417 275.615L178.337 349.192C178.337 349.192 153.768 313.795 206.417 275.615Z" fill="white"/><path d="M183.39 343.977L202.951 293.061C202.951 293.061 226.782 310.25 183.39 343.977Z" fill="white"/><rect width="78.3284" height="68.4768" transform="matrix(0.996731 0.0807976 -0.0805627 0.99675 154.216 336.166)" fill="url(#pattern0)"/><defs><pattern id="pattern0" patternContentUnits="objectBoundingBox" width="1" height="1"><use xlink:href="#image0_10349_105031" transform="scale(0.00325733 0.00373134)"/';
+    // Top for the available fert svg
+    string private constant FERT_TOP_AVAILABLE = '<path d="M76.634 162.915 212 84.133l44.034 75.909-135.842 78.544-43.557-75.671Z" fill="#81D672"/><path d="m124.966 134.97 40.624-24.001 44.031 75.906-41.098 23.765-43.557-75.67Z" fill="#46B955"/><path d="m212.125 47.918-.116 36.228-135.394 78.766.116-36.17c0-2.032-1.39-4.413-3.13-5.457-.87-.523-1.68-.523-2.261-.233l135.394-78.766c.58-.349 1.332-.29 2.203.233 1.736.989 3.188 3.425 3.188 5.4Z" fill="#6DCB60"/><path d="m165.713 74.752-.116 36.228-40.65 23.988.116-36.17c0-2.032-1.39-4.413-3.129-5.457-.872-.523-1.681-.523-2.262-.232l40.65-23.989c.58-.349 1.332-.29 2.203.233 1.739.986 3.188 3.425 3.188 5.4Z" fill="#42A84C"/><path d="M73.579 121.298c1.739 1.005 3.162 3.422 3.159 5.425l-.104 36.193 43.557 75.667-93.366-54.339 43.521-25.018.103-36.141c.004-2 1.39-2.795 3.13-1.787Z" fill="#2C9A2C"/><path d="M107.879 226.766 36.62 185.565l35.742-20.395 11.428 19.794 24.089 41.802Z" fill="#6DCB60"/><path d="m81.348 180.731-44.728 4.834 35.742-20.395 8.986 15.561Z" fill="#81D672"/>  <path d="M95.493 209.237c-9.447 2.966-17.845 10.637-21.62 21.552-.497 1.589-2.678 1.589-3.272 0-3.272-10.23-11.405-18.276-21.52-21.552-1.784-.598-1.784-2.782 0-3.377 10.115-3.312 18.174-11.506 21.52-21.552.594-1.689 2.778-1.689 3.272 0 3.768 10.689 11.563 18.195 21.62 21.552 1.687.595 1.687 2.779 0 3.377Z" fill="#fff"/>';
 
+    // Top for the active fert svg
+    string private constant FERT_TOP_ACTIVE ='<ellipse cx="113.247" cy="220.688" rx="38.717" ry="38.774" fill="#7F5533"/><ellipse cx="113.247" cy="220.688" rx="38.717" ry="38.774" fill="#7F5533"/><ellipse cx="70.013" cy="236.844" rx="38.717" ry="38.774" fill="#7F5533"/><path d="m26.825 184.242.87 221.567 93.367 54.339-.871-221.564-93.366-54.342Zm136.432-78.262.871 221.568 93.367 54.338-.871-221.564-93.367-54.342Z" fill="#3DB542"/><ellipse cx="156.805" cy="198.715" rx="38.717" ry="38.774" fill="#7F5533"/><ellipse cx="198.103" cy="189.668" rx="38.717" ry="38.774" fill="#7F5533"/>';
 
     /** 
         * @dev imageURI returns the base64 encoded image URI representation of the Fertilizer
@@ -46,9 +48,18 @@ contract FertilizerImage {
         return svgToImageURI(generateImageSvg(_id, bpfRemaining));
     }
 
+    /////////////// FERTILIZER SVG ORDER ///////////////////
+    // SVG_HEADER
+    // BASE_SVG_START
+    // FERT_SVG_TOP (available, active)
+    // BASE_SVG_END
+    // SVG_PRE_NUMBER 
+    // BPF_REMAINING
+    // END OF SVG
+
     /**
         * @dev generateImageSvg assembles the needed components for the Fertilizer svg
-        * for use in the json fertilizer metadata
+        * For use in the on-chain json fertilizer metadata
         * @param _id - the id of the Fertilizer
         * @param bpfRemaining - the bpfRemaining of the Fertilizer
         * @return imageUri - the image URI representation of the Fertilizer
@@ -56,20 +67,22 @@ contract FertilizerImage {
     function generateImageSvg(uint256 _id, uint128 bpfRemaining) internal view returns (string memory) {
         return string(
             abi.encodePacked(
-                '<svg width="294" height="512" viewBox="0 0 294 512" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="', // BASE SVG START
-                getFertilizerStatusSvg(_id, bpfRemaining), // fertilizerStatus
-                '></pattern></defs><text font-family="sans-serif" font-size="20" x="20" y="490" fill="black" ><tspan dy="0" x="20"> ', // BASE SVG PRE NUMBER FOR BPF REMAINING
-                LibStrings.formatUintWith6DecimalsTo2(bpfRemaining), // bpfRemaining with 2 decimal places
-                " BPF Remaining </tspan></text></svg>" // BASE SVG END
+                '<svg width="294" height="512" viewBox="0 0 294 512" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">', // SVG HEADER
+                BASE_SVG_START, // BASE SVG START
+                getFertilizerStatusSvg(_id, bpfRemaining), // FERT_SVG_TOP (available, active)
+                BASE_SVG_END, // BASE SVG END
+                '<text font-family="sans-serif" font-size="20" x="20" y="490" fill="black" ><tspan dy="0" x="20">', // PRE NUMBER FOR BPF REMAINING
+                LibStrings.formatUintWith6DecimalsTo2(bpfRemaining), // BPF_REMAINING with 2 decimal places
+                " BPF Remaining </tspan></text></svg>" // END OF SVG
             )
         );
     }
 
     /**
-        * @dev Returns the correct svg for the Fertilizer status based on the bpfRemaining.
+        * @dev Returns the correct svg top for the Fertilizer status based on the bpfRemaining.
         * @param _id - the id of the Fertilizer
         * @param bpfRemaining - the bpfRemaining of the Fertilizer
-        * @return fertilizerStatusSvg an svg for the correct Fertilizer status
+        * @return fertilizerStatusSvg an svg top for the correct Fertilizer status
      */
     function getFertilizerStatusSvg(uint256 _id, uint128 bpfRemaining) internal view returns (string memory) {
 
@@ -77,12 +90,12 @@ contract FertilizerImage {
             uint128(_id)
         );
 
-        string memory fertilizerStatusSvg = BASE_SVG_AVAILIBLE;
+        string memory fertilizerStatusSvg = FERT_TOP_AVAILABLE;
 
         if (fertilizerSupply > 0) {
             fertilizerStatusSvg = bpfRemaining > 0
-                ? BASE_SVG_ACTIVE
-                : BASE_SVG_USED;
+                ? FERT_TOP_ACTIVE
+                : ''; // a used fert (bpfRemaining = 0) has no top
         }
 
         return fertilizerStatusSvg;
