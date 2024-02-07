ea7120e8449938426cb33ef32a83801beb4f08b4
uncoolzero <107518216+uncoolzero@users.noreply.github.com>
2024-02-07 19:38:38 -0300

    add ebips to netlify function

--- projects/ui/src/functions/proposal/proposal.ts
+++ projects/ui/src/functions/proposal/proposal.ts
@@ -5,6 +5,7 @@ import path from 'path';
 import { cors, rateLimit } from '../middleware';
 import { oldBipList } from './oldBipList';
 import { oldBipVoteData } from './oldBipVoteData';
+import { ebipList } from './ebipList';
 
 /**
  * Return BIP content for prior on-chain BIPs.
@@ -12,45 +13,95 @@ import { oldBipVoteData } from './oldBipVoteData';
 const _handler: Handler = async (event) => {
   try {
 
-    if (event.queryStringParameters?.getProposals === 'all') {
-      return {
-        statusCode: 200,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(oldBipList),
+    if (event.queryStringParameters?.getEbip) {
+      if (event.queryStringParameters?.getEbip === 'all') {
+        return {
+          statusCode: 200,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(ebipList),
+        };
+      };
+
+      const ebipNumber = Number(event.queryStringParameters?.getEbip);
+      if (ebipNumber) {
+        const ebipBody = await new Promise((resolve, reject) => {
+          fs.readFile(path.join(__dirname, `./ebips/ebip-${ebipNumber}.md`), 'utf8', (err, data) => {
+            if (err) {
+              return reject(err);
+            }
+            resolve(data);
+          });
+        });
+
+        const output = {
+          ...ebipList[ebipNumber],
+          body: ebipBody,
+        };
+    
+        if (!ebipBody) {
+          return {
+            statusCode: 404,
+          };
+        };
+    
+        return {
+          statusCode: 200,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(output),
+        };
       };
     };
 
-    const bipNumber = Number(event.queryStringParameters?.bip);
+    if (event.queryStringParameters?.getOldBip) {
+      if (event.queryStringParameters?.getOldBip === 'all') {
+        return {
+          statusCode: 200,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(oldBipList),
+        };
+      };
 
-    const proposalBody = await new Promise((resolve, reject) => {
-      fs.readFile(path.join(__dirname, `./bips/bip-${bipNumber}.md`), 'utf8', (err, data) => {
-        if (err) {
-          return reject(err);
+      const bipNumber = Number(event.queryStringParameters?.getOldBip);
+      if (bipNumber) {
+        const proposalBody = await new Promise((resolve, reject) => {
+          fs.readFile(path.join(__dirname, `./bips/bip-${bipNumber}.md`), 'utf8', (err, data) => {
+            if (err) {
+              return reject(err);
+            }
+            resolve(data);
+          });
+        });
+
+        const output = {
+          ...oldBipList[bipNumber],
+          body: proposalBody,
+          votes: oldBipVoteData[bipNumber]
+        };
+
+        if (!proposalBody) {
+          return {
+            statusCode: 404,
+          };
         }
-        resolve(data);
-      });
-    });
 
-    const output = {
-      ...oldBipList[bipNumber],
-      body: proposalBody,
-      votes: oldBipVoteData[bipNumber]
-    };
-
-    if (!proposalBody) {
-      return {
-        statusCode: 404,
+        return {
+          statusCode: 200,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(output),
+        };
       };
-    }
+    };
 
     return {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(output),
+      statusCode: 403
     };
 
   } catch (err) {
