45b9fa2370029706e7739708c93a427e2250c253
uncoolzero <107518216+uncoolzero@users.noreply.github.com>
2024-02-13 03:23:59 -0300

    add note about on chain governance

--- projects/ui/src/components/Governance/Actions/index.tsx
+++ projects/ui/src/components/Governance/Actions/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Link, Typography } from '@mui/material';
 import Vote from '~/components/Governance/Actions/Vote';
 import {
   Module,
@@ -20,21 +20,29 @@ const GovernanceActions: FC<{
   const isOldBip = props.proposal.id.startsWith("bip-");
   const isEbip = props.proposal.id.startsWith("ebip-");
 
-  if (isEbip || isOldBip) return null;
-
   return (
   <Box sx={{ position: 'sticky', top: 120 }}>
-    <Module>
-      <ModuleHeader>
-        <Typography variant="h4">Vote</Typography>
-      </ModuleHeader>
-      <ModuleContent>
-        <Vote proposal={props.proposal} quorum={props.quorum} />
-      </ModuleContent>
-    </Module>
-    <Module sx={{ marginTop: 2 }}>
-      <ListOfVotes proposal={props.proposal} quorum={props.quorum} />
-    </Module>
+    {isEbip || isOldBip ? (
+      <Module sx={{ p: 2 }}>
+        <Typography variant="h4">Beanstalk governance was on-chain before the April 2022 Governance exploit.</Typography>
+          <br/>
+        <Typography variant="h4">Read more <Link underline="hover" href="https://bean.money/beanstalk.pdf#subsection.14.3">here</Link>.</Typography>
+      </Module>
+    ) : (
+      <>
+        <Module>
+          <ModuleHeader>
+            <Typography variant="h4">Vote</Typography>
+          </ModuleHeader>
+          <ModuleContent>
+            <Vote proposal={props.proposal} quorum={props.quorum} />
+          </ModuleContent>
+        </Module>
+        <Module sx={{ marginTop: 2 }}>
+          <ListOfVotes proposal={props.proposal} quorum={props.quorum} />
+        </Module>
+      </>
+    )}
   </Box>
   )
 };
