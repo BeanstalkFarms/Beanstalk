a4fbe31f6da34f4afd30dc57dd5a021c0374597e
Soil King <157099073+soilking@users.noreply.github.com>
2024-04-22 18:15:55 -0700

    Update gauge handlers for updated events

--- projects/subgraph-beanstalk/src/utils/Germinating.ts
+++ projects/subgraph-beanstalk/src/utils/Germinating.ts
@@ -3,7 +3,7 @@ import { ZERO_BI } from "../../../subgraph-core/utils/Decimals";
 import { Germinating } from "../../generated/schema";
 
 export function loadOrCreateGerminating(address: Address, season: i32): Germinating {
-  const id = address.toHexString() + "-" + germinationCategory(season);
+  const id = address.toHexString() + "-" + germinationSeasonCategory(season);
   let germinating = Germinating.load(id);
   if (germinating == null) {
     germinating = new Germinating(id);
@@ -16,7 +16,13 @@ export function loadOrCreateGerminating(address: Address, season: i32): Germinat
   return germinating as Germinating;
 }
 
-export function tryLoadGerminating(address: Address): Array<Germinating | null> {
+export function loadGerminating(address: Address, enumValue: i32): Germinating {
+  const id = address.toHexString() + "-" + germinationEnumCategory(enumValue);
+  let germinating = Germinating.load(id);
+  return germinating as Germinating;
+}
+
+export function tryLoadBothGerminating(address: Address): Array<Germinating | null> {
   return [Germinating.load(address.toHexString() + "-ODD"), Germinating.load(address.toHexString() + "-EVEN")];
 }
 
@@ -24,6 +30,10 @@ export function deleteGerminating(germinating: Germinating): void {
   store.remove("Germinating", germinating.id);
 }
 
-function germinationCategory(season: i32): string {
+function germinationSeasonCategory(season: i32): string {
   return season % 2 == 0 ? "EVEN" : "ODD";
+}
+
+function germinationEnumCategory(enumValue: i32): string {
+  return enumValue == 0 ? "ODD" : "EVEN";
 }
