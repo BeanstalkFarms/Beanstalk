31a8835bdcfcfab410683bf88858e930a4728a5b
Brean0 <midoryia33@proton.me>
2024-05-18 15:59:16 -0500

    reseedField

--- protocol/contracts/beanstalk/init/reseed/L2/ReseedField.sol
+++ protocol/contracts/beanstalk/init/reseed/L2/ReseedField.sol
@@ -16,5 +16,43 @@ import {C} from "contracts/C.sol";
 contract ReseedField {
     AppStorage internal s;
 
-    function init() external {}
+    // emitted when a plot is migrated.
+    event MigratedPlot(address indexed account, uint256 indexed plotIndex, uint256 pods);
+
+    /**
+     * @notice Re-initializes the field.
+     * @param accounts The addresses of the farmers.
+     * @param podIndex The index of the pods.
+     * @param podAmounts The amounts of the pods.
+     * @param totalPods The total number of pods on L1.
+     * @param harvestable The number of harvestable pods on L1.
+     * @param harvested The number of harvested pods on L1.
+     */
+    function init(
+        address[] calldata accounts,
+        uint256[][] calldata podIndex,
+        uint256[][] calldata podAmounts,
+        uint256 totalPods,
+        uint256 harvestable,
+        uint256 harvested
+    ) external {
+        uint256 calculatedTotalPods;
+        for (uint i; i < accounts.length; i++) {
+            for (uint j; j < podIndex.length; i++) {
+                s.a[accounts[i]].field.plots[podIndex[i][j]] = podAmounts[i][j];
+
+                emit MigratedPlot(accounts[i], podIndex[i][j], podAmounts[i][j]);
+                calculatedTotalPods += podAmounts[i][j];
+            }
+        }
+
+        //  perform verfication:
+        require(calculatedTotalPods == totalPods, "ReseedField: totalPods mismatch");
+        require(totalPods >= harvestable, "ReseedField: harvestable mismatch");
+        require(harvestable >= harvested, "ReseedField: harvested mismatch");
+
+        s.f.pods = totalPods;
+        s.f.harvestable = harvestable;
+        s.f.harvested = harvested;
+    }
 }
