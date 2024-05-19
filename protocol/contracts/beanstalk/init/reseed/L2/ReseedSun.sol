b2ce174a2e92c9786d443b2bd79844d46e11606d
Brean0 <midoryia33@proton.me>
2024-05-19 13:30:43 -0500

    ReseedSun.

--- protocol/contracts/beanstalk/init/reseed/L2/ReseedSun.sol
+++ protocol/contracts/beanstalk/init/reseed/L2/ReseedSun.sol
@@ -6,6 +6,7 @@ pragma solidity ^0.8.20;
 pragma experimental ABIEncoderV2;
 
 import {AppStorage} from "contracts/beanstalk/AppStorage.sol";
+import {LibCases} from "contracts/libraries/LibCases.sol";
 import {C} from "contracts/C.sol";
 
 /**
@@ -14,7 +15,43 @@ import {C} from "contracts/C.sol";
  * @dev cases are re-initialized. Season is set to L1 state.
  */
 contract ReseedSun {
+    uint256 constant PERIOD = 3600;
+    uint256 constant TIMESTAMP = 0;
+    /**
+     * @notice Emitted when the AverageGrownStalkPerBdvPerSeason Updates.
+     */
+    event UpdateAverageStalkPerBdvPerSeason(uint256 newStalkPerBdvPerSeason);
+
+    /**
+     * @notice Emitted when the grownStalkToLP changes.
+     * @param season The current Season
+     * @param caseId The Weather case, which determines how the BeanToMaxLPGpPerBDVRatio is adjusted.
+     * @param absChange The absolute change in the BeanToMaxLPGpPerBDVRatio.
+     * @dev formula: L_n = L_n-1 +/- bL
+     */
+    event BeanToMaxLpGpPerBdvRatioChange(uint256 indexed season, uint256 caseId, int80 absChange);
+
     AppStorage internal s;
 
-    function init() external {}
+    function init(
+        uint32 season,
+        uint32 temperature,
+        uint128 averageGrownStalkPerBdvPerSeason,
+        uint128 beanToMaxLpGpPerBdvRatio
+    ) external {
+        s.season.current = season;
+        s.season.period = PERIOD;
+        s.season.timestamp = TIMESTAMP;
+        s.w.t = temperature;
+        s.seedGauge.averageGrownStalkPerBdvPerSeason = averageGrownStalkPerBdvPerSeason;
+        emit BeanToMaxLpGpPerBdvRatioChange(
+            s.season.current,
+            type(uint256).max,
+            int80(int128(beanToMaxLpGpPerBdvRatio))
+        );
+
+        s.seedGauge.beanToMaxLpGpPerBdvRatio = beanToMaxLpGpPerBdvRatio;
+        emit UpdateAverageStalkPerBdvPerSeason(averageGrownStalkPerBdvPerSeason);
+        LibCases.setCasesV2();
+    }
 }
