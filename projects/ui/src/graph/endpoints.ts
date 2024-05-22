63f12c429f1ecc91acf8e24feb7495fa1fb8d1d7
uncoolzero <107518216+uncoolzero@users.noreply.github.com>
2024-05-22 04:41:39 -0300

    update endpoints

--- projects/ui/src/graph/endpoints.ts
+++ projects/ui/src/graph/endpoints.ts
@@ -19,7 +19,7 @@ export const SUBGRAPH_ENVIRONMENTS: Record<SGEnvironments, SGEnvironment> = {
   [SGEnvironments.BF_PROD]: {
     name: 'Beanstalk Farms / Production',
     subgraphs: {
-      beanstalk: 'https://graph.node.bean.money/subgraphs/name/beanstalk',
+      beanstalk: 'https://graph.node.bean.money/subgraphs/name/beanstalk-dev',
       bean: 'https://graph.node.bean.money/subgraphs/name/bean',
       beanft: 'https://graph.node.bean.money/subgraphs/name/beanft',
     },
@@ -36,7 +36,7 @@ export const SUBGRAPH_ENVIRONMENTS: Record<SGEnvironments, SGEnvironment> = {
     name: 'Beanstalk Farms / Test',
     subgraphs: {
       beanstalk:
-        'https://graph.node.bean.money/subgraphs/name/beanstalk', // fixme 
+        'https://graph.node.bean.money/subgraphs/name/beanstalk-testing',
       bean: 'https://graph.node.bean.money/subgraphs/name/bean-testing',
       beanft: 'https://graph.node.bean.money/subgraphs/name/beanft-dev',
     },
