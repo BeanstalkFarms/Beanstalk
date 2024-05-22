e51eb755ac17e2dfcb8b71e80e146cd5413c56b8
spacebean <wndgud00@gmail.com>
2024-05-22 16:02:43 -0600

    feat: update typography approach

--- projects/dex-ui/src/components/Typography/Text.tsx
+++ projects/dex-ui/src/components/Typography/Text.tsx
@@ -1,6 +1,19 @@
 import { HTMLAttributes } from "react";
 import { BoxModelBase, BoxModelProps } from "src/utils/ui/styled";
-import { theme, FontWeight, FontColor, FontVariant, FontSize, CssProps } from "src/utils/ui/theme";
+import {
+  theme,
+  FontWeight,
+  FontColor,
+  FontVariant,
+  FontSize,
+  CssProps,
+  FontSizeStyle,
+  LineHeightStyle,
+  FontWeightStyle,
+  TextAlignStyle,
+  TextAlign,
+  FontColorStyle
+} from "src/utils/ui/theme";
 import styled from "styled-components";
 
 export interface TextProps extends HTMLAttributes<HTMLDivElement>, BoxModelProps, CssProps {
@@ -8,15 +21,17 @@ export interface TextProps extends HTMLAttributes<HTMLDivElement>, BoxModelProps
   $weight?: FontWeight;
   $color?: FontColor;
   $size?: FontSize;
-  $lineHeight?: number;
+  $lineHeight?: number | FontSize;
+  $align?: TextAlign;
 }
 
 export const Text = styled.div<TextProps>`
   ${(props) => theme.font.styles.variant(props.$variant || "s")}
-  ${(props) => (props.$size ? theme.font.styles.size(props.$size) : "")}
-  ${(props) => (props.$weight ? theme.font.styles.weight(props.$weight) : "")}
-  ${(props) => (props.$color ? theme.font.styles.color(props.$color || "text.primary") : "")}
-  ${(props) => (props.$css ? props.$css : "")}
-  ${(props) => (props.$lineHeight ? `line-height: ${props.$lineHeight}px;` : "")}
+  ${FontSizeStyle}
+  ${LineHeightStyle}
+  ${FontWeightStyle}
+  ${TextAlignStyle}
+  ${FontColorStyle}
   ${BoxModelBase}
+  ${(props) => (props.$css ? props.$css : "")}
 `;
--- projects/dex-ui/src/utils/ui/theme/theme.ts
+++ projects/dex-ui/src/utils/ui/theme/theme.ts
@@ -1,19 +1,16 @@
 import { getFontVariantStyles } from "src/components/Typography/components";
-import { THEME_COLORS, getFontColorStyles } from "./colors";
+import { THEME_COLORS } from "./colors";
 import { themeSpacing } from "./spacing";
-import { getFontSizeStyles, getFontWeightStyles, getTextAlignStyles } from "./font";
+import { getFontSize, getTextAlignStyles } from "./font";
 
 export const theme = {
   colors: THEME_COLORS,
   spacing: themeSpacing,
   font: {
     styles: {
-      size: getFontSizeStyles,
       variant: getFontVariantStyles,
-      weight: getFontWeightStyles,
-      color: getFontColorStyles,
       textAlign: getTextAlignStyles
     },
-    size: getFontSizeStyles
+    size: getFontSize
   }
 } as const;
