f7513e0f37a504760c359ccb1bf0613268b4acde
Brean0 <midoryia33@proton.me>
2024-05-23 17:31:34 -0500

    baa029ae0 initWhitelistTokens
    Reseed txt

--- protocol/reseed/reseed.js
+++ protocol/reseed/reseed.js
@@ -1,64 +1,53 @@
-const { reseed1 } = require('./reseed1.js')
-const { reseed2 } = require('./reseed2.js')
-const { reseed3 } = require('./reseed3.js')
-const { reseed4 } = require('./reseed4.js')
-const { reseed5 } = require('./reseed5.js')
-const { reseed6 } = require('./reseed6.js')
-const { reseed7 } = require('./reseed7.js')
+const { reseedDeployL2Beanstalk } = require("./reseedDeployL2Beanstalk.js");
+const { reseed1 } = require("./reseed1.js");
 
-
-const fs = require('fs')
-
+const fs = require("fs");
 
 async function printBeanstalk() {
-  console.log('\n')
-  console.log('')
-  const text = fs.readFileSync('./reseed/data/reseed.txt');
-  console.log(text.toString())
-  console.log('')
+  console.log("\n");
+  console.log("");
+  const text = fs.readFileSync("./reseed/data/reseed.txt");
+  console.log(text.toString());
+  console.log("");
 }
 
-let reseeds
-async function reseed(account, deployAccount=undefined, mock=true, log=false, start=1, end=2) {
-  if (mock && start == 1) {
-    await hre.network.provider.request({
-      method: "evm_setNextBlockTimestamp",
-      params: [ ~~(Date.now() / 1000)],
-    });
-  }
-  reseeds = [
-    0,
-    reseed1,
-    reseed2
-  ]
-  // if (mock) reseeds.push(replantMock)
+let reseeds;
+async function reseed(
+  account,
+  deployAccount = undefined,
+  mock = true,
+  log = false,
+  start = 0,
+  end = 1
+) {
+  reseeds = [reseed1, reseedDeployL2Beanstalk];
 
-  console.clear()
-  await printBeanstalk()
+  console.clear();
+  await printBeanstalk();
   for (let i = start; i < end; i++) {
-    printStage(i, end, mock, log)
-    await reseeds[i](account)
+    printStage(i, end, mock, log);
+    await reseeds[i](account);
   }
-  console.log("Reseed successful.")
+  console.log("Reseed successful.");
 }
 
 function getProcessString(processed, total) {
-  const max = 20
-  const eq = max * processed / total
-  const sp = max-eq
-  return `[${'='.repeat(eq)}${' '.repeat(sp)}]`
+  const max = 20;
+  const eq = (max * processed) / total;
+  const sp = max - eq;
+  return `[${"=".repeat(eq)}${" ".repeat(sp)}]`;
 }
 
 async function printStage(i, end, mock, log) {
   if (!log) {
-    console.clear()
-    printBeanstalk()
+    console.clear();
+    printBeanstalk();
   } else {
-    console.log('==============================================')
+    console.log("==============================================");
   }
-  console.log("Reseeding Beanstalk:")
-  console.log(`Mocks Enabled: ${mock}`)
-  console.log(`Stage ${i}/${end-1}: ${getProcessString(i, end-1)}`)
+  console.log("Reseeding Beanstalk:");
+  console.log(`Mocks Enabled: ${mock}`);
+  console.log(`Stage ${i}/${end - 1}: ${getProcessString(i, end - 1)}`);
 }
 
-exports.reseed = reseed
+exports.reseed = reseed;
--- protocol/reseed/reseed1.js
+++ protocol/reseed/reseed1.js
@@ -1,28 +1,70 @@
-const { upgradeWithNewFacets } = require('../scripts/diamond.js');
-const fs = require('fs')
-const { BEANSTALK } = require('../test/utils/constants.js');
-const SELECTORS = require('./data/beanstalkSelectors.json')
+const { upgradeWithNewFacets } = require("../scripts/diamond.js");
+const fs = require("fs");
+const { BEANSTALK } = require("../test/utils/constants.js");
+const SELECTORS = require("./data/beanstalkSelectors.json");
 
-async function reseed1 (account) {
-    beanstalkSelectors = []
-    console.log('-----------------------------------')
-    console.log('Reseed1: Initialize L2 migration\n')
-    // read beanstalk selectors: 
-    for (let i = 0; i < SELECTORS.length; i++) {
-      selectors = SELECTORS[i][1];
-      for(let j = 0; j < selectors.length; j++) {
-        beanstalkSelectors.push(selectors[j])
+/**
+ * @notice reseed1 initializes migration by:
+ * - removing all functionality from beanstalk except for diamond functionality, and internal balance support.
+ * - pausing beanstalk.
+ * - transferring bean LP tokens to BCM.
+ */
+async function reseed1(account) {
+  beanstalkSelectors = [];
+  console.log("-----------------------------------");
+  console.log("Reseed1: Initialize L2 migration\n");
+  // read beanstalk selectors:
+  let listOfWhitelistedFunctionSelectors = [
+    "0x1f931c1c", // diamondCut
+    "0xcdffacc6", // facetAddress
+    "0x52ef6b2c", // facetAddresses
+    "0xadfca15e", // facetFunctionSelectors
+    "0x7a0ed627", // facets
+    "0x01ffc9a7", // supportsInterface
+    "0x4e71e0c8", // claimOwnership
+    "0x8da5cb5b", // owner
+    "0x5f504a82", // ownerCandidate
+    "0xf2fde38b", // transferOwnership
+    "0xda3e3397", // approveToken
+    "0x0bc33ce4", // decreaseTokenAllowance
+    "0xfdb28811", // getAllBalance
+    "0xb6fc38f9", // getAllBalances
+    "0xd4fac45d", // getBalance
+    "0x6a385ae9", // getBalances
+    "0x4667fa3d", // getExternalBalance
+    "0xc3714723", // getExternalBalances
+    "0x8a65d2e0", // getInternalBalance
+    "0xa98edb17", // getInternalBalances
+    "0xb39062e6", // increaseTokenAllowance
+    "0xbc197c81", // onERC1155BatchReceived
+    "0xf23a6e61", // onERC1155Received
+    "0x7c516e94", // permitToken
+    "0x8e8758d8", // tokenAllowance
+    "0x1f351f6a", // tokenPermitDomainSeparator
+    "0x4edcab2d", // tokenPermitNonces
+    "0xd3f4ec6f", // transferInternalTokenFrom
+    "0x6204aa43", // transferToken
+    "0xbd32fac3", // unwrapEth
+    "0x1c059365" // wrapEth
+  ];
+  for (let i = 0; i < SELECTORS.length; i++) {
+    selectors = SELECTORS[i][1];
+    for (let j = 0; j < selectors.length; j++) {
+      // if selector[j] is not in the whitelist, add it to the list of selectors to remove
+      if (!listOfWhitelistedFunctionSelectors.includes(selectors[j])) {
+        beanstalkSelectors.push(selectors[j]);
       }
     }
-    await upgradeWithNewFacets({
-      diamondAddress: BEANSTALK,
-      facetNames: [],
-      facetsToRemove: beanstalkSelectors,
-      initFacetName: 'InitL2Migration',
-      bip: false,
-      verbose: true,
-      account: account
-    });
-    console.log('-----------------------------------')
+  }
+  await upgradeWithNewFacets({
+    diamondAddress: BEANSTALK,
+    facetNames: [],
+    facetsToRemove: beanstalkSelectors,
+    initFacetName: "InitL2Migration",
+    bip: false,
+    verbose: true,
+    account: account
+  });
+  console.log("-----------------------------------");
 }
-exports.reseed1 = reseed1
+exports.reseed1 = reseed1;
