8e69949fd76608fc95399e531082b6e8a61da497
spacebean <wndgud00@gmail.com>
2024-05-23 13:04:10 -0600

    feat: update CSS rules

--- projects/dex-ui/src/utils/ui/styled/flex-model.ts
+++ projects/dex-ui/src/utils/ui/styled/flex-model.ts
@@ -1,59 +1,14 @@
+import type { CSSProperties } from "react";
 import { css } from "styled-components";
 import { theme } from "../theme";
 
 type FlexModelDirection = "row" | "column" | "row-reverse" | "column-reverse";
 
-type FlexAlignment =
-  | "center"
-  | "start"
-  | "end"
-  | "flex-start"
-  | "flex-end"
-  | "self-start"
-  | "self-end"
-  | "baseline"
-  | "stretch"
-  | "space-between"
-  | "space-around"
-  | "space-evenly";
-
-type FlexJustifyContent =
-  | FlexAlignment
-  | "normal"
-  | "first baseline"
-  | "last baseline"
-  | "safe center"
-  | "unsafe center"
-  | "inherit"
-  | "initial"
-  | "unset";
-
-type FlexAlignItems =
-  | FlexAlignment
-  | "normal"
-  | "baseline"
-  | "first baseline"
-  | "last baseline"
-  | "safe center"
-  | "unsafe center"
-  | "inherit"
-  | "initial"
-  | "unset";
-
-type FlexAlignContent = FlexAlignment | "normal" | "inherit" | "initial" | "unset";
-
-type FlexWrapItems = "nowrap" | "wrap" | "wrap-reverse" | "inherit" | "initial" | "unset";
-
-type DisplayType = "flex" | "block" | "inline" | "inline-block" | "none" | "inline-flex";
-
 export type FlexModelProps = {
-  $display?: DisplayType;
-  $flex?: boolean;
+  $display?: CSSProperties["display"];
   $direction?: FlexModelDirection;
-  $alignItems?: FlexAlignItems;
-  $justifyContent?: FlexJustifyContent;
-  $alignContent?: FlexAlignContent;
-  $flexWrap?: FlexWrapItems;
+  $alignItems?: CSSProperties["alignItems"];
+  $justifyContent?: CSSProperties["justifyContent"];
   $gap?: number;
   $width?: string;
   $fullWidth?: boolean;
@@ -64,8 +19,6 @@ export const FlexBase = css<FlexModelProps>`
   flex-direction: ${(props) => props.$direction || "column"};
   ${(props) => (props.$alignItems ? `align-items: ${props.$alignItems};` : "")}
   ${(props) => (props.$justifyContent ? `justify-content: ${props.$justifyContent};` : "")}
-  ${(props) => (props.$alignContent ? `align-content: ${props.$alignContent};` : "")}
-  ${(props) => (props.$flexWrap ? `flex-wrap: ${props.$flexWrap};` : "")}
   ${(props) => (props.$gap ? `gap: ${theme.spacing(props.$gap)};` : "")}
   ${(props) => (props.$fullWidth ? "width: 100%;" : props.$width ? `width: ${props.$width};` : "")}
 `;
