67da5010328cc4353e5066ffb04db713eef454a4
Brean0 <midoryia33@proton.me>
2024-05-24 13:30:03 -0500

    reseed5: separate deposit jsons.

--- protocol/reseed/reseed5.js
+++ protocol/reseed/reseed5.js
@@ -2,19 +2,22 @@ const { upgradeWithNewFacets } = require("../scripts/diamond.js");
 const fs = require("fs");
 
 // Files
-const FARMER_DEPOSITS = "./reseed/data/r5-deposits.json";
+const BEAN_DEPOSITS = "./reseed/data/r5-bean_deposits.json";
+const BEAN_ETH_DEPOSITS = "./reseed/data/r5-bean_eth_deposits.json";
+const BEAN_WSTETH_DEPOSITS = "./reseed/data/r5-bean_wsteth_deposits.json";
+const BEAN_3CRV_DEPOSITS = "./reseed/data/r5-bean_3crv_deposits.json";
+const UR_BEAN_DEPOSITS = "./reseed/data/r5-ur_bean_deposits.json";
+const UR_BEANLP_DEPOSITS = "./reseed/data/r5-ur_beanlp_deposits.json";
 
 async function reseed5(account, L2Beanstalk) {
   console.log("-----------------------------------");
   console.log("reseed5: reissue deposits.\n");
-  const [
-    beanDeposits,
-    beanEthDeposits,
-    beanWstEthDeposits,
-    bean3CrvDeposits,
-    urBeanDeposits,
-    urBeanLpDeposits
-  ] = JSON.parse(await fs.readFileSync(FARMER_DEPOSITS));
+  const beanDeposits = JSON.parse(await fs.readFileSync(BEAN_DEPOSITS));
+  const beanEthDeposits = JSON.parse(await fs.readFileSync(BEAN_ETH_DEPOSITS));
+  const beanWstEthDeposits = JSON.parse(await fs.readFileSync(BEAN_WSTETH_DEPOSITS));
+  const bean3CrvDeposits = JSON.parse(await fs.readFileSync(BEAN_3CRV_DEPOSITS));
+  const urBeanDeposits = JSON.parse(await fs.readFileSync(UR_BEAN_DEPOSITS));
+  const urBeanLpDeposits = JSON.parse(await fs.readFileSync(UR_BEANLP_DEPOSITS));
   await upgradeWithNewFacets({
     diamondAddress: L2Beanstalk,
     facetNames: [],
