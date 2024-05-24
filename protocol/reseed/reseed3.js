de9fe8a8b9b1925b1c701cf57dc371dce770de76
Brean0 <midoryia33@proton.me>
2024-05-24 13:15:01 -0500

    reseed3: initialize field.

--- protocol/reseed/reseed3.js
+++ protocol/reseed/reseed3.js
@@ -1,7 +1,24 @@
+const { upgradeWithNewFacets } = require("../scripts/diamond.js");
+const fs = require("fs");
 
-async function reseed3(account) {
-  console.log('-----------------------------------')
-  console.log('reseed3: reissue plots.\n')
-  console.log('-----------------------------------')
+// Files
+const FARMER_PLOTS = "./reseed/data/r3-field.json";
+
+async function reseed3(account, L2Beanstalk) {
+  console.log("-----------------------------------");
+  console.log("reseed3: re-initialize the field and plots.\n");
+  const [accounts, podIndex, podAmounts, TOTAL_PODS, HARVESTABLE, HARVESTED] = JSON.parse(
+    await fs.readFileSync(FARMER_PLOTS)
+  );
+  await upgradeWithNewFacets({
+    diamondAddress: L2Beanstalk,
+    facetNames: [],
+    initFacetName: "ReseedField",
+    initArgs: [accounts, podIndex, podAmounts, TOTAL_PODS, HARVESTABLE, HARVESTED],
+    bip: false,
+    verbose: true,
+    account: account
+  });
+  console.log("-----------------------------------");
 }
-exports.reseed3 = reseed3
+exports.reseed3 = reseed3;
