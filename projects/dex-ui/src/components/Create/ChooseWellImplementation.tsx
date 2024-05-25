5908af8a39a643a1ff6a8348e4e72ecb76765c77
spacebean <wndgud00@gmail.com>
2024-05-24 20:32:19 -0600

    feat: update components

--- projects/dex-ui/src/components/Create/ChooseWellImplementation.tsx
+++ projects/dex-ui/src/components/Create/ChooseWellImplementation.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Box, Flex } from "src/components/Layout";
 import { Text } from "src/components/Typography";
-
 import { ToggleSwitch } from "src/components/ToggleSwitch";
 import { ButtonPrimary } from "src/components/Button";
 import { BEANETH_ADDRESS } from "src/utils/addresses";
@@ -16,7 +15,31 @@ import { useBoolean } from "src/utils/ui/useBoolean";
 import { AddressInputField } from "src/components/Common/Form";
 import { Form, useForm } from "react-hook-form";
 import { ethers } from "ethers";
-import { useCreateWell } from "../CreateWellProvider";
+import { useCreateWell } from "./CreateWellProvider";
+
+const ChooseWellImplementationContent = () => {
+  return (
+    <Flex $gap={3} $fullWidth>
+      <Text $variant="h2">Create a Well - Choose a Well Implementation</Text>
+      <Flex $direction="row" $alignItems="flex-start" $gap={8}>
+        <InstructionContainer>
+          <Text $color="text.secondary" $lineHeight="l">
+            Deploy a Well using Aquifer, a Well factory contract.
+          </Text>
+          <Text $color="text.secondary" $lineHeight="l">
+            It is recommended to use the Well.sol Well Implementation, but you&apos;re welcome to use a custom contract.
+          </Text>
+          <Text $color="text.secondary" $lineHeight="l">
+            Visit the documentation to learn more about Aquifers and Well Implementations.
+          </Text>
+        </InstructionContainer>
+        <ChooseWellImplementationForm />
+      </Flex>
+    </Flex>
+  );
+};
+
+export const ChooseWellImplementation = React.memo(ChooseWellImplementationContent);
 
 type WellInfoEntry = {
   name: string;
@@ -54,11 +77,11 @@ const entries: Record<string, WellInfoEntry> = {
   }
 };
 
-export type ICreateWellForm = {
+type ICreateWellForm = {
   wellImplementation: string;
 };
 
-export const ChooseWellImplementation = () => {
+const ChooseWellImplementationForm = () => {
   const { wellImplementation, setWellImplementation } = useCreateWell();
   const [usingCustomWell, { toggle, set }] = useBoolean();
 
@@ -196,15 +219,16 @@ export const ChooseWellImplementation = () => {
   );
 };
 
-{
-  /* </form> */
-}
 const MayLink = ({ url, children }: { url?: string; children: React.ReactNode }) => {
   if (url) {
     return <LinkFormWrapperInner to={url}>{children}</LinkFormWrapperInner>;
   }
   return children;
 };
+
+const InstructionContainer = styled(Flex).attrs({ $gap: 2 })`
+  max-width: 274px;
+`;
 
 const LinkFormWrapperInner = styled(Link).attrs({
   target: "_blank",
--- projects/dex-ui/src/pages/Create.tsx
+++ projects/dex-ui/src/pages/Create.tsx
@@ -1,48 +1,23 @@
 import React from "react";
 import { CreateWellProvider, useCreateWell } from "src/components/Create/CreateWellProvider";
-import { ChooseWellImplementation } from "src/components/Create/steps/ChooseWellImplementation";
-import { Flex } from "src/components/Layout";
-import { Page } from "src/components/Page";
-import { Text } from "src/components/Typography";
+import { ChooseWellImplementation } from "src/components/Create/ChooseWellImplementation";
 
-import styled from "styled-components";
+import { Page } from "src/components/Page";
+import { ChooseFunctionAndPump } from "src/components/Create/ChooseFunctionAndPump";
 
 export type CreateWellStep = "well-implementation" | "function-pump" | "name-symbol" | "preview";
 
 export const Create = () => {
+  const { step } = useCreateWell();
+
   return (
     <CreateWellProvider>
       <Page>
-        <Flex $gap={3} $fullWidth>
-          <Text $variant="h2">Create a Well - Choose a Well Implementation</Text>
-          <Flex $direction="row" $alignItems="flex-start" $gap={8}>
-            <InstructionContainer>
-              <Text $color="text.secondary" $lineHeight="l">
-                Deploy a Well using Aquifer, a Well factory contract.
-              </Text>
-              <Text $color="text.secondary" $lineHeight="l">
-                It is recommended to use the Well.sol Well Implementation, but you&apos;re welcome to use a custom contract.
-              </Text>
-              <Text $color="text.secondary" $lineHeight="l">
-                Visit the documentation to learn more about Aquifers and Well Implementations.
-              </Text>
-            </InstructionContainer>
-            <CreateSteps />
-          </Flex>
-        </Flex>
+        <>
+          {step === 0 && <ChooseWellImplementation />}
+          {step === 1 && <ChooseFunctionAndPump />}
+        </>
       </Page>
     </CreateWellProvider>
   );
 };
-
-const CreateSteps = () => {
-  const { step } = useCreateWell();
-
-  return <>{step === 0 && <ChooseWellImplementation />}</>;
-};
-
-const InstructionContainer = styled(Flex).attrs({
-  $gap: 2
-})`
-  max-width: 274px;
-`;
