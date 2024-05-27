71759e302032acbb821561e2d61a3ad986f6bd78
Brean0 <midoryia33@proton.me>
2024-05-26 20:27:53 -0700

    Add additional initializations.

--- protocol/contracts/beanstalk/init/InitReseed.sol
+++ protocol/contracts/beanstalk/init/InitReseed.sol
@@ -5,6 +5,7 @@
 pragma solidity ^0.8.20;
 pragma experimental ABIEncoderV2;
 import {AppStorage} from "../AppStorage.sol";
+import {C} from "contracts/C.sol";
 import {LibTractor} from "contracts/libraries/LibTractor.sol";
 import {LibDiamond} from "contracts/libraries/LibDiamond.sol";
 
@@ -15,7 +16,7 @@ import {LibDiamond} from "contracts/libraries/LibDiamond.sol";
 contract InitReseed {
     AppStorage internal s;
 
-    function init() external {
+    function init(uint32 season) external {
         s.paused = false;
         s.isFarm = 1;
         s.earnedBeans = 0;
@@ -24,5 +25,15 @@ contract InitReseed {
         LibDiamond.DiamondStorage storage ds = LibDiamond.diamondStorage();
         ds.supportedInterfaces[0xd9b67a26] = true; // ERC1155
         ds.supportedInterfaces[0x0e89341c] = true; // ERC1155Metadata
+
+        // season
+        s.season.current = season;
+        s.season.period = C.getSeasonPeriod();
+        s.season.timestamp = block.timestamp;
+        // set the start of the Season based on the number of seasons,
+        // rounding down to the nearest hour.
+        s.season.start =
+            ((s.season.timestamp / s.season.period) * s.season.period) -
+            (s.season.period * s.season.current);
     }
 }
