9120d4b3fad1c85ed04f604ebdcb7656d2ab0e71
Brean0 <midoryia33@proton.me>
2024-05-26 20:20:22 -0700

    add interfaces to initReseed.

--- protocol/contracts/beanstalk/init/InitReseed.sol
+++ protocol/contracts/beanstalk/init/InitReseed.sol
@@ -6,6 +6,7 @@ pragma solidity ^0.8.20;
 pragma experimental ABIEncoderV2;
 import {AppStorage} from "../AppStorage.sol";
 import {LibTractor} from "contracts/libraries/LibTractor.sol";
+import {LibDiamond} from "contracts/libraries/LibDiamond.sol";
 
 /**
  * @author Brean
@@ -19,5 +20,9 @@ contract InitReseed {
         s.isFarm = 1;
         s.earnedBeans = 0;
         LibTractor._tractorStorage().activePublisher = payable(address(1));
+
+        LibDiamond.DiamondStorage storage ds = LibDiamond.diamondStorage();
+        ds.supportedInterfaces[0xd9b67a26] = true; // ERC1155
+        ds.supportedInterfaces[0x0e89341c] = true; // ERC1155Metadata
     }
 }
