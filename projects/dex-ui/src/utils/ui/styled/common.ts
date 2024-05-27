6fa8c0fb557d78c5c49ed7b69dd2e98ab9419f8f
spacebean <wndgud00@gmail.com>
2024-05-27 13:18:44 -0600

    feat: dimension style props

--- projects/dex-ui/src/utils/ui/styled/common.ts
+++ projects/dex-ui/src/utils/ui/styled/common.ts
@@ -1,10 +1,37 @@
 import type { CSSProperties } from "react";
-import { css } from "styled-components";
+import { ThemedStyledProps, css } from "styled-components";
+
+const makeStyle = <T>(
+  props: ThemedStyledProps<T, any>,
+  cssKey: string,
+  propKey: string
+): string => {
+  const prop = props[propKey as keyof typeof props];
+  return prop ? `${cssKey}: ${prop};` : "";
+};
 
 export type DisplayStyleProps = {
   $display?: CSSProperties["display"];
 };
 
 export const BlockDisplayStyle = css<DisplayStyleProps>`
-  ${({ $display }) => ($display ? `display: ${$display};` : "")}
+  ${(p) => makeStyle(p, "display", "$display")}
+`;
+
+export type DimensionStyleProps = {
+  height?: CSSProperties["height"];
+  minHeight?: CSSProperties["minHeight"];
+  maxHeight?: CSSProperties["maxHeight"];
+  width?: CSSProperties["width"];
+  minWidth?: CSSProperties["minWidth"];
+  maxWidth?: CSSProperties["maxWidth"];
+};
+
+export const DimensionStyleProps = css<DimensionStyleProps>`
+  ${(p) => makeStyle(p, "height", "height")}
+  ${(p) => makeStyle(p, "min-height", "minHeight")}
+  ${(p) => makeStyle(p, "max-height", "maxHeight")}
+  ${(p) => makeStyle(p, "width", "width")}
+  ${(p) => makeStyle(p, "min-width", "minWidth")}
+  ${(p) => makeStyle(p, "max-width", "maxWidth")}
 `;
