de439d291c1adbd296869c9c7af5035c67f639fd
spacebean <wndgud00@gmail.com>
2024-05-26 20:07:35 -0600

    feat: address form UI

--- projects/dex-ui/src/components/Common/Form.tsx
+++ projects/dex-ui/src/components/Common/Form.tsx
@@ -3,16 +3,21 @@ import { theme } from "src/utils/ui/theme";
 import styled from "styled-components";
 import { LinksButtonText, Text } from "src/components/Typography";
 import { Flex } from "../Layout";
+import { SearchIcon } from "../Icons";
 
 export type AddressInputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   error?: string;
+  isSearch?: boolean;
 };
 
 export const AddressInputField = forwardRef<HTMLInputElement, AddressInputFieldProps>(
-  ({ error, ...props }, ref) => {
+  ({ error, isSearch, ...props }, ref) => {
     return (
       <Flex>
-        <StyledAddressInputField {...props} onChange={props.onChange} ref={ref} type="text" />
+        <Wrapper>
+          {isSearch ? <SearchIcon /> : null}
+          <StyledAddressInputField {...props} onChange={props.onChange} ref={ref} type="text" />
+        </Wrapper>
         {error && (
           <Text $color="error" $variant="xs" $mt={0.5}>
             {error}
@@ -23,13 +28,31 @@ export const AddressInputField = forwardRef<HTMLInputElement, AddressInputFieldP
   }
 );
 
-const StyledAddressInputField = styled.input`
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: ${theme.spacing(0.5)};
+  box-sizing: border-box;
+  align-items: center;
   border: 0.5px solid ${theme.colors.black};
   background: ${theme.colors.white};
-  ${LinksButtonText};
-  color: ${theme.colors.black};
   padding: ${theme.spacing(1, 1.5)};
-  box-sizing: border-box;
+
+  input {
+    ${LinksButtonText}
+    outline: none;
+    border: none;
+    width: 100%;
+    box-sizing: border-box;
+  }
+
+  svg {
+    margin-bottom: ${theme.spacing(0.25)};
+  }
+`;
+
+const StyledAddressInputField = styled.input`
+  color: ${theme.colors.black};
 
   ::placeholder {
     color: ${theme.colors.gray};
