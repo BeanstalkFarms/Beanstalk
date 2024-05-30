b1c598e9ff054423af6ef14b81475b5cd1d90aa0
spacebean <wndgud00@gmail.com>
2024-05-30 14:22:30 -0600

    feat: create useERC20Token hook

---
+++ projects/dex-ui/src/tokens/useERC20Token.ts
@@ -0,0 +1,156 @@
+import { BEANETH_ADDRESS, getIsValidEthereumAddress } from "../utils/addresses";
+import useSdk from "../utils/sdk/useSdk";
+import { ERC20Token } from "@beanstalk/sdk";
+import { useQuery } from "@tanstack/react-query";
+
+import { queryKeys } from "src/utils/query/queryKeys";
+import { erc20Abi as abi } from "viem";
+import { multicall } from "@wagmi/core";
+import { useWells } from "src/wells/useWells";
+import { images } from "src/assets/images/tokens";
+import { alchemy } from "src/utils/alchemy";
+import { config } from "src/utils/wagmi/config";
+
+export const USE_ERC20_TOKEN_ERRORS = {
+  notERC20Ish: "Invalid ERC20 Token Address"
+} as const;
+
+const getERC20Data = async (_address: string) => {
+  const address = _address as `0x{string}`;
+  const args: any[] = [];
+
+  const calls: any[] = [
+    { address, abi, functionName: "decimals", args },
+    { address, abi, functionName: "totalSupply", args }
+  ];
+
+  return multicall(config, { contracts: calls });
+};
+
+export const useERC20TokenWithAddress = (address: string | undefined = "") => {
+  const { data: wells = [] } = useWells();
+  const sdk = useSdk();
+
+  const lpTokens = wells.map((w) => w.lpToken).filter(Boolean) as ERC20Token[];
+  const isValidAddress = getIsValidEthereumAddress(address);
+  const sdkToken = sdk.tokens.findByAddress(address);
+
+  const {
+    data: tokenMetadata,
+    refetch: refetchTokenMetadata,
+    ...tokenMetadataQuery
+  } = useQuery({
+    queryKey: queryKeys.tokenMetadata(address),
+    queryFn: async () => {
+      console.log("metadata fetching!!!!", address);
+      const multiCallResponse = await getERC20Data(address);
+
+      // Validate as much as we can that this is an ERC20 token
+      if (multiCallResponse[0]?.error || multiCallResponse[1]?.error) {
+        throw new Error(USE_ERC20_TOKEN_ERRORS.notERC20Ish);
+      }
+
+      const metadata = await alchemy.core.getTokenMetadata(address);
+
+      return {
+        name: metadata?.name ?? "",
+        symbol: metadata?.symbol ?? "",
+        decimals: metadata?.decimals ?? undefined,
+        logo: metadata?.logo ?? images.DEFAULT
+      };
+    },
+    enabled: isValidAddress && !sdkToken,
+    // We never need to refetch this data
+    staleTime: Infinity,
+    refetchOnMount: false,
+    retry: false
+  });
+
+  const getTokenLogo = async (token: ERC20Token) => {
+    // in the sdk, BEAN_ETH is BEAN_ETH, but in dex-ui, it's BEANWETHCP2w
+    const symbol = token.address === BEANETH_ADDRESS ? "BEANWETHCP2w" : token.symbol;
+    let logo: string | undefined = token.logo ?? images[symbol];
+    if (logo) return logo;
+
+    if (tokenMetadata) {
+      logo = tokenMetadata.logo;
+    } else {
+      const tokenMetadata = await refetchTokenMetadata();
+      logo = tokenMetadata?.data?.logo ?? undefined;
+    }
+
+    return logo ?? images.DEFAULT;
+  };
+
+  const handleIsLPToken = async () => {
+    const lpToken = lpTokens.find((lp) => lp.address.toLowerCase() === address.toLowerCase());
+    if (!lpToken) return undefined;
+
+    if (!lpToken.logo) {
+      const logo = await getTokenLogo(lpToken);
+      lpToken.setMetadata({ logo: logo });
+    }
+    return lpToken;
+  };
+
+  const handleIsSdkERC20Token = async () => {
+    let token = sdk.tokens.findByAddress(address);
+    if (!token) return undefined;
+
+    if (!(token instanceof ERC20Token)) {
+      return Promise.reject(new Error(USE_ERC20_TOKEN_ERRORS.notERC20Ish));
+    }
+
+    if (!token.logo) {
+      const logo = await getTokenLogo(token);
+      token.setMetadata({ logo: logo });
+    }
+    return token;
+  };
+
+  const shouldRunWhenNonSdkToken = Boolean(!sdkToken && isValidAddress && tokenMetadata);
+  const shouldRunWhenSdkToken = Boolean(sdkToken && isValidAddress && lpTokens.length);
+
+  const erc20Query = useQuery({
+    queryKey: queryKeys.erc20TokenWithAddress(address),
+    queryFn: async () => {
+      let token: ERC20Token | undefined = undefined;
+      token = await handleIsLPToken();
+      if (token) return token;
+
+      token = await handleIsSdkERC20Token();
+      if (token) return token;
+
+      // The query have run if this we get to this point
+      const { decimals = 0, name = "", symbol = "", logo } = tokenMetadata ?? {};
+
+      if (!decimals || !name || !symbol) {
+        return undefined;
+      }
+
+      const erc20 = new ERC20Token(
+        sdk.chainId,
+        address.toLowerCase(),
+        decimals,
+        symbol.toString() ?? "",
+        {
+          name: name,
+          logo: logo,
+          displayName: name
+        },
+        sdk.providerOrSigner
+      );
+
+      return erc20;
+    },
+    enabled: shouldRunWhenNonSdkToken || shouldRunWhenSdkToken,
+    staleTime: Infinity
+  });
+
+  return {
+    ...erc20Query,
+    error: erc20Query.error || tokenMetadataQuery.error,
+    isError: erc20Query.isError || tokenMetadataQuery.isError,
+    isLoading: erc20Query.isLoading || tokenMetadataQuery.isLoading
+  };
+};
