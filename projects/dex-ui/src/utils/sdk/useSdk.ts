125aaabd950bac2b71201aef74fb3855f1355250
spacebean <wndgud00@gmail.com>
2024-05-31 13:18:22 -0600

    feat: create useAquifer

--- projects/dex-ui/src/utils/sdk/useSdk.ts
+++ projects/dex-ui/src/utils/sdk/useSdk.ts
@@ -2,11 +2,11 @@ import { BeanstalkSDK } from "@beanstalk/sdk";
 import { useContext, useMemo } from "react";
 import { BeanstalkSDKContext } from "src/utils/sdk/SdkProvider";
 
-export default function useSdk() {
-  const sdk: BeanstalkSDK = useContext(BeanstalkSDKContext);
-  if (!sdk) {
+export default function useSdk(): BeanstalkSDK {
+  const sdk = useContext(BeanstalkSDKContext);
+  if (!sdk.sdk) {
     throw new Error("Expected sdk to be used within BeanstalkSDK context");
   }
 
-  return useMemo(() => sdk, [sdk]);
+  return useMemo(() => sdk.sdk, [sdk.sdk]);
 }
