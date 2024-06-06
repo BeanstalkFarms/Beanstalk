4556c0640b106b99becf0a69dcfdeb41ac3edce5
spacebean <wndgud00@gmail.com>
2024-06-05 18:51:39 -0600

    feat: add token sorting

--- projects/dex-ui/src/components/Create/ChooseFunctionAndPump.tsx
+++ projects/dex-ui/src/components/Create/ChooseFunctionAndPump.tsx
@@ -15,7 +15,8 @@ import { CreateWellStepProps, useCreateWell } from "./CreateWellProvider";
 import { CreateWellFormProgress } from "./shared/CreateWellFormProgress";
 import { ComponentInputWithCustom } from "./shared/ComponentInputWithCustom";
 import { USE_ERC20_TOKEN_ERRORS, useERC20TokenWithAddress } from "src/tokens/useERC20Token";
-import { ERC20Token } from "@beanstalk/sdk";
+import { BeanstalkSDK, ERC20Token } from "@beanstalk/sdk";
+import useSdk from "src/utils/sdk/useSdk";
 
 const additionalOptions = [
   {
@@ -40,6 +41,8 @@ const optionalKeys = ["wellFunctionData", "pumpData"] as const;
 
 const ChooseFunctionAndPumpForm = () => {
   const { wellTokens, wellFunction, pump, setStep2, wellFunctionData, pumpData } = useCreateWell();
+  const sdk = useSdk();
+
   const [token1, setToken1] = useState<ERC20Token | undefined>(undefined);
   const [token2, setToken2] = useState<ERC20Token | undefined>(undefined);
 
@@ -69,9 +72,10 @@ const ChooseFunctionAndPumpForm = () => {
       if (!valid || !token1 || !token2) return;
       if (token1.address === token2.address) return; // should never be true, but just in case
 
-      setStep2({ ...values, token1, token2, goNext: true });
+      const [tk1, tk2] = sortTokens(sdk, [token1, token2]);
+      setStep2({ ...values, token1: tk1, token2: tk2, goNext: true });
     },
-    [setStep2, methods, token1, token2]
+    [sdk, setStep2, methods, token1, token2]
   );
 
   return (
@@ -275,6 +279,35 @@ const TokenAddressInputWithSearch = ({
     </>
   );
 };
+
+/**
+ * Sorts the tokens in the following manner:
+ *  - if tokens includes BEAN, BEAN is first
+ *  - if tokens includes WETH, WETH is last
+ * - otherwise, the token order is preserved
+ *
+ * this is so that pairs with BEAN are BEAN:X
+ * and pairs with WETH are X:WETH
+ *
+ * TODO: do this with wStETH
+ */
+const sortTokens = (sdk: BeanstalkSDK, tokens: ERC20Token[]) => {
+  if (tokens.length < 2) {
+    throw new Error("2 Tokens are required");
+  }
+
+  const wethAddress = sdk.tokens.WETH.address.toLowerCase();
+  const beanAddress = sdk.tokens.BEAN.address.toLowerCase();
+
+  return tokens.sort((a, b) => {
+    const addressA = a.address.toLowerCase();
+    const addressB = b.address.toLowerCase();
+    if (addressA === beanAddress || addressB === wethAddress) return -1;
+    if (addressB === beanAddress || addressA === wethAddress) return 1;
+    return 0;
+  });
+};
+
 
 const FoundTokenInfo = styled.div`
   display: flex;
