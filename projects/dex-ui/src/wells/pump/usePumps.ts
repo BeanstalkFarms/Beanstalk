364afd0ba44bfb81407ef278b9d82cfd16ec0022
spacebean <wndgud00@gmail.com>
2024-06-06 12:12:16 -0600

    feat: new hooks + bytes utils

---
+++ projects/dex-ui/src/wells/pump/usePumps.ts
@@ -0,0 +1,24 @@
+import { useMemo } from "react";
+import { Pump } from "@beanstalk/sdk-wells";
+
+import { useWells } from "src/wells/useWells";
+
+export const usePumps = () => {
+  const { data: wells } = useWells();
+
+  return useMemo(() => {
+    if (!wells || !wells.length) return;
+
+    const pumpMap: Record<string, Pump> = {};
+
+    for (const well of wells) {
+      for (const pump of well.pumps || []) {
+        const pumpAddress = pump.address.toLowerCase();
+        if (pumpAddress in pumpMap) continue;
+        pumpMap[pumpAddress] = pump;
+      }
+    }
+
+    return Object.values(pumpMap);
+  }, [wells]);
+};
