d507572ef6ac0b672307e74eb1f35a8f7f4234b2
uncoolzero <107518216+uncoolzero@users.noreply.github.com>
2024-06-25 00:22:30 -0300

    retry system, error state, error handling

--- projects/ui/src/components/Analytics/AdvancedChart.tsx
+++ projects/ui/src/components/Analytics/AdvancedChart.tsx
@@ -37,6 +37,7 @@ const AdvancedChart: FC<{ isMobile?: boolean }> = ({ isMobile = false }) => {
   const [dialogOpen, showDialog, hideDialog] = useToggle();
   const [queryData, setQueryData] = useState<QueryData[][]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<boolean>(false);
 
   useMemo(() => {
     async function getSeasonData(getAllData?: boolean) {
@@ -44,79 +45,88 @@ const AdvancedChart: FC<{ isMobile?: boolean }> = ({ isMobile = false }) => {
       const output: any[] = [];
       const timestamps = new Map();
 
-      try {
-        for (let i = 0; i < selectedCharts.length; i += 1) {
-          const chartId = selectedCharts[i];
-          const queryConfig = chartSetupData[chartId].queryConfig;
-          const document = chartSetupData[chartId].document;
-          const entity = chartSetupData[chartId].documentEntity;
+      const maxRetries = 8
+      for (let retries = 0; retries < maxRetries; retries += 1) {
+        console.debug('[AdvancedChart] Fetching data...');
+        try {
+          for (let i = 0; i < selectedCharts.length; i += 1) {
+            const chartId = selectedCharts[i];
+            const queryConfig = chartSetupData[chartId].queryConfig;
+            const document = chartSetupData[chartId].document;
+            const entity = chartSetupData[chartId].documentEntity;
 
-          const currentSeason = season.toNumber();
+            const currentSeason = season.toNumber();
 
-          const iterations = getAllData ? Math.ceil(currentSeason / 1000) + 1 : 1;
-          for (let j = 0; j < iterations; j += 1) {
-            const startSeason = getAllData ? currentSeason - j * 1000 : 999999999;
-            if (startSeason <= 0) continue;
-            promises.push(
-              apolloClient
-                .query({
-                  ...queryConfig,
-                  query: document,
-                  variables: {
-                    ...queryConfig?.variables,
-                    first: 1000,
-                    season_lte: startSeason,
-                  },
-                  notifyOnNetworkStatusChange: true,
-                  fetchPolicy: 'no-cache', // Hitting the network every time is MUCH faster than the cache
-                })
-                .then((r) => {
-                  r.data[entity].forEach((seasonData: any) => {
-                    if (seasonData?.season && seasonData.season) {
-                      if (!output[chartId]?.length) {
-                        output[chartId] = [];
-                      }
-                      if (!timestamps.has(seasonData.season)) {
-                        timestamps.set(
-                          seasonData.season,
-                          Number(seasonData[chartSetupData[chartId].timeScaleKey])
-                        );
-                      };
-                      // Some charts will occasionally return two seasons as having the 
-                      // same timestamp, here we ensure we only have one datapoint per timestamp
-                      if (timestamps.get(seasonData.season + 1) !== timestamps.get(seasonData.season)
-                        && timestamps.get(seasonData.season - 1) !== timestamps.get(seasonData.season)
-                      ) {
-                        const formattedTime = timestamps.get(seasonData.season);
-                        const formattedValue = chartSetupData[
-                          chartId
-                        ].valueFormatter(
-                          seasonData[chartSetupData[chartId].priceScaleKey]
-                        );
-                        if (formattedTime > 0) {
-                          output[chartId][seasonData.season] = {
-                            time: formattedTime,
-                            value: formattedValue,
-                            customValues: {
-                              season: seasonData.season
-                            }
+            const iterations = getAllData ? Math.ceil(currentSeason / 1000) + 1 : 1;
+            for (let j = 0; j < iterations; j += 1) {
+              const startSeason = getAllData ? currentSeason - j * 1000 : 999999999;
+              if (startSeason <= 0) continue;
+              promises.push(
+                apolloClient
+                  .query({
+                    ...queryConfig,
+                    query: document,
+                    variables: {
+                      ...queryConfig?.variables,
+                      first: 1000,
+                      season_lte: startSeason,
+                    },
+                    notifyOnNetworkStatusChange: true,
+                    fetchPolicy: 'no-cache', // Hitting the network every time is MUCH faster than the cache
+                  })
+                  .then((r) => {
+                    r.data[entity].forEach((seasonData: any) => {
+                      if (seasonData?.season && seasonData.season) {
+                        if (!output[chartId]?.length) {
+                          output[chartId] = [];
+                        }
+                        if (!timestamps.has(seasonData.season)) {
+                          timestamps.set(
+                            seasonData.season,
+                            Number(seasonData[chartSetupData[chartId].timeScaleKey])
+                          );
+                        };
+                        // Some charts will occasionally return two seasons as having the 
+                        // same timestamp, here we ensure we only have one datapoint per timestamp
+                        if (timestamps.get(seasonData.season + 1) !== timestamps.get(seasonData.season)
+                          && timestamps.get(seasonData.season - 1) !== timestamps.get(seasonData.season)
+                        ) {
+                          const formattedTime = timestamps.get(seasonData.season);
+                          const formattedValue = chartSetupData[
+                            chartId
+                          ].valueFormatter(
+                            seasonData[chartSetupData[chartId].priceScaleKey]
+                          );
+                          if (formattedTime > 0) {
+                            output[chartId][seasonData.season] = {
+                              time: formattedTime,
+                              value: formattedValue,
+                              customValues: {
+                                season: seasonData.season
+                              }
+                            };
                           };
                         };
                       };
-                    };
-                  });
-                })
-            );
-          }
-        }
-        await Promise.all(promises);
-        output.forEach((dataSet, index) => {
-          output[index] = dataSet.filter(Boolean);
-        });
-        setQueryData(output);
-      } catch (e) {
-        console.debug('[AdvancedChart] Failed to fetch data');
-        console.error(e);
+                    });
+                  })
+              );
+            }
+          };
+          await Promise.all(promises);
+          output.forEach((dataSet, index) => {
+            output[index] = dataSet.filter(Boolean);
+          });
+          setQueryData(output);
+          console.debug('[AdvancedChart] Fetched data successfully!');
+          break;
+        } catch (e) {
+          console.debug('[AdvancedChart] Failed to fetch data.');
+          console.error(e);
+          if (retries === maxRetries - 1) {
+            setError(true);
+          };
+        };
       };
     };
 
@@ -274,7 +284,20 @@ const AdvancedChart: FC<{ isMobile?: boolean }> = ({ isMobile = false }) => {
             >
               <CircularProgress variant="indeterminate" />
             </Box>
-          ) : (
+          ) : 
+          error ? (
+            <Box
+              sx={{
+                display: 'flex',
+                height: '100%',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              Error fetching data
+            </Box>
+          ) :
+          (
             selectedCharts.length > 0 ? (
               <ChartV2
                 formattedData={queryData}
