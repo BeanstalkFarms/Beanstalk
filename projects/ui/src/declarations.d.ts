dc5a17ef7546855c8ac57e7157074587609b9c6d
Spacebean <wndgud00@gmail.com>
2024-07-15 11:28:02 +0200

    feat: declare types in UI

--- projects/ui/src/declarations.d.ts
+++ projects/ui/src/declarations.d.ts
@@ -1,12 +1,15 @@
-import { TokenValue, BigNumber, ContractTransaction } from "@beanstalk/sdk-core";
+import { TokenValue } from '@beanstalk/sdk';
+import { BigNumber, ContractTransaction } from 'ethers';
 
-declare module "@beanstalk/sdk-core" {
+declare module '@beanstalk/sdk-core' {
   interface Token {
     isUnripe: boolean;
     rewards?: { stalk: TokenValue; seeds: TokenValue | null };
     getStalk(bdv?: TokenValue): TokenValue;
     getSeeds(bdv?: TokenValue): TokenValue;
-    approveBeanstalk(amount: TokenValue | BigNumber): Promise<ContractTransaction>;
+    approveBeanstalk(
+      amount: TokenValue | BigNumber
+    ): Promise<ContractTransaction>;
   }
 
   namespace Token {
