2e5f7ce8113e295e2d9d8c2cda71fba0a00250da
nickkatsios <nickkatsios0@gmail.com>
2024-08-01 15:59:05 +0300

    clean up salt debugging code

--- protocol/contracts/beanstalk/init/reseed/L2/ReseedBean.sol
+++ protocol/contracts/beanstalk/init/reseed/L2/ReseedBean.sol
@@ -44,7 +44,7 @@ contract ReseedBean {
     string internal constant BEAN_NAME = "Bean";
     string internal constant BEAN_SYMBOL = "BEAN";
     bytes32 internal constant BEAN_SALT = 
-        0xd1a0060ba708bc4bcd3da6c37efa8dedf015fb70b115b16c5e1ca208f6ba0715;
+        0x0000000000000000000000000000000000000000000000000000000000000000;
     // UNRIPE_BEAN parameters.
     string internal constant UNRIPE_BEAN_NAME = "Unripe Bean";
     string internal constant UNRIPE_BEAN_SYMBOL = "urBEAN";
@@ -107,14 +107,6 @@ contract ReseedBean {
 
         // deploy new unripe lp contract.
         BeanstalkERC20 urBeanLPERC20 = deployUnripeLP(unripeLpSupply);
-       
-        address beanAddress = address(bean);
-        address urBeanAddress = address(urBeanERC20);
-        address urBeanLPAddress = address(urBeanLPERC20);
-
-        console.log("Bean address: %s", beanAddress);
-        console.log("Unripe Bean address: %s", urBeanAddress);
-        console.log("Unripe LP address: %s", urBeanLPAddress);
 
         // wells are deployed as ERC1967Proxies in order to allow for future upgrades.
 
@@ -134,11 +126,6 @@ contract ReseedBean {
     }
 
     function deployBean(uint256 supply) internal returns (BeanstalkERC20) {
-        console.log("Inside deployBean");
-        console.log("msg.sender: %s", msg.sender);
-        console.log("address(this): %s", address(this));
-        console.log("BEAN_NAME: %s", BEAN_NAME);
-        console.log("BEAN_SYMBOL: %s", BEAN_SYMBOL);
         BeanstalkERC20 bean = new BeanstalkERC20{salt: BEAN_SALT}(
             address(this),
             BEAN_NAME,
