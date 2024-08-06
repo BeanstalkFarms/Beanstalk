d87991776174ebf6be7e70980dc68c2c5c75d49f
nickkatsios <nickkatsios0@gmail.com>
2024-08-06 18:16:36 +0300

    Deploy the rest of the wells, clean up and simulate mainnet token addresses

--- protocol/contracts/beanstalk/init/reseed/L2/ReseedBean.sol
+++ protocol/contracts/beanstalk/init/reseed/L2/ReseedBean.sol
@@ -94,35 +94,35 @@ contract ReseedBean {
         0x0000000000000000000000000000000000000000000000000000000000000004;
     string internal constant BEAN_WSTETH_NAME = "BEAN:WSTETH Constant Product 2 Upgradeable Well";
     string internal constant BEAN_WSTETH_SYMBOL = "U-BEANWSTETHCP2w";
-    address internal constant WSTETH = address(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1);
+    address internal constant WSTETH = address(0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0);
 
     // BEAN_WEETH parameters.
     bytes32 internal constant BEAN_WEETH_SALT =
         0x0000000000000000000000000000000000000000000000000000000000000005;
     string internal constant BEAN_WEETH_NAME = "BEAN:WEETH Constant Product 2 Upgradeable Well";
     string internal constant BEAN_WEETH_SYMBOL = "U-BEANWEETHCCP2w";
-    address internal constant WEETH = address(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1);
+    address internal constant WEETH = address(0xCd5fE23C85820F7B72D0926FC9b05b43E359b7ee);
 
     // BEAN_WBTC parameters.
     bytes32 internal constant BEAN_WBTC_SALT =
         0x0000000000000000000000000000000000000000000000000000000000000004;
     string internal constant BEAN_WBTC_NAME = "BEAN:WBTC Constant Product 2 Upgradeable Well";
     string internal constant BEAN_WBTC_SYMBOL = "U-BEANWBTCCP2w";
-    address internal constant WBTC = address(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1);
+    address internal constant WBTC = address(0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599);
 
     // BEAN_USDC parameters.
     bytes32 internal constant BEAN_USDC_SALT =
         0x0000000000000000000000000000000000000000000000000000000000000005;
     string internal constant BEAN_USDC_NAME = "BEAN:USDC Stable 2 Upgradeable Well";
     string internal constant BEAN_USDC_SYMBOL = "U-BEANUSDCS2w";
-    address internal constant USDC = address(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1);
+    address internal constant USDC = address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
 
     // BEAN_USDT parameters.
     bytes32 internal constant BEAN_USDT_SALT =
         0x0000000000000000000000000000000000000000000000000000000000000003;
     string internal constant BEAN_USDT_NAME = "BEAN:USDT Stable 2 Upgradeable Well";
     string internal constant BEAN_USDT_SYMBOL = "U-BEANUSDTS2w";
-    address internal constant USDT = address(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1);
+    address internal constant USDT = address(0xdAC17F958D2ee523a2206206994597C13D831ec7);
 
     /**
      * @notice deploys bean, unripe bean, unripe lp, and wells.
@@ -147,7 +147,7 @@ contract ReseedBean {
         // deploy new unripe lp contract.
         deployUnripeLP(unripeLpSupply);
         // wells are deployed as ERC1967Proxies in order to allow for future upgrades.
-        deployUpgradableWells();
+        deployUpgradableWells(address(bean));
     }
 
     function deployBean(uint256 supply) internal returns (BeanstalkERC20) {
@@ -192,46 +192,107 @@ contract ReseedBean {
         string memory symbol
     ) internal {
         
-        // Bore upgradeable well.
+        // Encode well data
         (bytes memory immutableData, bytes memory initData) =
             encodeWellDeploymentData(AQUIFER, tokens, wellFunction, pumps);
 
-        console.logBytes(immutableData);
-        console.logBytes(initData);
+        // Bore upgradeable well
+        address _well = IAquifer(AQUIFER).boreWell(
+            UPGRADEABLE_WELL_IMPLEMENTATION, immutableData, initData, salt
+        );
 
-        // address _well = IAquifer(AQUIFER).boreWell(
-        //     UPGRADEABLE_WELL_IMPLEMENTATION, immutableData, initData, salt
-        // );
-        // console.log("well bored at: ", _well);
-
-        // // deploy proxy
-        // address wellProxy = address(
-        //     new ERC1967Proxy{salt: salt}(
-        //         _well,
-        //         abi.encodeCall(IWellUpgradeable.init, (name, symbol))
-        //     )
-        // );
-        // console.log(name);
-        // console.log("well proxy deployed at:", wellProxy);
+        // Deploy proxy
+        address wellProxy = address(
+            new ERC1967Proxy{salt: salt}(
+                _well,
+                abi.encodeCall(IWellUpgradeable.init, (name, symbol))
+            )
+        );
     }
 
-    function deployUpgradableWells() internal {
-        IERC20[] memory beanEthTokens;
-        beanEthTokens[0] = IERC20(0xBEA0000029AD1c77D3d5D23Ba2D8893dB9d1Efab);
-        beanEthTokens[1] = IERC20(WETH);
+    function deployUpgradableWells(address bean) internal {
+        // tokens
+        IERC20[] memory tokens = new IERC20[](2);
+        tokens[0] = IERC20(bean);
+        tokens[1] = IERC20(WETH);
+
+        // cp2 
+        // TODO: change data if needed
+        Call memory cp2 = Call(CONSTANT_PRODUCT_2, abi.encode("beanWF"));
+
+        // stable2
+        // TODO: change data if needed
+        Call memory stable2 = Call(STABLE_2, abi.encode("beanStable"));
         
-        Call memory beanEthWellFunction = Call(CONSTANT_PRODUCT_2, abi.encode("beanstalkWF"));
-        Call[] memory beanEthPumps;
+        // pumps
+        // TODO: change pump data
+        // NOTE: Different data for each well pump or could this be the same? 
+        Call[] memory beanEthPumps = new Call[](1);
         beanEthPumps[0] = Call(MULTIFLOW_PUMP, abi.encode("beanstalkPump"));
 
         // BEAN/ETH well
         deployUpgradebleWell( 
-            beanEthTokens, // tokens (IERC20[])
-            beanEthWellFunction, // well function (Call)
+            tokens, // tokens (IERC20[])
+            cp2, // well function (Call)
             beanEthPumps, // pumps (Call[])
             BEAN_ETH_SALT, // salt
             BEAN_ETH_NAME, // name
             BEAN_ETH_SYMBOL // symbol
+        );
+
+        // BEAN/WSTETH well
+        tokens[1] = IERC20(WSTETH);
+        deployUpgradebleWell(
+            tokens, // tokens (IERC20[])
+            cp2, // well function (Call)
+            beanEthPumps, // pumps (Call[])
+            BEAN_WSTETH_SALT,
+            BEAN_WSTETH_NAME,
+            BEAN_WSTETH_SYMBOL
+        );
+
+        // BEAN/WEWETH well
+        tokens[1] = IERC20(WEETH);
+        deployUpgradebleWell(
+            tokens, // tokens (IERC20[])
+            cp2, // well function (Call)
+            beanEthPumps, // pumps (Call[])
+            BEAN_WEETH_SALT,
+            BEAN_WEETH_NAME,
+            BEAN_WEETH_SYMBOL
+        );
+
+        // BEAN/WBTC well
+        tokens[1] = IERC20(WBTC);
+        deployUpgradebleWell(
+            tokens, // tokens (IERC20[])
+            cp2, // well function (Call)
+            beanEthPumps, // pumps (Call[])
+            BEAN_WBTC_SALT,
+            BEAN_WBTC_NAME,
+            BEAN_WBTC_SYMBOL
+        );
+
+        // BEAN/USDC well
+        tokens[1] = IERC20(USDC);
+        deployUpgradebleWell(
+            tokens, // tokens (IERC20[])
+            stable2, // well function (Call)
+            beanEthPumps, // pumps (Call[])
+            BEAN_USDC_SALT,
+            BEAN_USDC_NAME,
+            BEAN_USDC_SYMBOL
+        );
+
+        // BEAN/USDT well
+        tokens[1] = IERC20(USDT);
+        deployUpgradebleWell(
+            tokens, // tokens (IERC20[])
+            stable2, // well function (Call)
+            beanEthPumps, // pumps (Call[])
+            BEAN_USDT_SALT,
+            BEAN_USDT_NAME,
+            BEAN_USDT_SYMBOL
         );
 
     }
