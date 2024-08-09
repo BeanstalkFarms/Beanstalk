1a722e35c8a1fd60179f2e998f79dd29bc9c19ef
Spacebean <wndgud00@gmail.com>
2024-08-09 22:04:56 +0200

    feat: update useCharttimePeriodState hook

--- projects/ui/src/hooks/display/useChartTimePeriodState.ts
+++ projects/ui/src/hooks/display/useChartTimePeriodState.ts
@@ -1,5 +1,6 @@
+import { subWeeks } from 'date-fns';
 import { Time, Range } from 'lightweight-charts';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export type CalendarTimeRange = Range<Time>;
 
@@ -14,17 +15,38 @@ export type CalendarRangePreset =
   | '2Y'
   | 'ALL';
 
-const getTimePeriodState = (prefix: string) => {
-  const storedSetting1 = localStorage.getItem(`${prefix}TimePeriod`);
-  return storedSetting1 ? JSON.parse(storedSetting1) : undefined;
+// charts default to 1 week
+const getWeek = (): Range<Time> => {
+  const now = Date.now();
+
+  return {
+    from: (subWeeks(now, 1).getTime() / 1000) as Time,
+    to: (now / 1000) as Time,
+  };
 };
 
-const useChartTimePeriodState = (
-  storageKeyPrefix: string = 'advancedChart'
-) => {
+const getTimePeriodState = (prefix: string): Range<Time> => {
+  const storedSetting = localStorage.getItem(`${prefix}TimePeriod`);
+  if (!storedSetting) {
+    const week = getWeek();
+    localStorage.setItem(`${prefix}TimePeriod`, JSON.stringify(week));
+    return week;
+  }
+  return JSON.parse(storedSetting) as Range<Time>;
+};
+
+/**
+ * defaults to 1 week
+ */
+const useChartTimePeriodState = (prefix: string = 'advancedChart') => {
   const [timePeriod, setTimePeriod] = useState<Range<Time> | undefined>(
-    getTimePeriodState(storageKeyPrefix)
+    getTimePeriodState(prefix)
   );
+
+  useEffect(() => {
+    const item = !timePeriod ? getWeek() : timePeriod;
+    localStorage.setItem(`${prefix}TimePeriod`, JSON.stringify(item));
+  }, [prefix, timePeriod]);
 
   return [timePeriod, setTimePeriod] as const;
 };
