3851ae9538b0d85be00790890b02e4a42d01fbfa
funderbrker <meellone01@gmail.com>
2024-08-15 17:12:49 +0800

    test - migrate deposits to new

--- protocol/test/foundry/lineage/MigrateToFork.t.sol
+++ protocol/test/foundry/lineage/MigrateToFork.t.sol
@@ -3,24 +3,33 @@ pragma solidity >=0.6.0 <0.9.0;
 pragma abicoder v2;
 
 import {C} from "contracts/C.sol";
+import {LibAltC} from "test/foundry/utils/LibAltC.sol";
+import {LibConstant} from "test/foundry/utils/LibConstant.sol";
 import {TestHelper} from "test/foundry/utils/TestHelper.sol";
 import {LibMigrateOut} from "contracts/libraries/Lineage/LibMigrateOut.sol";
 import {LibTransfer} from "contracts/libraries/Token/LibTransfer.sol";
+import {LibBytes} from "contracts/libraries/LibBytes.sol";
 import {IMockFBeanstalk} from "contracts/interfaces/IMockFBeanstalk.sol";
 import {BeanstalkDeployer} from "test/foundry/utils/BeanstalkDeployer.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /**
- * @notice Tests a migration from a Source to Destination Beanstalk.
+ * @notice Tests migrations to a child fork from og Beanstalk.
  */
-contract MigrateToChildTest is TestHelper {
+contract MigrateToForkTest is TestHelper {
+    IMockFBeanstalk newBs;
+    address NEW_BEANSTALK;
+
     uint32 constant REPLANT_SEASON = 6074;
+
+    address ZERO_ADDR = LibConstant.ZERO_ADDRESS;
 
     // test accounts
     address[] farmers;
     address payable newBeanstalk;
 
     function setUp() public {
-        initializeBeanstalkTestState(true, false);
+        initializeBeanstalkTestState(true, false, false);
 
         farmers = createUsers(3);
 
@@ -46,6 +55,24 @@ contract MigrateToChildTest is TestHelper {
         passGermination();
 
         addFertilizerBasedOnSprouts(REPLANT_SEASON, 100e6);
+
+        // Deploy new Beanstalk fork.
+        TestHelper altEcosystem = new TestHelper();
+        altEcosystem.initializeBeanstalkTestState(true, true, false);
+        altEcosystem.addLiquidityToWell(
+            C.BEAN_ETH_WELL,
+            10000e6, // 10,000 Beans
+            10 ether // 10 ether.
+        );
+        altEcosystem.addLiquidityToWell(
+            C.BEAN_WSTETH_WELL,
+            10000e6, // 10,000 Beans
+            10 ether // 10 ether.
+        );
+
+        newBs = IMockFBeanstalk(altEcosystem.bs());
+        NEW_BEANSTALK = address(newBs);
+        newBs.fastForward(100);
     }
 
     /**
@@ -55,7 +82,7 @@ contract MigrateToChildTest is TestHelper {
     // function test_migrateRevert() public {}
 
     /**
-     * @notice Performs a migration of all asset types from a Source to Destination Beanstalk.
+     * @notice Performs a migration of all asset types from a Source to Destination fork.
      */
     function test_migrateDeposits(uint256 depositAmount) public {
         depositAmount = bound(depositAmount, 100, 10_000_000e6);
@@ -94,37 +121,49 @@ contract MigrateToChildTest is TestHelper {
                 address(0), // populated by source
                 0 // populated by source
             );
+            // Check events for burning and minting of bean token.
+            vm.expectEmit();
+            emit IERC20.Transfer(BEANSTALK, ZERO_ADDR, firstMigrationAmount);
+            vm.expectEmit();
+            emit IERC20.Transfer(BEANSTALK, ZERO_ADDR, secondMigrationAmount);
+            vm.expectEmit();
+            emit IERC20.Transfer(ZERO_ADDR, NEW_BEANSTALK, firstMigrationAmount);
+            vm.expectEmit();
+            emit IERC20.Transfer(ZERO_ADDR, NEW_BEANSTALK, secondMigrationAmount);
         }
 
         vm.prank(user);
         bs.migrateOut(
-            // TODO change to not migrate into self
-            BEANSTALK, // Migrate into self
+            NEW_BEANSTALK,
             deposits,
             new IMockFBeanstalk.SourcePlot[](0),
             new IMockFBeanstalk.SourceFertilizer[](0),
             abi.encode("")
         );
 
-        // NOTE: Cannot do this yet, bc source == destination.
-        // // Verify Source deposits.
-        // {
-        //     (uint256 sourceDepositAmount, uint256 sourceDepositBdv) = bs.getDeposit(
-        //         user,
-        //         C.BEAN,
-        //         stem
-        //     );
-        //     require(sourceDepositAmount == 0, "Source deposit amount mismatch");
-        //     require(sourceDepositBdv == 0, "Source deposit bdv mismatch");
-        // }
-        // Verify Destination deposits.
+        // Verify Source deposits.
         {
-            int96 destinationStem = stem;
-            (uint256 destinationDepositAmount, uint256 destinationDepositBdv) = bs.getDeposit(
+            (uint256 sourceDepositAmount, uint256 sourceDepositBdv) = bs.getDeposit(
                 user,
                 C.BEAN,
-                destinationStem
+                stem
             );
+            require(sourceDepositAmount == 0, "Source deposit amount mismatch");
+            require(sourceDepositBdv == 0, "Source deposit bdv mismatch");
+        }
+        // Verify Destination deposits.
+        {
+            uint256[] memory destDepositIds = newBs
+                .getTokenDepositsForAccount(user, C.BEAN)
+                .depositIds;
+            require(destDepositIds.length == 1, "Dest deposit count mismatch");
+            (address token, int96 stem) = LibBytes.unpackAddressAndStem(destDepositIds[0]);
+            (uint256 destinationDepositAmount, uint256 destinationDepositBdv) = newBs.getDeposit(
+                user,
+                C.BEAN, // TODO: Can this be made into a new Bean token?
+                stem
+            );
+            require(stem < newBs.stemTipForToken(token), "Dest deposit stem too high");
             require(depositAmount == destinationDepositAmount, "Dest deposit amount mismatch");
             require(depositBdv == destinationDepositBdv, "Dest deposit bdv mismatch");
         }
@@ -138,13 +177,11 @@ contract MigrateToChildTest is TestHelper {
         address user = farmers[2];
 
         uint256 podsPerSow = sowForUser(user, sowAmount);
+        uint256 halfPlot = podsPerSow / 2;
         sowForUser(user, sowAmount);
 
         IMockFBeanstalk.SourcePlot[] memory plots = new IMockFBeanstalk.SourcePlot[](2);
         {
-            uint256 firstPlotAmount = 50;
-            uint256 thirdPlotAmount = 1;
-            uint256 secondPlotAmount = sowAmount - firstPlotAmount - thirdPlotAmount;
             // Initial Migration of a Plot. With index != 0.
             plots[0] = IMockFBeanstalk.SourcePlot(
                 0, // fieldId
@@ -156,7 +193,7 @@ contract MigrateToChildTest is TestHelper {
             plots[1] = IMockFBeanstalk.SourcePlot(
                 0, // fieldId
                 0, // plotId
-                podsPerSow, // amount
+                halfPlot, // amount
                 0 // prevDestIndex
             );
         }
@@ -164,33 +201,30 @@ contract MigrateToChildTest is TestHelper {
         vm.prank(user);
         bs.migrateOut(
             // TODO change to not migrate into self, but this requires significant changes to initialization system.
-            BEANSTALK, // Migrate into self
+            NEW_BEANSTALK, // Migrate into self
             new IMockFBeanstalk.SourceDeposit[](0),
             plots,
             new IMockFBeanstalk.SourceFertilizer[](0),
             abi.encode("")
         );
 
-        // NOTE: Cannot do this yet, bc source == destination.
-        // // Verify Source plots.
-        // {
-        //     require(bs.plot(user, 0, 0) == 0, "First source plot amount mismatch");
-        //     require(bs.plot(user, 0, podsPerSow) == 0, "Second source plot amount mismatch");
-        //     require(bs.plot(NULL_ADDR, 0, 0) == podsPerSow, "First source null plot amount mismatch");
-        //     require(bs.plot(NULL_ADDR, 0, podsPerSow) == podsPerSow, "Second source null plot amount mismatch");
-        // }
+        // Verify Source plots.
+        {
+            require(bs.plot(user, 0, 0) == 0, "1st src plot amount");
+            require(bs.plot(user, 0, podsPerSow) == halfPlot, "2nd src plot amount");
+            require(bs.plot(user, 0, podsPerSow + halfPlot) == halfPlot, "2.5 src plot amount");
+            require(bs.plot(ZERO_ADDR, 0, 0) == podsPerSow, "1st src null plot amount");
+            require(bs.plot(ZERO_ADDR, 0, podsPerSow) == halfPlot, "2nd src null plot amount");
+        }
         // Verify Destination plots.
         {
-            require(bs.plot(user, 0, 0) == podsPerSow, "First source plot amount mismatch");
-            require(
-                bs.plot(user, 0, podsPerSow) == podsPerSow,
-                "Second source plot amount mismatch"
-            );
+            require(newBs.plot(user, 0, 0) == podsPerSow, "1st src plot amount");
+            require(newBs.plot(user, 0, podsPerSow) == podsPerSow, "2nd src plot amount");
         }
     }
 
     /**
-     * @notice Performs a migration of all asset types from a Source to Destination Beanstalk.
+     * @notice Performs a migration of all asset types from a Source to Destination fork.
      */
     function test_migrateFertilizer(uint256 ethAmount) public {
         address user = farmers[2];
