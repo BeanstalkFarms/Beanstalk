f1db4053b270104b5266438bcf7068875f32890d
Spacebean <wndgud00@gmail.com>
2024-08-19 10:27:40 -0600

    feat: update chains

--- projects/ui/src/util/wagmi/chains.ts
+++ projects/ui/src/util/wagmi/chains.ts
@@ -1,5 +1,40 @@
 import { defineChain } from 'viem';
 
+// Define our own mainnet instead of using the default from viem b/c
+// it defaults to the cloudflare-eth rpc instead of alchemy.
+export const mainnet = defineChain({
+  id: 1,
+  name: 'Ethereum',
+  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: [
+        `https://eth-mainnet.g.alchemy.com/v2/${import.meta.env.VITE_ALCHEMY_API_KEY}`,
+      ],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Etherscan',
+      url: 'https://etherscan.io',
+      apiUrl: 'https://api.etherscan.io/api',
+    },
+  },
+  contracts: {
+    ensRegistry: {
+      address: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
+    },
+    ensUniversalResolver: {
+      address: '0xE4Acdd618deED4e6d2f03b9bf62dc6118FC9A4da',
+      blockCreated: 16773775,
+    },
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 14353601,
+    },
+  },
+});
+
 export const localFork = defineChain({
   id: 1337,
   name: 'localhost:8545',
