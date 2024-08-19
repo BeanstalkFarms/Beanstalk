b326d3c8e5cda6f8d8e3ef5821aee3c7c8062f5b
Soil King <157099073+soilking@users.noreply.github.com>
2024-08-19 13:57:18 -0700

    increment version

--- projects/subgraph-beanstalk/src/utils/Init.ts
+++ projects/subgraph-beanstalk/src/utils/Init.ts
@@ -3,7 +3,7 @@ import { Version } from "../../generated/schema";
 
 export function handleInitVersion(block: ethereum.Block): void {
   const versionEntity = new Version("subgraph");
-  versionEntity.versionNumber = "2.4.0";
+  versionEntity.versionNumber = "2.4.1";
   versionEntity.subgraphName = subgraphNameForBlockNumber(block.number);
   versionEntity.chain = chainForBlockNumber(block.number);
   versionEntity.save();
