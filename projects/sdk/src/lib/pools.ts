795c59a6b653ec01a01f485363af899f6a9dad5d
Spacebean <wndgud00@gmail.com>
2024-08-24 11:52:54 -0600

    feat: add wells

--- projects/sdk/src/lib/pools.ts
+++ projects/sdk/src/lib/pools.ts
@@ -6,11 +6,19 @@ import { BeanstalkSDK } from "src/lib/BeanstalkSDK";
 
 export class Pools {
   static sdk: BeanstalkSDK;
-  public readonly BEAN_CRV3: CurveMetaPool;
   public readonly BEAN_ETH_WELL: BasinWell;
   public readonly BEAN_WSTETH_WELL: BasinWell;
+  public readonly BEAN_WEETH_WELL: BasinWell;
+  public readonly BEAN_WBTC_WELL: BasinWell;
+  public readonly BEAN_USDC_WELL: BasinWell;
+  public readonly BEAN_USDT_WELL: BasinWell;
+
+  /** @deprecated */
+  public readonly BEAN_CRV3: CurveMetaPool;
 
   public readonly pools: Set<Pool>;
+
+  public readonly wells: Set<BasinWell>;
 
   private lpAddressMap = new Map<string, Pool>();
 
@@ -35,8 +43,12 @@ export class Pools {
         color: "#ed9f9c"
       }
     );
-    this.pools.add(this.BEAN_CRV3);
-    this.lpAddressMap.set(sdk.tokens.BEAN_CRV3_LP.address.toLowerCase(), this.BEAN_CRV3);
+
+    // Add the pool to the pools set and the lpAddressMap if the LP token exists on selected chain
+    if (sdk.tokens.BEAN_CRV3_LP.address) {
+      this.pools.add(this.BEAN_CRV3);
+      this.lpAddressMap.set(sdk.tokens.BEAN_CRV3_LP.address, this.BEAN_CRV3);
+    }
 
     ////// Basin Well
 
@@ -46,14 +58,14 @@ export class Pools {
       sdk.tokens.BEAN_ETH_WELL_LP,
       [sdk.tokens.BEAN, sdk.tokens.WETH],
       {
-        name: "Basin Bean:ETH Well",
+        name: "Basin BEAN:ETH Well",
         logo: "",
         symbol: "BEAN:ETH",
         color: "#ed9f9c"
       }
     );
-    this.pools.add(this.BEAN_ETH_WELL);
-    this.lpAddressMap.set(sdk.tokens.BEAN_ETH_WELL_LP.address.toLowerCase(), this.BEAN_ETH_WELL);
+    this.wells.add(this.BEAN_ETH_WELL);
+    this.lpAddressMap.set(sdk.addresses.BEANWETH_WELL.get(sdk.chainId), this.BEAN_ETH_WELL);
 
     this.BEAN_WSTETH_WELL = new BasinWell(
       sdk,
@@ -61,17 +73,78 @@ export class Pools {
       sdk.tokens.BEAN_WSTETH_WELL_LP,
       [sdk.tokens.BEAN, sdk.tokens.WSTETH],
       {
-        name: "Basin Bean:wstETH Well",
+        name: "Basin BEAN:wstETH Well",
         logo: "",
         symbol: "BEAN:wstETH",
         color: "#ed9f9c"
       }
     );
-    this.pools.add(this.BEAN_WSTETH_WELL);
-    this.lpAddressMap.set(
-      sdk.tokens.BEAN_WSTETH_WELL_LP.address.toLowerCase(),
-      this.BEAN_WSTETH_WELL
+    this.wells.add(this.BEAN_WSTETH_WELL);
+    this.lpAddressMap.set(sdk.tokens.BEAN_WSTETH_WELL_LP.address, this.BEAN_WSTETH_WELL);
+
+    this.BEAN_WEETH_WELL = new BasinWell(
+      sdk,
+      sdk.addresses.BEANWEETH_WELL.get(sdk.chainId),
+      sdk.tokens.BEAN_WEETH_WELL_LP,
+      [sdk.tokens.BEAN, sdk.tokens.WEETH],
+      {
+        name: "Basin BEAN:weETH Well",
+        logo: "",
+        symbol: "BEAN:weETH",
+        color: "#ed9f9c"
+      }
     );
+    this.wells.add(this.BEAN_WEETH_WELL);
+    this.lpAddressMap.set(sdk.tokens.BEAN_WEETH_WELL_LP.address, this.BEAN_WEETH_WELL);
+
+    this.BEAN_WBTC_WELL = new BasinWell(
+      sdk,
+      sdk.addresses.BEANWBTC_WELL.get(sdk.chainId),
+      sdk.tokens.BEAN_WBTC_WELL_LP,
+      [sdk.tokens.BEAN, sdk.tokens.WBTC],
+      {
+        name: "Basin BEAN:wBTC Well",
+        logo: "",
+        symbol: "BEAN:WBTC",
+        color: "#ed9f9c"
+      }
+    );
+    this.wells.add(this.BEAN_WBTC_WELL);
+    this.lpAddressMap.set(sdk.tokens.BEAN_WBTC_WELL_LP.address, this.BEAN_WBTC_WELL);
+
+    this.BEAN_USDC_WELL = new BasinWell(
+      sdk,
+      sdk.addresses.BEANUSDC_WELL.get(sdk.chainId),
+      sdk.tokens.BEAN_USDC_WELL_LP,
+      [sdk.tokens.BEAN, sdk.tokens.USDC],
+      {
+        name: "Basin BEAN:USDC Well",
+        logo: "",
+        symbol: "BEAN:USDC",
+        color: "#ed9f9c"
+      }
+    );
+    this.wells.add(this.BEAN_USDC_WELL);
+    this.lpAddressMap.set(sdk.tokens.BEAN_USDC_WELL_LP.address, this.BEAN_USDC_WELL);
+
+    this.BEAN_USDT_WELL = new BasinWell(
+      sdk,
+      sdk.addresses.BEANUSDT_WELL.get(sdk.chainId),
+      sdk.tokens.BEAN_USDT_WELL_LP,
+      [sdk.tokens.BEAN, sdk.tokens.USDT],
+      {
+        name: "Basin BEAN:USDT Well",
+        logo: "",
+        symbol: "BEAN:USDT",
+        color: "#ed9f9c"
+      }
+    );
+    this.wells.add(this.BEAN_USDT_WELL);
+    this.lpAddressMap.set(sdk.tokens.BEAN_USDT_WELL_LP.address, this.BEAN_USDT_WELL);
+
+    this.wells.forEach((well) => {
+      this.pools.add(well);
+    });
   }
 
   getPoolByLPToken(token: Token): Pool | undefined {
@@ -85,5 +158,13 @@ export class Pools {
     }
 
     return wells;
+  }
+
+  getWellByLPToken(token: Token): BasinWell | undefined {
+    const well = this.lpAddressMap.get(token.address);
+    if (well && well instanceof BasinWell) {
+      return well;
+    }
+    return;
   }
 }
