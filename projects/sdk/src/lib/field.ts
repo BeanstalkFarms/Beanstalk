7af83a0c041f1d65f352eccaf352e88fd1b40d5b
Spacebean <wndgud00@gmail.com>
2024-08-26 16:51:03 -0600

    feat: update field get plots

--- projects/sdk/src/lib/field.ts
+++ projects/sdk/src/lib/field.ts
@@ -1,6 +1,7 @@
-import { BigNumberish } from "ethers";
+import { BigNumber, BigNumberish, ethers } from "ethers";
 import { BeanstalkSDK } from "./BeanstalkSDK";
 import { TokenValue } from "@beanstalk/sdk-core";
+import { ZERO_BN } from "src/constants";
 
 export class Field {
   static sdk: BeanstalkSDK;
@@ -9,17 +10,65 @@ export class Field {
     Field.sdk = sdk;
   }
 
-  public async getPlots(account: string, _fieldId: BigNumberish = "0") {
-    const plots = await Field.sdk.contracts.beanstalk.getPlotsFromAccount(account, _fieldId);
+  public async getPlots({
+    harvestableIndex: _harvestableIndex,
+    account,
+    fieldId = "0"
+  }: {
+    harvestableIndex: BigNumber | TokenValue;
+    account: string;
+    fieldId: BigNumberish;
+  }) {
+    const harvestableIndex =
+      _harvestableIndex instanceof TokenValue ? _harvestableIndex.toBigNumber() : _harvestableIndex;
 
-    const plotMap = plots.reduce<Record<string, TokenValue>>((prev, curr) => {
-      const index = Field.sdk.tokens.PODS.fromBlockchain(curr.index);
-      const pods = Field.sdk.tokens.PODS.fromBlockchain(curr.pods);
+    const plots = await Field.sdk.contracts.beanstalk
+      .getPlotsFromAccount(account, fieldId)
+      .then(
+        (p) =>
+          new Map<string, BigNumber>(p.map(({ pods, index }) => [index.toString(), pods] as const))
+      );
 
-      prev[index.toHuman()] = pods;
-      return prev;
-    }, {});
+    let pods = ZERO_BN;
+    let harvestablePods = ZERO_BN;
 
-    return plotMap;
+    const unharvestablePlots: Map<string, ethers.BigNumber> = new Map();
+    const harvestablePlots: Map<string, ethers.BigNumber> = new Map();
+
+    plots.forEach((plot, startIndexStr) => {
+      const startIndex = ethers.BigNumber.from(startIndexStr);
+
+      // Fully harvestable
+      if (startIndex.add(plot).lte(harvestableIndex)) {
+        harvestablePods = harvestablePods.add(plot);
+        harvestablePlots.set(startIndexStr, plot);
+      }
+
+      // Partially harvestable
+      else if (startIndex.lt(harvestableIndex)) {
+        const partialAmount = harvestableIndex.sub(startIndex);
+
+        harvestablePods = harvestablePods.add(partialAmount);
+        pods = pods.add(plot.sub(partialAmount));
+
+        harvestablePlots.set(startIndexStr, partialAmount);
+        unharvestablePlots.set(harvestableIndex.toString(), plot.sub(partialAmount));
+      }
+
+      // Unharvestable
+      else {
+        pods = pods.add(plot);
+        unharvestablePlots.set(startIndexStr, plot);
+      }
+    });
+
+    // FIXME: "unharvestable pods" are just Pods,
+    // but we can't reuse "plots" in the same way.
+    return {
+      pods,
+      harvestablePods,
+      plots: unharvestablePlots,
+      harvestablePlots
+    };
   }
 }
