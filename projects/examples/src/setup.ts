d7d9cbb3d150b5b05dec8df6c15231931d980f43
Spacebean <wndgud00@gmail.com>
2024-08-28 06:19:14 -0600

    feat: update examples + setup

--- projects/examples/src/setup.ts
+++ projects/examples/src/setup.ts
@@ -3,8 +3,14 @@ import { Provider } from "@beanstalk/sdk/dist/types/lib/BeanstalkSDK";
 import { ethers } from "ethers";
 
 const RPC_URL = "http://127.0.0.1:8545";
+
+const network = {
+  name: "local",
+  chainId: 41337,
+  _defaultProvider: () => new ethers.providers.JsonRpcProvider(RPC_URL)
+};
 // const RPC_URL = "https://anvil1.bean.money:443"
-export const provider = new ethers.providers.StaticJsonRpcProvider(RPC_URL);
+export const provider = new ethers.providers.StaticJsonRpcProvider(RPC_URL, network);
 export const { signer, account } = TestUtils.setupConnection(provider);
 
 export const sdk = new BeanstalkSDK({
@@ -15,8 +21,7 @@ export const sdk = new BeanstalkSDK({
 
 export const impersonate = async (account) => {
   const stop = await chain.impersonate(account);
-
-  const provider = ethers.getDefaultProvider(RPC_URL) as Provider;
+  const provider = ethers.getDefaultProvider(network) as Provider;
   const signer = await provider.getSigner(account);
   const sdk = new BeanstalkSDK({
     signer,
