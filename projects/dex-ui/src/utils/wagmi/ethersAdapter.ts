86d64aaccd9d79030ee4d5b5f784e4214433e526
Spacebean <wndgud00@gmail.com>
2024-09-03 13:10:55 -0600

    feat: update wagmi adapters

--- projects/dex-ui/src/utils/wagmi/ethersAdapter.ts
+++ projects/dex-ui/src/utils/wagmi/ethersAdapter.ts
@@ -12,7 +12,9 @@ function clientToProvider(client: Client<Transport, Chain>) {
   };
   if (transport.type === "fallback")
     return new providers.FallbackProvider(
-      (transport.transports as ReturnType<Transport>[]).map(({ value }) => new providers.JsonRpcProvider(value?.url, network))
+      (transport.transports as ReturnType<Transport>[]).map(
+        ({ value }) => new providers.JsonRpcProvider(value?.url, network)
+      )
     );
   return new providers.JsonRpcProvider(transport.url, network);
 }
@@ -36,11 +38,21 @@ export function useEthersProvider({ chainId }: { chainId?: number | undefined } 
   if (!client) {
     throw new Error("No client to create Ethers Adapter");
   }
-  return useMemo(() => clientToProvider(client), [client]);
+
+  return useMemo(
+    () => clientToProvider(client),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [client, chainId]
+  );
 }
 
 /** Hook to convert a Viem Client to an ethers.js Signer. */
 export function useEthersSigner({ chainId }: { chainId?: number } = {}) {
   const { data: client } = useConnectorClient<Config>({ chainId });
-  return useMemo(() => (client ? clientToSigner(client) : undefined), [client]);
+
+  return useMemo(
+    () => (client ? clientToSigner(client) : undefined),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [client, chainId]
+  );
 }
