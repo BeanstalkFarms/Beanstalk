b83628bff9ef1967270dcca3528179a23108115a
Spacebean <wndgud00@gmail.com>
2024-09-06 19:31:02 -0600

    feat: update useWellFunctionNames to be multicall

--- projects/dex-ui/src/wells/wellFunction/useWellFunctionNames.ts
+++ projects/dex-ui/src/wells/wellFunction/useWellFunctionNames.ts
@@ -1,11 +1,19 @@
-import { Well, WellFunction } from "@beanstalk/sdk-wells";
 import { useQuery } from "@tanstack/react-query";
+import { multicall } from "@wagmi/core";
+import { ContractFunctionParameters, MulticallReturnType } from "viem";
+
+import { Well, WellFunction } from "@beanstalk/sdk-wells";
+
 import { AddressMap } from "src/types";
+import { Log } from "src/utils/logger";
 import { queryKeys } from "src/utils/query/queryKeys";
+import { config } from "src/utils/wagmi/config";
 
 interface WellWithWellFn extends Well {
   wellFunction: WellFunction;
 }
+
+const Logger = Log.module("useWellFunctionNames");
 
 /**
  * Returns a Record of well function addresses to their names.
@@ -19,16 +27,36 @@ export const useWellFunctionNames = (_wells: Well[] | undefined) => {
   return useQuery({
     queryKey: queryKeys.wellFunctionNames(addresses.length ? addresses : ["invalid"]),
     queryFn: async () => {
-      // TODO: make me into a multi call at some point.
-      const names = await Promise.all(
-        wellsWithWellFunctions.map((well) => well.wellFunction.getName())
-      );
+      Logger.debug(`Fetching well function names for wells: ${addresses}`);
+      const calls = makeMultiCall(wellsWithWellFunctions);
+      const names = await multicall(config, { contracts: calls, allowFailure: true });
+      Logger.debug(`Well function names: ${names}`);
 
       return wellsWithWellFunctions.reduce<AddressMap<string>>((prev, curr, i) => {
-        prev[curr.wellFunction.address] = names[i];
+        const result = extractResult(names[i]);
+        if (!result) return prev;
+        prev[curr.wellFunction.address] = result as string;
         return prev;
       }, {});
     },
     enabled: !!wells.length
   });
+};
+
+const extractResult = (result: MulticallReturnType[number]) => {
+  if (result.error) return null;
+  return result.result as string;
+};
+
+const makeMultiCall = (wells: WellWithWellFn[]) => {
+  const calls: ContractFunctionParameters<typeof WellFunction.abi>[] = wells.map((well) => {
+    return {
+      address: well.wellFunction.address as `0x{string}`,
+      abi: WellFunction.abi,
+      functionName: "name",
+      args: []
+    };
+  });
+
+  return calls;
 };
