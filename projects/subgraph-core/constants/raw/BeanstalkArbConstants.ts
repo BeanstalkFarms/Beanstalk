3b4707e57b2998f4f69628bbb32c9d70e77aa5c1
Soil King <157099073+soilking@users.noreply.github.com>
2024-09-12 15:27:30 -0700

    update type

---
+++ projects/subgraph-core/constants/raw/BeanstalkArbConstants.ts
@@ -0,0 +1,5 @@
+import { Address, BigInt } from "@graphprotocol/graph-ts";
+
+export const BEANSTALK = Address.fromString("0xD1A0060ba708BC4BCD3DA6C37EFa8deDF015FB70");
+
+// export const RESEED_SEASON = BigInt.fromU32(??);
