e8cc4223ae8c9c514b78099318aeed759530333c
Spacebean <wndgud00@gmail.com>
2024-09-13 11:39:31 -0600

    feat: minor updates to UI

--- projects/ui/src/types.ts
+++ projects/ui/src/types.ts
@@ -1,3 +1,4 @@
+import { BeanstalkSDK } from '@beanstalk/sdk';
 import React from 'react';
 
 export type FC<T extends any> = React.FC<React.PropsWithChildren<T>>;
@@ -5,3 +6,10 @@ export type FC<T extends any> = React.FC<React.PropsWithChildren<T>>;
 export type MayPromise<V> = V | Promise<V>;
 
 export type MayArray<V> = V | V[];
+
+// Beanstalk SDK Workflows
+type SDkFarm = BeanstalkSDK['farm'];
+
+export type FarmWorkflow = ReturnType<SDkFarm['create']>;
+export type AdvancedFarmWorkflow = ReturnType<SDkFarm['createAdvancedFarm']>;
+export type AdvancedPipeWorkflow = ReturnType<SDkFarm['createAdvancedPipe']>;
