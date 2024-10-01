af2e57e2e5ac3d6a363587d59770a3f1c00d9aea
Brean0 <midoryia33@proton.me>
2024-09-30 19:38:05 -0500

    update with marketplace

--- protocol/reseed/reseedL1.js
+++ protocol/reseed/reseedL1.js
@@ -33,9 +33,26 @@ async function reseedL1(account, mock) {
     "0x4e71e0c8", // claimOwnership
     "0x8da5cb5b", // owner
     "0x5f504a82", // ownerCandidate
-    "0xf2fde38b" // transferOwnership
+    "0xf2fde38b", // transferOwnership
+    "0xda3e3397", // TokenFacet functions.
+    "0x0bc33ce4",
+    "0xfdb28811",
+    "0xb6fc38f9",
+    "0xd4fac45d",
+    "0x6a385ae9",
+    "0x4667fa3d",
+    "0xc3714723",
+    "0x8a65d2e0",
+    "0xa98edb17",
+    "0xb39062e6",
+    "0xbc197c81",
+    "0xf23a6e61",
+    "0x8e8758d8",
+    "0xd3f4ec6f",
+    "0x6204aa43",
+    "0xbd32fac3",
+    "0x1c059365"
   ];
-
   for (let i = 0; i < SELECTORS.length; i++) {
     selectors = SELECTORS[i][1];
     for (let j = 0; j < selectors.length; j++) {
@@ -48,14 +65,14 @@ async function reseedL1(account, mock) {
 
   // remove only the siloFacet:
   console.log("Stage 1: Remove siloFacet");
-  await upgradeWithNewFacets({
-    diamondAddress: BEANSTALK,
-    selectorsToRemove: SILO_SELECTORS[1],
-    bip: false,
-    verbose: true,
-    account: account,
-    object: !mock
-  });
+  // await upgradeWithNewFacets({
+  //   diamondAddress: BEANSTALK,
+  //   selectorsToRemove: SILO_SELECTORS[1],
+  //   bip: false,
+  //   verbose: true,
+  //   account: account,
+  //   object: mock
+  // });
 
   // add the BeanL2MigrationFacet, L1TokenFacet, remove all selectors other than the diamond functionality.
   console.log(
