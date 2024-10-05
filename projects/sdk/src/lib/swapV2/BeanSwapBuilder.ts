f26d38c228c228ddd190745bb3cce924bb25009b
Spacebean <wndgud00@gmail.com>
2024-10-05 06:55:34 -0600

    feat: new quoteBuilder implementation

---
+++ projects/sdk/src/lib/swapV2/BeanSwapBuilder.ts
@@ -0,0 +1,309 @@
+import { ERC20Token, NativeToken, Token } from "src/classes/Token";
+
+import { BeanstalkSDK } from "src/lib/BeanstalkSDK";
+import { AdvancedFarmWorkflow, FarmFromMode, FarmToMode } from "src/lib/farm";
+import { AdvancedPipeWorkflow } from "src/lib/depot";
+
+import {
+  ERC20SwapNode,
+  NativeSwapNode,
+  SwapNode,
+  UnwrapEthSwapNode,
+  WellSwapNode,
+  WrapEthSwapNode,
+  ZeroXSwapNode
+} from "./nodes";
+import { TokenValue } from "@beanstalk/sdk-core";
+
+class Builder {
+  private static sdk: BeanstalkSDK;
+
+  #advPipe: AdvancedPipeWorkflow;
+
+  #advFarm: AdvancedFarmWorkflow;
+
+  #nodes: SwapNode[] = [];
+
+  constructor(sdk: BeanstalkSDK) {
+    Builder.sdk = sdk;
+    this.#initWorkFlows();
+  }
+
+  get advancedFarm() {
+    return this.#advFarm;
+  }
+
+  get nodes(): ReadonlyArray<SwapNode> {
+    return this.#nodes as ReadonlyArray<SwapNode>;
+  }
+
+  /**
+   * Compiles the swapNodes into an advanced farm workflow.
+   * @param nodes
+   * @param fromMode
+   * @param toMode
+   * @param caller
+   * @param recipient
+   *
+   * assumes that:
+   * - The nodes are in order of execution
+   * - The token in the first node is the token to be sold
+   * - The token in the last node is the token to be bought
+   * - The sellAmount in the first node is the amount to be sold
+   * - The buyAmount in the last node is the amount to be bought
+   */
+  translateNodesToWorkflow(
+    nodes: SwapNode[],
+    initFromMode: FarmFromMode,
+    finalToMode: FarmToMode,
+    caller: string,
+    recipient: string
+  ) {
+    this.#nodes = nodes;
+
+    if (!this.nodes.length) {
+      !this.#advFarm.length && this.#initWorkFlows();
+      throw new Error("Error building swap. No swap nodes provided.");
+    }
+
+    let fromMode = initFromMode;
+    let toMode = finalToMode;
+    const first = nodes[0];
+    const last = nodes[nodes.length - 1];
+    const numNodes = nodes.length;
+    const maxIndex = numNodes - 1;
+
+    this.#initWorkFlows(
+      `advFarm ${first.sellToken.symbol} -> ${last.buyToken.symbol}`,
+      `advPipe ${first.sellToken.symbol} -> ${last.buyToken.symbol}`
+    );
+
+    // Handle the case where there is only one action in the swap & it is a wrap or unwrap action.
+    if (numNodes === 1 && isNativeNode(first)) {
+      const wrapOrUnwrap = isWrapEthNode(first)
+        ? this.#getWrapETH(first, toMode, 0)
+        : this.#getUnwrapETH(first as UnwrapEthSwapNode, fromMode, 0);
+
+      this.#advFarm.add(wrapOrUnwrap, { tag: first.tag });
+      return;
+    }
+
+    for (const [i, node] of this.#nodes.entries()) {
+      // 1st leg of the swap
+      if (i === 0) {
+        toMode = FarmToMode.INTERNAL;
+
+        // Wrap ETH before loading pipeline
+        if (isWrapEthNode(node)) {
+          this.#advFarm.add(this.#getWrapETH(node, toMode, i), { tag: node.tag });
+          fromMode = FarmFromMode.INTERNAL_TOLERANT;
+        }
+
+        this.#loadPipeline(node, caller, fromMode);
+
+        // go next if wrapETH was the first action.
+        if (!isERC20Node(node)) {
+          continue;
+        }
+      }
+
+      // No need to update Farm modes until we offload pipeline.
+      if (isERC20Node(node)) {
+        const swap = isWellNode(node)
+          ? node.buildStep({ copySlot: this.#getPrevNodeCopySlot(i) })
+          : (node as ZeroXSwapNode).buildStep();
+
+        this.#advPipe.add(this.#getApproveERC20MaxAllowance(node));
+        this.#advPipe.add(swap, { tag: node.tag });
+      }
+
+      // Last leg of swap
+      if (i === maxIndex) {
+        fromMode = FarmFromMode.INTERNAL_TOLERANT;
+        toMode = toMode;
+
+        this.#offloadPipeline(node, recipient, fromMode, toMode, i);
+        this.#advFarm.add(this.#advPipe);
+
+        // Add UnwrapETH if last action
+        if (isUnwrapEthNode(node)) {
+          this.#advFarm.add(this.#getUnwrapETH(node, fromMode, i), { tag: node.tag });
+        }
+      }
+    }
+  }
+
+  /**
+   * Loads pipeline w/ the first ERC20 token in the swap sequence.
+   * @param node
+   *
+   * If the first action is to wrap ETH, we sequence it before loading pipeline. w/ the buyToken.
+   * If the caller is pipeline, no need to load tokens.
+   */
+  #loadPipeline(node: SwapNode, caller: string, from: FarmFromMode) {
+    const callerIsPipeline =
+      caller.toLowerCase() === Builder.sdk.contracts.pipeline.address.toLowerCase();
+    if (!callerIsPipeline) {
+      const loadToken = isWrapEthNode(node) ? node.buyToken : (node as ERC20SwapNode).sellToken;
+
+      const transfer = new Builder.sdk.farm.actions.TransferToken(
+        loadToken.address,
+        Builder.sdk.contracts.pipeline.address,
+        from,
+        FarmToMode.EXTERNAL
+      );
+
+      this.#advFarm.add(transfer);
+    }
+  }
+
+  /**
+   * Adds approve Beanstalk & transferToken to advancedPipe.
+   * Transfer to the recipient is conditional on the recipient not being pipeline.
+   */
+  #offloadPipeline(
+    node: SwapNode,
+    recipient: string,
+    fromMode: FarmFromMode,
+    toMode: FarmToMode,
+    i: number
+  ) {
+    const recipientIsPipeline =
+      recipient.toLowerCase() === Builder.sdk.contracts.pipeline.address.toLowerCase();
+    if (recipientIsPipeline) return;
+
+    const outputToken = node.buyToken;
+    const prevCopySlot = this.#getPrevNodeCopySlot(i);
+
+    let approveToken: ERC20Token;
+
+    if (isUnwrapEthNode(node)) {
+      approveToken = node.sellToken;
+    } else if (isERC20Node(node)) {
+      approveToken = node.buyToken;
+    } else {
+      throw new Error("Error building swap: Cannot determine approval token for transfer.");
+    }
+
+    if (prevCopySlot === undefined) {
+      throw new Error("Error building swap: Cannot determine copySlot from previous node.");
+    }
+
+    const prevActionClipboard = {
+      tag: node.tag,
+      copySlot: prevCopySlot
+    };
+
+    const approve = new Builder.sdk.farm.actions.ApproveERC20(
+      outputToken as ERC20Token,
+      Builder.sdk.contracts.beanstalk.address,
+      { ...prevActionClipboard, pasteSlot: 1 }
+    );
+
+    const transfer = new Builder.sdk.farm.actions.TransferToken(
+      outputToken.address,
+      recipient,
+      fromMode,
+      toMode,
+      { ...prevActionClipboard, pasteSlot: 2 }
+    );
+
+    this.#advPipe.add(approve);
+    this.#advPipe.add(transfer);
+  }
+
+  /**
+   *
+   */
+  #getApproveERC20MaxAllowance(node: SwapNode) {
+    if (!isERC20Node(node)) {
+      throw new Error("Misconfigured Swap Route. Cannot approve non-ERC20 token.");
+    }
+
+    // allow the allowance target to spend max tokens from Pipeline.
+    const approve = new Builder.sdk.farm.actions.ApproveERC20(node.sellToken, node.allowanceTarget);
+    approve.setAmount(TokenValue.MAX_UINT256);
+
+    return approve;
+  }
+
+  // ---------- SwapNode Utils ----------
+
+  /**
+   * extracts the unwrapETH step from the node
+   * @throws if UnwrapETH is not the last txn
+   */
+  #getUnwrapETH(node: UnwrapEthSwapNode, fromMode: FarmFromMode, i: number) {
+    const isFirst = i === 0;
+
+    // Scenarios where UnwrapETH is allowed:
+    const isFirstAndOnly = isFirst && this.#nodes.length === 1;
+    const isLast = i === this.#nodes.length - 1;
+
+    if (!isFirstAndOnly && !isLast) {
+      throw new Error(
+        "Error building swap: UnwrapETH can only be performed as the last txn in a sequnce of swaps."
+      );
+    }
+
+    const copySlot = this.#getPrevNodeCopySlot(i);
+
+    return node.buildStep({ fromMode, copySlot });
+  }
+
+  /**
+   * extracts the wrapETH step from the node
+   * @throws if WrapETH is not the first txn
+   */
+  #getWrapETH(node: WrapEthSwapNode, toMode: FarmToMode, i: number) {
+    // Ensure that WrapETH can only be added to the workflow as the first txn
+    if (i !== 0) {
+      throw new Error(
+        "Error building swap: WrapETH can only be performed first in a sequnce of txns."
+      );
+    }
+
+    return node.buildStep({ toMode });
+  }
+
+  /**
+   * Returns the copySlot of the previous node in the swap sequence.
+   */
+  #getPrevNodeCopySlot(i: number) {
+    if (!this.#nodes.length || !i) return undefined;
+
+    const prevNode = this.#nodes[i - 1];
+    if (prevNode && isERC20Node(prevNode)) {
+      return prevNode.amountOutCopySlot;
+    }
+    return undefined;
+  }
+
+  /// -------------------- Init Utils --------------------
+
+  #initWorkFlows(advFarmName?: string, advPipeName?: string) {
+    this.#advFarm = Builder.sdk.farm.createAdvancedFarm(advFarmName);
+    this.#advPipe = Builder.sdk.farm.createAdvancedPipe(advPipeName);
+    return this;
+  }
+}
+
+/// -------------------- Node Utils --------------------
+
+const isWrapEthNode = (node: SwapNode): node is WrapEthSwapNode => {
+  return node instanceof WrapEthSwapNode;
+};
+const isUnwrapEthNode = (node: SwapNode): node is UnwrapEthSwapNode => {
+  return node instanceof UnwrapEthSwapNode;
+};
+const isERC20Node = (node: SwapNode): node is ERC20SwapNode => {
+  return node instanceof ERC20SwapNode;
+};
+const isNativeNode = (node: SwapNode): node is NativeSwapNode => {
+  return node instanceof NativeSwapNode;
+};
+const isWellNode = (node: SwapNode): node is WellSwapNode => {
+  return node instanceof WellSwapNode;
+};
+
+export { Builder as BeanSwapBuilder };
