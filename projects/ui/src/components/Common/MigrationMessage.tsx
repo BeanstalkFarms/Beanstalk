514b7560c047750d15c0a909f147753a1924c5ba
Spacebean <wndgud00@gmail.com>
2024-10-08 08:56:50 -0600

    feat: add migration message

--- projects/ui/src/components/Common/MigrationMessage.tsx
+++ projects/ui/src/components/Common/MigrationMessage.tsx
@@ -3,13 +3,15 @@ import { Box, Typography } from '@mui/material';
 import useChainState from '~/hooks/chain/useChainState';
 
 export default function MigrationMessage({ message }: { message?: string }) {
-
   const { isArbitrum, isTestnet } = useChainState();
 
   return (
     <Box>
       <Typography variant="h1">
-        {message || isArbitrum ? "Coming Soon!" : "We've moved to Arbitrum!"}
+        {message || isArbitrum ? "We're migrating!" : "We're migrating!"}
+      </Typography>
+      <Typography variant="body1" textAlign="center">
+        Please check discord for details.
       </Typography>
     </Box>
   );
