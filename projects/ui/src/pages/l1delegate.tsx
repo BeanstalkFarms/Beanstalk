7846ac2cd5e3da204e378627960a01511b3302ec
uncoolzero <107518216+uncoolzero@users.noreply.github.com>
2024-10-08 08:19:44 -0300

    disabled condition

--- projects/ui/src/pages/l1delegate.tsx
+++ projects/ui/src/pages/l1delegate.tsx
@@ -293,7 +293,7 @@ export default function L1Delegate() {
                                                 specified.
                                             </Typography>
                                             <Button
-                                                disabled={!isAddressValid && beanBalance.eq(0)}
+                                                disabled={(!isAddressValid && beanBalance.eq(0)) || !destinationAccount}
                                                 sx={{
                                                     width: '100%',
                                                     height: 60,
@@ -312,7 +312,7 @@ export default function L1Delegate() {
                                                 specified.
                                             </Typography>
                                             <Button
-                                                disabled={!isAddressValid && !migrateInternal}
+                                                disabled={(!isAddressValid && !migrateInternal) || !destinationAccount}
                                                 sx={{
                                                     width: '100%',
                                                     height: 60,
