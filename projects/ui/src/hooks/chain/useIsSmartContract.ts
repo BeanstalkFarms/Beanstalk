55a2ab6201203fe969654b45ea35c8e29a7a7e3e
uncoolzero <107518216+uncoolzero@users.noreply.github.com>
2024-10-11 01:09:11 -0300

    switch to wagmi's getbytecode

--- projects/ui/src/hooks/chain/useIsSmartContract.ts
+++ projects/ui/src/hooks/chain/useIsSmartContract.ts
@@ -1,29 +1,34 @@
 import { useState, useEffect } from "react";
 import useAccount from "../ledger/useAccount";
-import useSdk from "../sdk";
+import { useChainId } from "wagmi";
+import { getBytecode } from "viem/actions";
+import { client } from "~/util/wagmi/config";
 
 export default function useIsSmartContract() {
 
     const account = useAccount();
-    const sdk = useSdk();
+    const chainId = useChainId();
     const [isContract, setIsContract] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         async function contractCheck() {
+            if (isLoading) return;
             if (!account) {
                 setIsContract(false)
             } else {
-                const _isContract = await sdk.provider.getCode(account);
+                setIsLoading(true);
+                const _isContract = await getBytecode(client, { address: account as `0x${string}` })
                 if (_isContract === "0x") {
                     setIsContract(false);
                 } else {
                     setIsContract(true);
                 }
+                setIsLoading(false);
             }
         };
         contractCheck()
-    }, [account])
+    }, [account, chainId])
 
     return isContract
-
 }
