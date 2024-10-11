14df70ba6394c28af643531fd14c24bdf6c04ca1
Spacebean <wndgud00@gmail.com>
2024-10-11 15:28:48 -0600

    feat: update schema + add comments to getGasToUsd()

--- projects/ui/src/hooks/ledger/useGasToUSD.ts
+++ projects/ui/src/hooks/ledger/useGasToUSD.ts
@@ -4,14 +4,12 @@ import { useSelector } from 'react-redux';
 import { AppState } from '~/state';
 import { useBalanceTokens } from '../beanstalk/useTokens';
 
-const ETH_PER_GWEI = new BigNumber(10).pow(-9);
-
 /**
  * Convert a gas limit from estimateGas into a USD gas price.
  *
  * Gas units (limit)         beanstalk.estimateGas.someFunction() returns gas limit (gwei)
- *   (Base Fee + Tip (gwei)) prices.gas.safe (in gwei)
- *   (1 ETH / 10^9 gwei)     constant
+ *   (Base Fee)              prices.baseFeePerGas (in wei)
+ *   (gasPrice)              prices.gasPrice (in wei)
  *   (X USD / 1 ETH)         prices.ethusd
  */
 const useGasToUSD = () => {
