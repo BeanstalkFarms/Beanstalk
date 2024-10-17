d55559373960425c944b80a3e38ffb2ca202f6d1
Soil King <157099073+soilking@users.noreply.github.com>
2024-10-17 14:22:30 -0700

    removed subgraphs from monorepo

--- projects/subgraph-beanstalk/src/handlers/legacy/LegacyFarmHandler.ts
+++
@@ -1,9 +0,0 @@
-import { InternalBalanceChanged } from "../../../generated/Beanstalk-ABIs/SeedGauge";
-import { loadFarmer } from "../../entities/Beanstalk";
-import { updateFarmTotals } from "../../utils/Farm";
-
-// Replanted -> Reseed
-export function handleInternalBalanceChanged(event: InternalBalanceChanged): void {
-  loadFarmer(event.params.user);
-  updateFarmTotals(event.address, event.params.user, event.params.token, event.params.delta, event.block);
-}
